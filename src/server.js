58c31279d4ea007e7919f916251e3ee5840ab438
larry <ladams1776@yahoo.com>
2019-03-18 22:54:56 -0400

    Update: Mongodb, body-parser, update react.

--- src/server.js
+++ src/server.js
@@ -1,15 +1,70 @@
 const express = require ('express');
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser');
 const app = express();
 
-app.listen(3001, function () {
-    console.log();
-});
+
+
+
+
+
+
+
+const TaskListValidatior = {
+    $jsonSchema: {            
+        bsonType: "object",
+        required: [ 
+            "description", 
+            "customer",
+            "contract",            
+            "time"
+            ],
+        properties: {
+            description: {
+                bsonType: "string",
+                maximum: 254,
+                description: "must be a string and no greater than 254"
+            },
+            customer: {
+                bsonType: "string",
+                description: "must be a string and is required"
+            },
+            contract: {
+                bsonType: "string",
+                description: "must be a string is required"
+            },
+            time: {
+                bsonType: "int",                
+                description: "must be a int and it is in millisecond format"
+            }
+        }            
+    }
+}
+
+var db;
+
+MongoClient.connect('mongodb://localhost:27017/tasks-lists', (err, client) => {
+  if (err) throw err
+
+//   db.collection('tasks').find().toArray(function (err, result) {
+//     if (err) throw err
+
+//     console.log(result)
+//   })
+    db = client.db('tasks');
+    app.listen(3001, function () {
+        console.log();
+    });    
+})
+
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/task", function (req, res) {
     //@todo: simulate db
