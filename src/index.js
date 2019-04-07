99f1a432d9e4cae37b62585b0d09e3ce972f574d
larry <ladams1776@yahoo.com>
2019-04-06 21:07:04 -0400

    Added font family

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Main';
+import './index.css';
 
 
 const dropDownListContracts = require('./resources/dropdownDefinition.json');
