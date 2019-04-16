d23b15a68e363a3612303eb5564257246a145b31
larry <ladams1776@yahoo.com>
2019-04-15 23:34:11 -0400

    Add Date, replace 'X' (for delete) on Task.

--- src/Form/TaskList/Task.js
+++ src/Form/TaskList/Task.js
@@ -33,12 +33,16 @@ export default class Task extends React.Component {
                 <NavLink to={"/task/" + this.state.task._id} id={this.state.task._id} className="task-list" >
                     <span className="task-list__time">
                         <span className="task-list__time-label">Time:
-                    </span>
-                        <span className="task-list__time-value"> {((((this.state.task.time / 1000) / 60) / 60)).toFixed(2)}
-                            <button onClick={this.onClick} className="task-list__delete-btn">X</button>
                         </span>
+                        <span className="task-list__time-value"> {((((this.state.task.time / 1000) / 60) / 60)).toFixed(2)}
+                            <button onClick={this.onClick} className="task-list__delete-btn glyphicon glyphicon-remove"></button>
+                        </span>
+                        <span className="task-list__item-date">{new Date(this.state.task.date).toDateString()}</span>
                     </span>
-                    <span className="task-list__description">{this.state.task.description}</span>
+                    <p className="task-list__description">{this.state.task.description.split("\n").map((paragraph, index) => { 
+                        return <p key={index} className="task-list__description-item">{paragraph}</p> 
+                    })
+                    }</p>
                     <span className="task-list__customer">{this.state.existingTasks[this.state.task.contractId].label}</span> - <span className="task-list__contract">{this.state.existingTasks[this.state.task.contractId].customer}</span>
                 </NavLink>
             </li>
