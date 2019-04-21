31382f2c3eabcb08fdbe2f2eb6a103580fd4701f
larry adams <ladams1776@yahoo.com>
2019-04-21 08:52:26 -0400

    Changed class to className
    Because React does not like 'class'

--- src/Form/TaskList/TaskListForm.js
+++ src/Form/TaskList/TaskListForm.js
@@ -86,7 +86,7 @@ export default class TaskListForm extends React.Component {
                 <div className="task-list__header">
                     {this.showDeleteButton()}
                     {this.showDownloadButton()}
-                    <NavLink to={"/task/-1"} className="button-add"><span class="glyphicon glyphicon-plus mr-5px"></span>New Task</NavLink>
+                    <NavLink to={"/task/-1"} className="button-add"><span className="glyphicon glyphicon-plus mr-5px"></span>New Task</NavLink>
                 </div>
                 <ul>
                     {this.state.tasks}
@@ -99,13 +99,13 @@ export default class TaskListForm extends React.Component {
 
     showDownloadButton() {
         if (this.state.tasks !== null) {
-            return <a href="#" className="button-download" onClick={this.handleDownload}><span class="glyphicon glyphicon-download-alt mr-5px"></span>Download</a>;
+            return <a href="#" className="button-download" onClick={this.handleDownload}><span className="glyphicon glyphicon-download-alt mr-5px"></span>Download</a>;
         }
     }
 
     showDeleteButton() {
         if (this.state.tasks !== null) {
-            return <a href="#" className="button-delete" onClick={this.handleDelete}><span class="glyphicon glyphicon-remove mr-5px"></span>Delete</a>;
+            return <a href="#" className="button-delete" onClick={this.handleDelete}><span className="glyphicon glyphicon-remove mr-5px"></span>Delete</a>;
         }
     }
 }
