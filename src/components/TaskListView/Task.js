f2aab23d9c7c570c4ba1e6395c3c3c4261e5ad65
larry adams <ladams1776@yahoo.com>
2019-09-28 10:18:04 -0400

    Refactor: Task into a func component

--- src/components/TaskListView/Task.js
+++ src/components/TaskListView/Task.js
@@ -3,17 +3,13 @@ import { NavLink } from "react-router-dom";
 import { getFormattedDate } from "../../utils/DateFormat";
 import "./Task.css";
 
-export default class Task extends React.Component {
-  constructor(props) {
-    super(props);
+const Task = props => {
+  const state = {
+    task: this.props.task,
+    existingTasks: this.props.existingTasks
+  };
 
-    this.state = {
-      task: this.props.task,
-      existingTasks: this.props.existingTasks
-    };
-  }
-
-  onClick = e => {
+  const _onClick = e => {
     e.preventDefault();
     fetch(`/api/task/${this.state.task._id}`, {
       method: "DELETE"
@@ -22,51 +18,49 @@ export default class Task extends React.Component {
       .then(setTimeout(() => window.location.reload(), 500));
   };
 
-  render() {
-    return (
-      <li key={this.state.task._id}>
-        <NavLink
-          to={"/task/" + this.state.task._id}
-          id={this.state.task._id}
-          className="task-item"
-        >
-          <div className="task-item__left">
-            <div className="task-item__description">
-              {this.state.task.description
-                .split("\n")
-                .map((paragraph, index) => {
-                  return (
-                    <p key={index} className="task-item__description-item">
-                      {paragraph}
-                    </p>
-                  );
-                })}
-            </div>
-            <span className="task-item__customer">
-              {this.state.existingTasks[this.state.task.contractId].label}
-            </span>{" "}
-            -{" "}
-            <span className="task-item__contract">
-              {this.state.existingTasks[this.state.task.contractId].customer}
-            </span>
+  return (
+    <li key={this.state.task._id}>
+      <NavLink
+        to={"/task/" + this.state.task._id}
+        id={this.state.task._id}
+        className="task-item"
+      >
+        <div className="task-item__left">
+          <div className="task-item__description">
+            {this.state.task.description.split("\n").map((paragraph, index) => {
+              return (
+                <p key={index} className="task-item__description-item">
+                  {paragraph}
+                </p>
+              );
+            })}
           </div>
-
-          <span className="task-item__time">
-            <span className="task-item__time-label">Time:</span>
-            <span className="task-item__time-value">
-              {" "}
-              {(this.state.task.time / 1000 / 60 / 60).toFixed(2)}
-              <button
-                onClick={this.onClick}
-                className="task-item__delete-btn glyphicon glyphicon-remove"
-              />
-            </span>
-            <span className="task-item__item-date">
-              {getFormattedDate(this.state.task.date)}
-            </span>
+          <span className="task-item__customer">
+            {this.state.existingTasks[this.state.task.contractId].label}
+          </span>{" "}
+          -{" "}
+          <span className="task-item__contract">
+            {this.state.existingTasks[this.state.task.contractId].customer}
           </span>
-        </NavLink>
-      </li>
-    );
-  }
-}
+        </div>
+
+        <span className="task-item__time">
+          <span className="task-item__time-label">Time:</span>
+          <span className="task-item__time-value">
+            {" "}
+            {(this.state.task.time / 1000 / 60 / 60).toFixed(2)}
+            <button
+              onClick={_onClick}
+              className="task-item__delete-btn glyphicon glyphicon-remove"
+            />
+          </span>
+          <span className="task-item__item-date">
+            {getFormattedDate(this.state.task.date)}
+          </span>
+        </span>
+      </NavLink>
+    </li>
+  );
+};
+
+export default Task;
