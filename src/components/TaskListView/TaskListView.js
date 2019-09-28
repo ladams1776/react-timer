27efdf610e447de1cb956431dad51ae61076ccfd
larry adams <ladams1776@yahoo.com>
2019-09-28 10:14:21 -0400

    Refactor: reformat TaskListView

--- src/components/TaskListView/TaskListView.js
+++ src/components/TaskListView/TaskListView.js
@@ -1,114 +1,117 @@
-import React from 'react';
-import {
-    NavLink
-} from "react-router-dom";
-import './TaskListView.css';
-import JsonWriter from './JsonWriter';
-import Task from './Task';
-import { getFormattedDate } from '../../utils/DateFormat';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./TaskListView.css";
+import JsonWriter from "./JsonWriter";
+import Task from "./Task";
+import { getFormattedDate } from "../../utils/DateFormat";
 
 export default class TaskListView extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tasks: [],
-            existingTasks: props.list
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      existingTasks: props.list
+    };
+
+    this.handleDownload = this.handleDownload.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ tasks: null });
+    fetch(`/api/tasks`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        if (data.length >= 1) {
+          let tasks = data.map(task => {
+            return (
+              <div key={task._id}>
+                <Task task={task} existingTasks={this.state.existingTasks} />
+              </div>
+            );
+          });
+          this.setState({ tasks: tasks });
+        }
+      });
+  }
+
+  handleDownload() {
+    fetch(`/api/tasks`)
+      .then(response => response.json())
+      .then(tasks => {
+        const date = new Date();
+        const dateFormatted = getFormattedDate(date);
+
+        const timeTask = {
+          date: dateFormatted
         };
 
-        this.handleDownload = this.handleDownload.bind(this);
+        const existingTasks = this.state.existingTasks;
+
+        tasks.forEach(function(task) {
+          task.time = (task.time / 1000 / 60 / 60).toFixed(2);
+
+          existingTasks.forEach(function(existingTask) {
+            if (task.contractId === existingTask.key) {
+              task.contract = existingTask.contract;
+              task.customer = existingTask.customer;
+            }
+          });
+        });
+
+        timeTask.WorkUnit = tasks;
+
+        const writer = new JsonWriter();
+        writer.write(timeTask);
+      });
+  }
+
+  /**
+   * Setting a timeout when reloading, because I think the reloading is happening too fast.
+   */
+  handleDelete = e => {
+    e.preventDefault();
+    fetch(`/api/tasks`, {
+      method: "DELETE"
+    })
+      .then(response => response.json())
+      .then(setTimeout(() => window.location.reload(), 500));
+  };
+
+  showDownloadButton() {
+    if (this.state.tasks !== null) {
+      return (
+        <a href="#" className="button-download" onClick={this.handleDownload}>
+          <span className="glyphicon glyphicon-download-alt mr-5px"></span>
+          Download
+        </a>
+      );
     }
+  }
 
-    componentDidMount() {
-        this.setState({ tasks: null });
-        fetch(`/api/tasks`)
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                if (data.length >= 1) {
-                    let tasks = data.map((task) => {
-                        return <div key={task._id} >
-                            <Task task={task} existingTasks={this.state.existingTasks} />
-                        </div>
-                    })
-                    this.setState({ tasks: tasks });
-                }
-            });
+  showDeleteButton() {
+    if (this.state.tasks !== null) {
+      return (
+        <a href="#" className="button-delete" onClick={this.handleDelete}>
+          <span className="glyphicon glyphicon-remove mr-5px"></span>Delete
+        </a>
+      );
     }
+  }
 
-    handleDownload() {
-
-        fetch(`/api/tasks`)
-            .then(response => response.json())
-            .then((tasks) => {
-
-                const date = new Date();
-                const dateFormatted = getFormattedDate(date);
-
-                const timeTask = {
-                    date: dateFormatted,
-                };
-
-                const existingTasks = this.state.existingTasks;
-
-                tasks.forEach(function (task) {
-                    task.time = ((((task.time / 1000) / 60) / 60)).toFixed(2);
-
-                    existingTasks.forEach(function (existingTask) {
-                        if (task.contractId === existingTask.key) {
-                            task.contract = existingTask.contract;
-                            task.customer = existingTask.customer;
-                        }
-                    });
-                });
-
-                timeTask.WorkUnit = tasks;
-
-                const writer = new JsonWriter();
-                writer.write(timeTask);
-            });
-    }
-
-    /**
-     * Setting a timeout when reloading, because I think the reloading is happening too fast.
-     */
-    handleDelete = (e) => {
-        e.preventDefault();
-        fetch(`/api/tasks`, {
-            method: 'DELETE'
-        })
-            .then(response => response.json())
-            .then(setTimeout(() => window.location.reload(), 500));
-    }
-
-
-
-
-    showDownloadButton() {
-        if (this.state.tasks !== null) {
-            return <a href="#" className="button-download" onClick={this.handleDownload}><span className="glyphicon glyphicon-download-alt mr-5px"></span>Download</a>;
-        }
-    }
-
-    showDeleteButton() {
-        if (this.state.tasks !== null) {
-            return <a href="#" className="button-delete" onClick={this.handleDelete}><span className="glyphicon glyphicon-remove mr-5px"></span>Delete</a>;
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="task-list__header">
-                    {this.showDeleteButton()}
-                    {this.showDownloadButton()}
-                    <NavLink to={"/task/-1"} className="button-add"><span className="glyphicon glyphicon-plus mr-5px"></span>New Task</NavLink>
-                </div>
-                <ul>
-                    {this.state.tasks}
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className="task-list__header">
+          {this.showDeleteButton()}
+          {this.showDownloadButton()}
+          <NavLink to={"/task/-1"} className="button-add">
+            <span className="glyphicon glyphicon-plus mr-5px"></span>New Task
+          </NavLink>
+        </div>
+        <ul>{this.state.tasks}</ul>
+      </div>
+    );
+  }
 }
