ed7679041f67f65aee65d55b172592681ada6bf0
larry.adams <ladams1776@yahoo.com>
2019-09-28 21:20:47 -0400

    Refactor TextArea into func comp

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -1,69 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import ReactRouterDom from "react-router-dom";
 
-import './TextArea.css';
+import "./TextArea.css";
 
+const TextArea = props => {
+  const [description, setDescription] = useState(null);
+  const { taskId, handler } = props;
 
-export default class TextArea extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            taskId: props.taskId,
-            labelTitle: props.title,
-            description: '',
-        }
-    }
-
-
-
-    componentDidMount() {
-        const taskId = this.state.taskId;
-
-        if (taskId !== -1) {
-            fetch('/api/task/' + taskId)
-                .then(response => {
-                    return response.json();
-                })
-                .then((task) => {
-                    const description = task.description ? task.description : '';
-                    this.setState({
-                        description: description,
-                    });
-
-                    this.props.handler(description);
-
-                });
-        }
-    }
-
-    handleChange = (e) => {
-        const newDescription = e.target.value;
-        this.setState({ 
-            description: newDescription
+  useEffect(() => {
+    if (taskId !== -1) {
+      fetch("/api/task/" + props.taskId)
+        .then(response => {
+          return response.json();
+        })
+        .then(task => {
+          //   const description = task.description ? task.description : '';
+          setDescription(task && task.description ? task.description : "");
+          handler(task && task.description ? task.description : "");
         });
-        this.props.handler(newDescription);
     }
+  }, [taskId]);
 
-    render() {
+  const handleChange = e => {
+    const newDescription = e.target.value;
+    setDescription(newDescription);
+    props.handler(newDescription);
+  };
 
-        return (
-            <div className="text-area">
-                <label
-                    className="text-area__label"
-                    htmlFor="description"
-                >
-                    {this.state.labelTitle}
-                </label>
-                <textarea className="text-area__description"
-                    name="description"
-                    value={this.state.description}
-                    onChange={this.handleChange}
-                    maxLength="254"
-                    rows="5"
-                    cols="52"
-                />
-                <span className="text-area__character-length">{this.state.description.length} character(s)</span>
-            </div>
-        )
-    }
-}
+  return (
+    <div className="text-area">
+      <label className="text-area__label" htmlFor="description">
+        {"Description"}
+      </label>
+      <textarea
+        className="text-area__description"
+        name="description"
+        value={description}
+        onChange={handleChange}
+        maxLength="254"
+        rows="5"
+        cols="52"
+      />
+      <span className="text-area__character-length">
+        {description && description.length} character(s)
+      </span>
+    </div>
+  );
+};
+
+export default TextArea;
