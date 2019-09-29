03ea8e65e501179ebf5350f91e7b013ad819b099
larry.adams <ladams1776@yahoo.com>
2019-09-29 09:45:07 -0400

    Refactor Timer into func component
    Also was able to reduce quite a few variables and simplify the logic.

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,113 +1,87 @@
-import React from 'react';
-import ms from 'pretty-ms';
-import './Timer.css';
+import React, { useEffect, useState } from "react";
+import ms from "pretty-ms";
+import "./Timer.css";
 
-export default class Timer extends React.Component {
+const Timer = ({ taskId, handler }) => {
+  const [time, setTime] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  const [timer, setTimer] = useState(null);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            taskId: this.props.taskId ? this.props.taskId : -1,
-            time: 0,
-            isOn: false,
-            start: 0,
-            stoppedTimeAt: 0,
-        };
-
-        this.startTimer = this.startTimer.bind(this);
-        this.resetTimer = this.resetTimer.bind(this);
-    }
-
-
-    componentDidMount() {
-        const taskId = this.state.taskId;
-
-        if (taskId !== -1) {
-            fetch('http://localhost:3001/api/task/' + taskId)
-                .then(response => {
-                    return response.json();
-                })
-                .then((task) => {
-                    this.setState({
-                        taskId: task._id,
-                        time: task.time,
-                    });
-
-                    this.setState({ stoppedTimeAt: task.time });
-                    this.props.handler(task.time)
-                });
-        }
-
-
-    }
-
-
-    startTimer() {
-        this.setState({
-            isOn: true,
-            time: this.state.time,
-            start: Date.now() - this.state.time
+  useEffect(() => {
+    if (taskId !== -1) {
+      fetch("http://localhost:3001/api/task/" + taskId)
+        .then(response => {
+          return response.json();
+        })
+        .then(task => {
+          setTime(task.time);
+          handler(task.time);
         });
-
-        this.timer = setInterval(() => this.setState({
-            time: Date.now() - this.state.start
-        }), 1);
     }
+  }, []);
 
-    stopTimer = (e) => {
-        this.setState({ isOn: false });
-        clearInterval(this.timer);
-        let timePassed = this.state.time;
-        this.setState({ stoppedTimeAt: timePassed });
-        this.props.handler(timePassed)
-    }
+  const startTimer = () => {
+    setIsOn(true);
+    let timeOffset = Date.now() - time;
+    setTimer(setInterval(() => setTime(Date.now() - timeOffset), 1));
+  };
 
-    resetTimer() {
-        this.stopTimer();
-        this.setState({ time: 0, isOn: false });
-    }
+  const stopTimer = e => {
+    setIsOn(false);
+    clearInterval(timer);
+    handler(time);
+  };
 
+  const resetTimer = () => {
+    stopTimer();
+    setTime(0);
+    setIsOn(false);
+  };
 
+  let startTime =
+    time === 0 && !isOn ? (
+      <button className="timer__start" onClick={startTimer}>
+        start
+      </button>
+    ) : null;
 
+  let stopTime = isOn ? (
+    <button className="timer__stop" onClick={stopTimer}>
+      stop
+    </button>
+  ) : null;
 
+  let resumeTime =
+    time !== 0 && !isOn ? (
+      <button className="timer__resume" onClick={startTimer}>
+        resume
+      </button>
+    ) : null;
 
-    render() {
+  let resetTime =
+    isOn || time > 0 ? (
+      <button className="timer__reset" onClick={resetTimer}>
+        reset
+      </button>
+    ) : null;
 
-        let start = (this.state.time === 0 && !this.state.isOn)
-            ? <button className="timer__start" onClick={this.startTimer}>start</button>
-            : null
+  return (
+    <div className="timer">
+      <div className="timer__display">
+        <label className="timer__display__label">Time: </label>
+        <div className="timer__display__content">
+          {ms(time)} - hours: {(time / 1000 / 60 / 60).toFixed(2)}
+        </div>
+      </div>
 
-        let stop = (this.state.isOn)
-            ? <button className="timer__stop" onClick={this.stopTimer}>stop</button>
-            : null
+      <div className="timer__buttons">
+        {startTime}
+        {resumeTime}
+        {stopTime}
+        {resetTime}
+      </div>
+    </div>
+  );
+};
 
-        let resume = (this.state.time !== 0 && !this.state.isOn)
-            ? <button className="timer__resume" onClick={this.startTimer}>resume</button>
-            : null
-
-        let reset = (this.startTimer.isOn || this.state.time > 0)
-            ? <button className="timer__reset" onClick={this.resetTimer}>reset</button>
-            : null
-
-
-        return (
-            <div className="timer">
-                <div className="timer__display">
-                    <label className="timer__display__label">Time: </label>
-                    <div className="timer__display__content">
-                        {ms(this.state.time)} - hours: {((((this.state.time / 1000) / 60) / 60)).toFixed(2)}
-                    </div>
-                </div>
-
-                <div className="timer__buttons">
-                    {start}
-                    {resume}
-                    {stop}
-                    {reset}
-                </div>
-
-            </div>
-        );
-    }
-}
+export default Timer;
