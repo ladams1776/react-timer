c56b517d4c2ac02671109b9dcab78998d4b5d328
larry adams <ladams1776@yahoo.com>
2019-11-17 12:42:27 -0500

    single quote refactor

--- src/components/TaskListView/Task/__test__/DeleteTaskButton.test.js
+++ src/components/TaskListView/Task/__test__/DeleteTaskButton.test.js
@@ -1,18 +1,17 @@
-import React from "react";
-import sinon from "sinon";
-import chai, { expect } from "chai";
-import SinonChai from "sinon-chai";
-import DeleteTaskButton from "../DeleteTaskButton";
-import Enzyme, { mount } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import React from 'react';
+import sinon from 'sinon';
+import chai, { expect } from 'chai';
+import SinonChai from 'sinon-chai';
+import DeleteTaskButton from '../DeleteTaskButton';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 chai.use(SinonChai);
 
-describe.only("src/components/DeleteTaskButton/__test__/DeleteTaskButton.test.js", () => {
-  describe("DeleteTaskButton", () => {
-   
-    it("should display DeleteTaskBu", () => {
+describe('src/components/DeleteTaskButton/__test__/DeleteTaskButton.test.js', () => {
+  describe('DeleteTaskButton', () => {
+    it('should display DeleteTaskBu', () => {
       const wrapper = mount(<DeleteTaskButton />);
       const preventDefaultSpy = sinon.spy();
 
@@ -21,7 +20,7 @@ describe.only("src/components/DeleteTaskButton/__test__/DeleteTaskButton.test.js
         .props()
         .onClick({ preventDefault: preventDefaultSpy });
 
-      expect(preventDefaultSpy).to.have.been.calledOnce;     
+      expect(preventDefaultSpy).to.have.been.calledOnce;
     });
     //@TODO: Must figure out how to test the `fetch` pieces.
   });
