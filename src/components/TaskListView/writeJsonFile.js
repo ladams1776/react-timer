33899e575301acc957bb0493caef48165e782b60
larry.adams <ladams1776@yahoo.com>
2019-11-24 21:50:05 -0500

    Fix: the files names that were being written out
    At one point this was refactored from single json projects to an
    aggregate of notes on projects into one json file
    and we did not take that into account when
    we wrote the file's name down.

--- src/components/TaskListView/writeJsonFile.js
+++ src/components/TaskListView/writeJsonFile.js
@@ -3,7 +3,6 @@ var FileSaver = require('file-saver');
 export default function writeJsonFile(taskBundle) {
   let json = JSON.stringify(taskBundle);
   let blob = new Blob([json], { type: 'application/json' });
-  let fileName =
-    new Date().toString() + '_' + taskBundle.description + '_' + taskBundle.time + '.json';
+  let fileName = 'time-logs_' + taskBundle.date + '.json';
   FileSaver.saveAs(blob, fileName);
 }
