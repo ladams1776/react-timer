11c0d534f2c89a5bc3e4c9de56ef1560fae140ed
larry adams <ladams1776@yahoo.com>
2019-11-28 09:33:14 -0500

    Stop exporting default and just export
    for spyOn purposes in ControlButtons test

--- src/components/TaskListView/writeJsonFile.js
+++ src/components/TaskListView/writeJsonFile.js
@@ -1,6 +1,10 @@
 var FileSaver = require('file-saver');
 
-export default function writeJsonFile(taskBundle) {
+/**
+ * Not exporting default, so we can spyOn
+ * @param {Object} taskBundle
+ */
+export function writeJsonFile(taskBundle) {
   let json = JSON.stringify(taskBundle);
   let blob = new Blob([json], { type: 'application/json' });
   let fileName = 'time-logs_' + taskBundle.date + '.json';
