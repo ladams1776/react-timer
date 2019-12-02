a36cfd029408a2c41aa915cb9e8c642149372017
larry.adams <ladams1776@yahoo.com>
2019-12-01 21:37:45 -0500

    Update Task.test.js

--- src/components/TaskListView/Task/__test__/Task.test.js
+++ src/components/TaskListView/Task/__test__/Task.test.js
@@ -4,7 +4,7 @@ import chai from 'chai';
 import SinonChai from 'sinon-chai';
 import * as useTaskEditContext from '../../../../Form/EditTask/useTaskEditContext';
 import Task from '../Task';
-import Enzyme, { mount, shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
