0d78546a949a5400041eca278b92e550057f7615
larry adams <ladams1776@yahoo.com>
2019-12-14 12:13:46 -0500

    Update CreateWrapperWithContext
    so there is less boilerplate when using it

--- src/testUtils/createWrapperWithContext.js
+++ src/testUtils/createWrapperWithContext.js
@@ -1,13 +1,17 @@
-import { shallow } from "enzyme";
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import * as useTaskEditContext from '../Form/EditTask/hooks/useTaskEditContext';
 
+let stuber;
+stuber = sinon.stub(useTaskEditContext, 'default');
 /**
  *
  * @param {function} component React Component
  * @param {Object} mockContext Context for the React Component for the test
  * @param {Object} sinonStub a Sinon stub wrapping the context
  */
-const createWrapperWithContext = (component, mockContext, sinonStub) => {
-  sinonStub.returns(mockContext);
+const createWrapperWithContext = (component, mockContext) => {
+  stuber.returns(mockContext);
   return shallow(component);
 };
 
