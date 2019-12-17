05b1daef70a55fc47452bbd020a0813a4919e78b
larry.adams <ladams1776@yahoo.com>
2019-12-16 20:26:57 -0500

    Add comprehensive test for DeleteTaskButton

--- src/pages/home/TaskListView/Task/__test__/DeleteTaskButton.test.js
+++ src/pages/home/TaskListView/Task/__test__/DeleteTaskButton.test.js
@@ -1,27 +1,57 @@
-import React from 'react';
-import sinon from 'sinon';
-import chai, { expect } from 'chai';
-import SinonChai from 'sinon-chai';
-import DeleteTaskButton from '../DeleteTaskButton';
-import Enzyme, { mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import { createWrapperWithContext, findByTestId } from "testUtils";
+import DeleteTaskButton from "../DeleteTaskButton";
 
-Enzyme.configure({ adapter: new Adapter() });
-chai.use(SinonChai);
+describe("src/pages/home/TaskListView/Task/__test__/DeleteTaskButton/__test__/DeleteTaskButton.test.js", () => {
+  let wrapper;
+  describe("#_deleteClick", () => {
+    describe("DeleteTaskButton", () => {
+      it("should display DeleteTaskButton", () => {
+        const wrapper = createWrapperWithContext(
+          <DeleteTaskButton taskId={"stubTaskID"} />,
+          {}
+        );
 
-describe('src/components/DeleteTaskButton/__test__/DeleteTaskButton.test.js', () => {
-  describe('DeleteTaskButton', () => {
-    it('should display DeleteTaskBu', () => {
-      const wrapper = mount(<DeleteTaskButton />);
-      const preventDefaultSpy = sinon.spy();
+        expect(wrapper.length).toEqual(1);
+      });
 
-      wrapper
-        .find("[data-test-id='delete-task-button']")
-        .props()
-        .onClick({ preventDefault: preventDefaultSpy });
+      describe("#_deleteClick", () => {
+        it("should display DeleteTaskButton", async () => {
+          const preventDefaultSpy = jest.fn().mockImplementation();
 
-      expect(preventDefaultSpy).to.have.been.calledOnce;
+          global.fetch = jest.fn().mockImplementation(() =>
+            Promise.resolve({
+              json: jest.fn().mockImplementation(() => Promise.resolve())
+            })
+          );
+
+          global.window.location.reload = jest.fn().mockImplementation();
+
+          jest.useFakeTimers(); // Need to declare we are using setTimeout
+
+          const context = {
+            setMessage: jest.fn().mockImplementation()
+          };
+
+          const wrapper = createWrapperWithContext(
+            <DeleteTaskButton taskId={"stubTaskID"} />,
+            context
+          );
+
+          const response = await wrapper
+            .find(findByTestId("delete-task-button"))
+            .props()
+            .onClick({ preventDefault: preventDefaultSpy });
+
+          jest.runTimersToTime(500); // speed up the time on the setTimeout
+
+          expect(preventDefaultSpy).toBeCalled();
+          expect(context.setMessage).toBeCalledWith(
+            "Successfully Deleted Task with id of stubTaskID"
+          );
+          expect(global.window.location.reload).toBeCalled();
+        });
+      });
     });
-    //@TODO: Must figure out how to test the `fetch` pieces.
   });
 });
