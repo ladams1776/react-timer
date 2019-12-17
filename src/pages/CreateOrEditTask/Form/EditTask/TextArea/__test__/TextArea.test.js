708404ff44b6d4c75b0743195db1d2374e5ebded
larry.adams <ladams1776@yahoo.com>
2019-12-16 20:26:15 -0500

    Deleted unnecessary imports

--- src/pages/CreateOrEditTask/Form/EditTask/TextArea/__test__/TextArea.test.js
+++ src/pages/CreateOrEditTask/Form/EditTask/TextArea/__test__/TextArea.test.js
@@ -1,49 +1,43 @@
-import React from 'react';
-import sinon from 'sinon';
-import { findByTestId, createWrapperWithContext } from 'testUtils';
-import chai, { expect } from 'chai';
-import SinonChai from 'sinon-chai';
-import TextArea from '../TextArea';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import sinon from "sinon";
+import { findByTestId, createWrapperWithContext } from "testUtils";
+import { expect } from "chai";
+import TextArea from "../TextArea";
 
-Enzyme.configure({ adapter: new Adapter() });
-chai.use(SinonChai);
-
-describe.only('src/Form/EditTask/TextArea/__test__/TextArea.test.js', () => {
+describe("src/Form/EditTask/TextArea/__test__/TextArea.test.js", () => {
   let wrapper;
 
-  describe('TextArea', () => {
-    it('should display a count of characters that is in the description', () => {
+  describe("TextArea", () => {
+    it("should display a count of characters that is in the description", () => {
       const context = {
-        description: 'stub description',
+        description: "stub description",
         updateDescription: sinon.spy()
       };
 
       wrapper = createWrapperWithContext(<TextArea />, context);
 
       const descriptionCountSpan = wrapper.find(
-        findByTestId('description-count')
+        findByTestId("description-count")
       );
 
-      expect(descriptionCountSpan.text()).to.equal('16 character(s)');
+      expect(descriptionCountSpan.text()).to.equal("16 character(s)");
     });
 
-    describe('onChange', () => {
+    describe("onChange", () => {
       it("should call updateDescription with the text area's value, when onChange is called", () => {
         const context = {
-          description: 'stub description',
+          description: "stub description",
           updateDescription: sinon.spy()
         };
 
         wrapper = createWrapperWithContext(<TextArea />, context);
 
         wrapper
-          .find(findByTestId('description-textarea'))
+          .find(findByTestId("description-textarea"))
           .props()
-          .onChange({ target: { value: 'stub description + 1' } });
+          .onChange({ target: { value: "stub description + 1" } });
         expect(context.updateDescription).calledOnceWith(
-          'stub description + 1'
+          "stub description + 1"
         );
       });
     });
