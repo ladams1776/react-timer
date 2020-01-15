213130b6e026e814701e55704e939ab5150428d2
ladams1776 <ladams1776@yahoo.com>
2020-01-14 22:07:42 -0500

    Stopped here - actually need this
    for the rest of the commits coming

---
+++ src/hooks/__test__/useFetchProjectOptions.test.js
@@ -0,0 +1,9 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+//@TODO: Leaving off here
+describe('src/hooks/__test__/useFetchProjectOptions.test.js', () => {
+    describe('#useFetchProjectOptions', () => {
+        it('should return project objects', () => {
+
+        });
+    });
+});
---
+++ src/hooks/useFetchProjectOptions.js
@@ -0,0 +1,7 @@
+const projectOptions = require('./projects.json');
+
+const useFetchProjectOptions = () => {
+    return projectOptions;
+};
+
+export default useFetchProjectOptions;
