d3dbc98d85903efb9e7c7780696a4250219c27fb
ladams1776 <ladams1776@yahoo.com>
2020-01-18 16:09:46 -0500

    remove reference to old form

--- src/pages/CreateOrEditTask/CreateOrEditTaskPage.js
+++ src/pages/CreateOrEditTask/CreateOrEditTaskPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import EditTaskForm from './Form/EditTask/EditTaskForm';
 import TaskForm from './newForm/TaskForm';
 
 const CreateOrEditTaskPage = props => <TaskForm {...props} />;
