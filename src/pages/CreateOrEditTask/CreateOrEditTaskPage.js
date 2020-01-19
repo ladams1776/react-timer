c2100a4a34b25225e7468faf84fec3ecd83a4d2d
ladams1776 <ladams1776@yahoo.com>
2020-01-19 17:05:56 -0500

    Moved the fetching of the task up in the Page
    Not sure if I will keep it there

--- src/pages/CreateOrEditTask/CreateOrEditTaskPage.js
+++ src/pages/CreateOrEditTask/CreateOrEditTaskPage.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import TaskForm from './newForm/TaskForm';
+import useFetchTaskById from './newForm/useFetchTaskById';
 
-const CreateOrEditTaskPage = props => <TaskForm {...props} />;
+const CreateOrEditTaskPage = ({ match }) => {
+    const taskId = match?.params?.id || "-1"
+    useFetchTaskById(taskId);
+
+    return <TaskForm />
+};
 
 export default CreateOrEditTaskPage;
