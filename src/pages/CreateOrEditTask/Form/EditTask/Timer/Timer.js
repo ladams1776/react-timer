13bb66a225686ad72e6a5bd4178116d56f6b3f7d
ladams1776 <ladams1776@yahoo.com>
2020-01-19 21:41:44 -0500

    useEffect for Timer component

--- src/pages/CreateOrEditTask/Form/EditTask/Timer/Timer.js
+++ src/pages/CreateOrEditTask/Form/EditTask/Timer/Timer.js
@@ -1,60 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ms from 'pretty-ms';
 import useTaskEditContext from 'hooks/useTaskEditContext';
 import { displayMsInFractionalHourFormat } from 'utils';
 import './Timer.css';
 
+
+const status = {
+  ON: 'ON',
+  OFF: 'OFF',
+  RESET: 'RESET'
+}
+
 const Timer = () => {
   const { time, updateTime } = useTaskEditContext();
-  const [isOn, setIsOn] = useState(false);
+  const [currentTimerStatus, setCurrentTimerStatus] = useState(status.OFF);
   const [timer, setTimer] = useState(null);
 
+  //@TODO: Still not cleaning up if the timer is on and we back out
+  useEffect(() => {
+    switch (currentTimerStatus) {
+      case status.ON:
+        let timeOffset = Date.now() - time;
+        setTimer(setInterval(() => updateTime(Date.now() - timeOffset), 1));
+        break;
+      case status.OFF:
+        timer && clearInterval(timer);
+        updateTime(time);
+        break;
+      case status.RESET:
+        updateTime(0);
+        setCurrentTimerStatus(status.OFF)
+        break;
+    }
+
+    return () => setCurrentTimerStatus(status.ON)
+  }, [currentTimerStatus]);
+
+
   const startTimer = () => {
-    setIsOn(true);
-    let timeOffset = Date.now() - time;
-    setTimer(setInterval(() => updateTime(Date.now() - timeOffset), 1));
+    setCurrentTimerStatus(status.ON);
   };
 
-  //@TODO: Need to stop the timer when we leave the comps
-
   const stopTimer = e => {
-    setIsOn(false);
-    clearInterval(timer);
-    updateTime(time);
+    setCurrentTimerStatus(status.OFF);
   };
 
   const resetTimer = () => {
-    stopTimer();
-    updateTime(0);
-    setIsOn(false);
+    setCurrentTimerStatus(status.RESET);
   };
 
-  let startTime =
-    time === 0 && !isOn ? (
-      <button className="timer__start" onClick={startTimer}>
-        start
-      </button>
-    ) : null;
-
-  let stopTime = isOn ? (
-    <button className="timer__stop" onClick={stopTimer}>
-      stop
-    </button>
-  ) : null;
-
-  let resumeTime =
-    time !== 0 && !isOn ? (
-      <button className="timer__resume" onClick={startTimer}>
-        resume
-      </button>
-    ) : null;
-
-  let resetTime =
-    isOn || time > 0 ? (
-      <button className="timer__reset" onClick={resetTimer}>
-        reset
-      </button>
-    ) : null;
 
   return (
     <div className="timer">
@@ -65,10 +59,25 @@ const Timer = () => {
         </div>
 
         <div className="timer__buttons">
-          {startTime}
-          {resumeTime}
-          {stopTime}
-          {resetTime}
+          {time === 0 && currentTimerStatus === status.OFF &&
+            (<button className="timer__start" onClick={startTimer}>
+              start
+          </button>)}
+
+          {time !== 0 && currentTimerStatus == status.OFF && (
+            <button className="timer__resume" onClick={startTimer}>
+              resume
+            </button>
+          )}
+
+          {currentTimerStatus === status.ON &&
+            (<button className="timer__stop" onClick={stopTimer}>
+              stop
+          </button>)}
+
+          {timer > 0 && <button className="timer__reset" onClick={resetTimer}>
+            reset
+          </button>}
         </div>
       </div>
     </div>
