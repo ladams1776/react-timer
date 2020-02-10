dbb88596a6beaacaf6f9f342d57b55ec4e0d1dc3
ladams1776 <ladams1776@yahoo.com>
2020-02-09 20:32:51 -0500

    Register the new hook in the index file

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useFetchProjectOptions } from './useFetchProjectOptions';
 export { default as useTaskEditContext } from './useTaskEditContext';
+export { default as useBackButtonListener} from './useBackButtonListener';
