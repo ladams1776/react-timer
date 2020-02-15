b3a5ab1fdd807a51c78d7e9b8aa989678dca50d8
ladams1776 <ladams1776@yahoo.com>
2020-02-15 07:48:20 -0500

    Test the new hook that fetches the options

--- src/hooks/__test__/useFetchProjectOptions.test.js
+++ src/hooks/__test__/useFetchProjectOptions.test.js
@@ -1,9 +1,12 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-//@TODO: Leaving off here
+import useFetchProjectOptions from '../useFetchProjectOptions';
+const projectOptions = require('../projects.json');
+
 describe('src/hooks/__test__/useFetchProjectOptions.test.js', () => {
     describe('#useFetchProjectOptions', () => {
         it('should return project objects', () => {
-
+            const { result } = renderHook(() => useFetchProjectOptions());
+            expect(result.current).toEqual(projectOptions);
         });
     });
 });
