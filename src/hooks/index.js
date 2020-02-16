359f2c98576c9503b4f8e6c2fad838e95cb85b39
ladams1776 <ladams1776@yahoo.com>
2020-02-16 09:16:53 -0500

    Export the new hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,5 @@
 export { default as useFetchProjectOptions } from './useFetchProjectOptions';
+export { default as useGetProjectOptionLabel } from './useGetProjectOptionLabel';
 export { default as useTaskEditContext } from './useTaskEditContext';
 export { default as useBackButtonListener } from './useBackButtonListener';
 export { default as useFetchTaskById } from './useFetchTaskById';
