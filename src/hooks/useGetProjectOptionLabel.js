4d8cc2293fc629ec4588b49ca6a9774c3638cdea
ladams1776 <ladams1776@yahoo.com>
2020-02-16 09:17:39 -0500

    Helps if we just number parse the values, incase

--- src/hooks/useGetProjectOptionLabel.js
+++ src/hooks/useGetProjectOptionLabel.js
@@ -1,9 +1,9 @@
 import useFetchProjectOptions from './useFetchProjectOptions';
 
-//@TODO: Need to test this
 const useGetProjectOptionLabel = taskProjectOptionId => {
     const projectOptions = useFetchProjectOptions();
-    return projectOptions.filter(project => project.value === taskProjectOptionId)[0].label;
+    const projectOptionLabels = projectOptions.filter(project => Number.parseInt(project.value) === taskProjectOptionId);
+    return projectOptionLabels[0].label;
 }
 
 export default useGetProjectOptionLabel;
