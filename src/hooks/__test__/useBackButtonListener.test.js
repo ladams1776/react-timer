fb478994b4c9f78753909a4d7ec20feb6f1883c9
ladams1776 <ladams1776@yahoo.com>
2020-02-17 14:36:17 -0500

    Refactor: Clean up the it comments
    I meant to circle back on this at some point.

--- src/hooks/__test__/useBackButtonListener.test.js
+++ src/hooks/__test__/useBackButtonListener.test.js
@@ -3,21 +3,20 @@ import useBackButtonListener from '../useBackButtonListener';
 import useBackButtonlistener from '../useBackButtonListener';
 
 describe('src/hooks/__test__/useBackButtonListener.test.js', () => {
-    //@TODO: LEFT OFF HERE
-    // 1. Gert the comment wording correct
-    // 2. Get the correct function for the second test
-
     describe('#useBackButtonListener', () => {
+        // Mocking the browser's back function
         global.onpopstate = jest.fn();
-        it("should call 'replace' on 'history', if 'history's action is 'POP'", () => {
-            const history = { replace: jest.fn(), action: 'POP' };
 
-            const { result } = renderHook(() => useBackButtonlistener(history));
-            global.onpopstate();
-            expect(history.replace).toHaveBeenNthCalledWith(1, '/');
-        });
+        it("should invoke 'replace' on 'history', if 'history's action is 'POP'. " +
+            " 'POP' is a react-router convention, adopted by browsers conventions.", () => {
+                const history = { replace: jest.fn(), action: 'POP' };
 
-        it("should not call 'replace' on 'history', if 'history's action is not 'POP'", () => {
+                renderHook(() => useBackButtonlistener(history));
+                global.onpopstate();
+                expect(history.replace).toHaveBeenNthCalledWith(1, '/');
+            });
+
+        it("should not invoke 'replace' on 'history', if 'history's action is not 'POP'", () => {
             const history = { replace: jest.fn(), action: 'NOT_POP' };
 
             const { result } = renderHook(() => useBackButtonlistener(history));
