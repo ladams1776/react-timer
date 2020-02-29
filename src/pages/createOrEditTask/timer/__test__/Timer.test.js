ab53c3fe68f3d3790042e49d953c4a0359a2d25c
larry.adams <larry.adams@echobh.com>
2020-02-28 20:08:50 -0500

    begin working on the test for timer

--- src/pages/createOrEditTask/timer/__test__/Timer.test.js
+++ src/pages/createOrEditTask/timer/__test__/Timer.test.js
@@ -1,4 +1,13 @@
-//@TODO: About to do this, but breaking out the hook first.
-describe('Write me', () => {
-    it('fill me');
+import { renderHook, act } from '@testing-library/react-hooks';
+import Timer from '../Timer';
+import { displayMsInFractionalHourFormat } from 'utils';
+import * as useUpdateCurrentTime from './hooks/useUpdateCurrentTime';
+import { render } from '@testing-library/react';
+
+describe('src/pages/createOrEditTask/timer/__test__/Timer.test.js', () => {
+    describe('Timer', () => {
+        it('', () => {
+            
+        });
+    });
 });
