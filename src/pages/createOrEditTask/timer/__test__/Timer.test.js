91f75599cbeaa37b083c423c868a88627fa39187
Larry Adams <ladams@Laurens-Air.lan1>
2020-03-07 21:49:26 -0500

    Fill in tons of tests for Timer component

--- src/pages/createOrEditTask/timer/__test__/Timer.test.js
+++ src/pages/createOrEditTask/timer/__test__/Timer.test.js
@@ -1,35 +1,79 @@
 import React from 'react';
-import Timer from '../Timer';
 import ms from 'pretty-ms';
-import displayMsInFractionalHourFormat from 'utils/displayMsInFractionalHourFormat';
-import { createWrapperWithContext, findByTestId } from "testUtils";
-// import * as useUpdateCurrentTime from './hooks/useUpdateCurrentTime';
-import { render } from '@testing-library/react';
-
-import sinon from 'sinon';
-import Enzyme, { shallow } from 'enzyme';
-import chai from 'chai';
-import SinonChai from 'sinon-chai';
-import Adapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new Adapter() });
-chai.use(SinonChai);
+import { displayMsInFractionalHourFormat } from 'utils';
+import { createWrapperWithContext, findByTestId } from 'testUtils';
+import Timer from '../Timer';
 
 describe.only('src/pages/createOrEditTask/timer/__test__/Timer.test.js', () => {
-    describe('Timer', () => {
-        let wrapper;
+  describe('Timer', () => {
+    let wrapper;
 
-        it('should display the time in the right format', () => {
-            const context = {
-                isActive: false,
-                setIsActive: sinon.spy()
-            };
-            const time = 1000;
-            const setTimeSpy = jest.fn().mockImplementation();
-            wrapper = createWrapperWithContext(<Timer time={time} setTime={setTimeSpy} />, context);
-            expect(wrapper.find(findByTestId("timer__display__content")).text()).toEqual(`${ms(time)} - hours: ${displayMsInFractionalHourFormat(time)}`);
+    it('should display the time in the right format', () => {
+      const context = {
+        isActive: false,
+        setIsActive: jest.fn().mockImplementation(),
+      };
+      const time = 1000;
+      const setTimeSpy = jest.fn().mockImplementation();
+
+      wrapper = createWrapperWithContext(
+        <Timer time={time} setTime={setTimeSpy} />,
+        context,
+      );
+
+      expect(
+        wrapper.find(findByTestId('timer__display__content')).text(),
+      ).toEqual(
+        `${ms(time)} - hours: ${displayMsInFractionalHourFormat(time)}`,
+      );
+    });
+
+    describe('timerButtons', () => {
+      describe('reset button', () => {
+        it("should be disabled, if time equals '0' ", () => {
+          wrapper = createWrapperWithContext(<Timer time={0} />);
+          var timerResetButton = wrapper.find(findByTestId('timerReset'));
+          expect(timerResetButton.prop('disabled')).toBeTruthy();
         });
 
-        //@TODO: We want to test hte buttons
+        it("reset button should be enabled, if time is equal to anything BUT '0' ", () => {
+          wrapper = createWrapperWithContext(<Timer time={1} />);
+          const timerResetButton = wrapper.find(findByTestId('timerReset'));
+          expect(timerResetButton.prop('disabled')).toBeFalsy();
+        });
+
+        describe('onClick', () => {
+          it("should call 'reset()'", () => {
+            const stubTime = 1000;
+            const spySetTime = jest.fn().mockImplementation();
+            const spySetIsActive = jest.fn().mockImplementation();
+
+            wrapper = createWrapperWithContext(
+              <Timer
+                time={stubTime}
+                setTime={spySetTime}
+                setIsActive={spySetIsActive}
+              />,
+            );
+
+            wrapper
+              .find(findByTestId('timerReset'))
+              .props()
+              .onClick();
+
+            expect(spySetTime).toBeCalledWith(0);
+            expect(spySetIsActive).toBeCalledWith(false);
+          });
+        });
+      });
+
+      describe('stop button', () => {
+        describe('onClick', () => {
+          it("should call 'setIsActive' ", () => {
+            //@TODO: Left off here
+          });
+        });
+      });
     });
+  });
 });
