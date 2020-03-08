91748ce7b3a6aa825c4d75f0b8aba8aeb4491875
Larry Adams <ladams@Laurens-Air.lan1>
2020-03-07 21:48:17 -0500

    Reformat CreateOrEditTaskPage comp

--- src/pages/createOrEditTask/CreateOrEditTaskPage.js
+++ src/pages/createOrEditTask/CreateOrEditTaskPage.js
@@ -3,15 +3,13 @@ import EditTaskForm from './form/EditTaskForm';
 import AddTaskForm from './form/AddTaskForm';
 
 const CreateOrEditTaskPage = ({ match }) => {
-    const taskId = match?.params?.id;
+  const taskId = match?.params?.id;
 
-    if (taskId && taskId !== "-1") {
-        return <EditTaskForm taskId={taskId} />
-    } else {
-        return <AddTaskForm />
-    }
-
-
+  if (taskId && taskId !== '-1') {
+    return <EditTaskForm taskId={taskId} />;
+  } else {
+    return <AddTaskForm />;
+  }
 };
 
 export default CreateOrEditTaskPage;
