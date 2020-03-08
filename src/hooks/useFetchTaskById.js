b2a5497a7fc0b568bd6fbfc45752920db097e5b4
Larry Adams <ladams@Laurens-Air.lan1>
2020-03-07 21:48:33 -0500

    Reformat useFetchTaskById hook

--- src/hooks/useFetchTaskById.js
+++ src/hooks/useFetchTaskById.js
@@ -1,25 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useTaskEditContext } from 'hooks';
 
 const useFetchTaskById = (taskId, setTime) => {
-    const { updateTask } = useTaskEditContext();
+  const { updateTask } = useTaskEditContext();
 
-    return useEffect(() => {
-        if (taskId !== "-1") {
-            fetch("/api/task/" + taskId)
-                .then(response => {
-                    return response.json();
-                })
-                .then(task => {
-                    setTime(task.time);
-                    updateTask(task);
-                }).catch(error => {
-                    //@TODO: Could add some sort of flag to change oclor to red.
-                    // setMessage(`Error: ${error}`);
-                    updateTask({});
-                })
-        }
-    }, [taskId]);
+  return useEffect(() => {
+    if (taskId !== '-1') {
+      fetch('/api/task/' + taskId)
+        .then(response => {
+          return response.json();
+        })
+        .then(task => {
+          setTime(task.time);
+          updateTask(task);
+        })
+        .catch(error => {
+          //@TODO: Could add some sort of flag to change oclor to red.
+          // setMessage(`Error: ${error}`);
+          updateTask({});
+        });
+    }
+  }, [taskId]);
 };
 
-export default useFetchTaskById
+export default useFetchTaskById;
