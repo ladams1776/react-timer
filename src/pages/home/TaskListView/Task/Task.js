a05278e395ba6778fe982da2d1b53eb9cd7cc894
Larry Adams <ladams1776@yahoo.com>
2020-03-12 23:20:47 -0400

    Change style on the individual tasks in the TaskListView
    Also cut down on some stylistic stuff

--- src/pages/home/TaskListView/Task/Task.js
+++ src/pages/home/TaskListView/Task/Task.js
@@ -2,48 +2,36 @@ import React from 'react';
 import PropType from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import DeleteTaskButton from './DeleteTaskButton';
-import { useGetProjectOptionLabel  } from 'hooks';
+import { useGetProjectOptionLabel } from 'hooks';
 import { getFormattedDate, displayMsInFractionalHourFormat } from 'utils';
 import './Task.css';
 
 const Task = ({ task }) => {
   const { _id, description = '', contractId, time, date } = task;
   const projectOptionLabel = useGetProjectOptionLabel(contractId);
+  const displayableDescription = description
+    .split('\n')
+    .map(paragraph => paragraph);
 
   return (
     <NavLink to={'/task/' + _id} id={_id} className="task-item">
       <div className="task-item__left">
-        <div className="task-item__description">
-          {description.split('\n').map((paragraph, index) => {
-            return (
-              <p key={index} className="task-item__description-item">
-                {paragraph}
-              </p>
-            );
-          })}
+        <div className="task-item__description">{displayableDescription}</div>
+        <span className="task-item__customer">{projectOptionLabel}</span>
+        <div className="task-item__date-time">
+          <span className="block">Date: {getFormattedDate(date)}</span>
+          <span className="block">
+            Hours: {displayMsInFractionalHourFormat(time)}
+          </span>
         </div>
-        <span className="task-item__customer">
-          {projectOptionLabel}
-        </span>
       </div>
-
-      <span className="task-item__time">
-        <span className="task-item__time-label">Time:</span>
-        <span className="task-item__time-value">
-          {' '}
-          {displayMsInFractionalHourFormat(time)}
-          <DeleteTaskButton taskId={_id} />
-        </span>
-        <span className="task-item__item-date">
-          {getFormattedDate(date)}
-        </span>
-      </span>
+      <DeleteTaskButton taskId={_id} />
     </NavLink>
   );
 };
 
 Task.PropType = {
-  task: PropType.object
+  task: PropType.object,
 };
 
 export default Task;
