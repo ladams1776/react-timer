259847c763eb4629f39088d26d5f6dedd3c0b8f8
Larry Adams <ladams1776@yahoo.com>
2020-03-14 08:04:42 -0400

    Unused import removed

--- src/pages/createOrEditTask/timer/Timer.js
+++ src/pages/createOrEditTask/timer/Timer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ms from 'pretty-ms';
 import { displayMsInFractionalHourFormat } from 'utils';
 import { useUpdateCurrentTime } from './hooks';
