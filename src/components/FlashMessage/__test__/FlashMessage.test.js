266ba1c604940e030708e370dab4258c594bf2de
Larry Adams <ladams1776@yahoo.com>
2020-03-15 09:43:06 -0400

    Clean up FlashMessage test's boilerplate

--- src/components/FlashMessage/__test__/FlashMessage.test.js
+++ src/components/FlashMessage/__test__/FlashMessage.test.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import sinon from 'sinon';
-import chai from 'chai';
-import SinonChai from 'sinon-chai';
-import Enzyme, { mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { createWrapperWithContext, findByTestId } from 'testUtils';
+import { createWrapperWithContext } from 'testUtils';
 import FlashMessage from '../FlashMessage';
-Enzyme.configure({ adapter: new Adapter() });
-chai.use(SinonChai);
 
 describe('src/components/FlashMessage/__test__/FlashMessage.test.js', () => {
   let wrapper;
   let context;
 
   describe('FlashMessage', () => {
-
     it('should not show FlashMessage when no message is present.', () => {
       context = {
-        message: undefined
+        message: undefined,
       };
 
       wrapper = createWrapperWithContext(<FlashMessage />, context);
@@ -26,22 +18,21 @@ describe('src/components/FlashMessage/__test__/FlashMessage.test.js', () => {
 
     it('should show FlashMessage when message has a value.', () => {
       context = {
-        message: 'Success'
+        message: 'Success',
       };
 
       wrapper = createWrapperWithContext(<FlashMessage />, context);
 
       expect(wrapper.text()).toEqual('SuccessX');
-    }); 
+    });
   });
 
   describe('onClick', () => {
     it('should set the message on the FlashMessage to null', () => {
       context = {
         message: 'Success',
-        setMessage: jest.fn()
+        setMessage: jest.fn(),
       };
-
 
       wrapper = createWrapperWithContext(<FlashMessage />, context);
 
