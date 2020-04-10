bcfbd1ba7ff26ea313453a8cb048e6fbf5c1a651
larry adams <ladams1776@yahoo.com>
2020-04-10 18:41:15 -0400

    Add slight style for ProjectDropDown
    Since we are adjusting the styling on the form

--- src/pages/createOrEditTask/projectDropdown/ProjectDropdown.js
+++ src/pages/createOrEditTask/projectDropdown/ProjectDropdown.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Field } from 'react-final-form';
 import { useFetchProjectOptions } from 'hooks';
+import styles from './ProjectDropdown.module.css';
+
 //@TODO: Make test for this
 const ProjectDropDown = () => {
     const projectOptions = useFetchProjectOptions();
 
-    return <div className="dropDown">
+    return <div className={styles.dropDown}>
         <Field name="selectedProject" component="select" >
             {projectOptions.map(project => <option value={project.value} key={project.value}>{project.label}</option>)}
         </Field>
