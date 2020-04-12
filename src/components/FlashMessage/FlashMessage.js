5cca3c14ce7fb3738c903b7082a62a56d6a294e7
Larry Adams <ladams1776@yahoo.com>
2020-04-12 11:00:07 -0400

    Work on the styles for the FlashMessage
    Fix the size
    Change the time it takes to fade away
    Add glyphicons

--- src/components/FlashMessage/FlashMessage.js
+++ src/components/FlashMessage/FlashMessage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import classnames from 'classnames';
+import cn from 'classnames';
 import useFlashMessageContext from 'hooks/useFlashMessageContext';
 import './FlashMessage.css';
 
@@ -9,27 +9,32 @@ const FlashMessage = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       resetFlashMessage()
-    }, 5000);
+    }, 10000);
 
     return () => clearTimeout(timer);
   }, [message, resetFlashMessage]);
 
   return (
     !message || (
-      <div className={classnames('flashMessage', 'fade-out', {
+      <div className={cn('flashMessage', 'fade-out', {
         success,
         info,
         error
       })}
         test-data-id="flash-flashMessage"
         onClick={() => resetFlashMessage()}>
-
+        {/* <div className="innerFlashMessage">   */}
+        <div className={cn("flashMessageIcon", { 
+          "glyphicon glyphicon-ok-sign": success,
+          "glyphicon glyphicon-exclamation-sign": error,
+          "glyphicon glyphicon-info-sign": info,
+        })}/>
         <div className="flashMessageContent"
           test-data-id="flash-message_message">
           {message}
         </div>
-
-        <div className="flashMessageCancel">X</div>
+        <div className={cn("flashMessageCancel", "glyphicon glyphicon-remove-circle")}/>
+        {/* </div> */}
       </div>
     )
   );
