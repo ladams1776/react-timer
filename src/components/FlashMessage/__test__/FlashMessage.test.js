7f2d1f745685617e6c78606fb5687c92457c7991
Larry Adams <ladams1776@yahoo.com>
2020-04-12 19:10:11 -0400

    Fix the test for FlashMessage

--- src/components/FlashMessage/__test__/FlashMessage.test.js
+++ src/components/FlashMessage/__test__/FlashMessage.test.js
@@ -1,44 +1,74 @@
 import React from 'react';
 import { createWrapperWithContext } from 'testUtils';
 import FlashMessage from '../FlashMessage';
+import useFlashMessageContext from 'hooks/useFlashMessageContext';
+import useFadeOutFlashMessage from '../hooks/useFadeOutFlashMessage';
+
+jest.mock('hooks/useFlashMessageContext');
+jest.mock('../hooks/useFadeOutFlashMessage');
 
 describe('src/components/FlashMessage/__test__/FlashMessage.test.js', () => {
   let wrapper;
   let context;
 
-  describe('FlashMessage', () => {
-    it('should not show FlashMessage when no message is present.', () => {
-      context = {
-        message: undefined,
-      };
-
-      wrapper = createWrapperWithContext(<FlashMessage />, context);
-      expect(wrapper).tobeNull;
-    });
-
-    it('should show FlashMessage when message has a value.', () => {
-      context = {
-        message: 'Success',
-      };
-
-      wrapper = createWrapperWithContext(<FlashMessage />, context);
-
-      expect(wrapper.text()).toEqual('SuccessX');
-    });
+  beforeEach(() => {
+    useFadeOutFlashMessage.mockClear();
   });
 
-  describe('onClick', () => {
-    it('should set the message on the FlashMessage to null', () => {
+  describe('FlashMessage', () => {
+    it("should not show FlashMessage when 'message' is NOT present.", () => {
+      // arrange
+      context = {
+        message: undefined,
+        resetFlashMessage: jest.fn()
+      };
+      useFlashMessageContext.mockReturnValue(context);
+
+      // act
+      wrapper = createWrapperWithContext(<FlashMessage />);
+
+      // assert
+      expect(useFadeOutFlashMessage).toHaveBeenNthCalledWith(1);
+      expect(wrapper).tobeNull;
+    });
+
+    it('should show FlashMessage when message is present.', () => {
+      // arrange
       context = {
         message: 'Success',
-        setMessage: jest.fn(),
+        success: false,
+        info: false,
+        error: false,
+        resetFlashMessage: jest.fn()
       };
+      useFlashMessageContext.mockReturnValue(context);
 
-      wrapper = createWrapperWithContext(<FlashMessage />, context);
+      // act
+      wrapper = createWrapperWithContext(<FlashMessage />);
 
-      wrapper.props().onClick();
-      expect(context.setMessage).toHaveBeenNthCalledWith(1, null);
-      expect(wrapper).tobeNull;
+      // assert
+      expect(useFadeOutFlashMessage).toHaveBeenNthCalledWith(1);
+      expect(wrapper.text()).toEqual('Success');
+    });
+
+    describe('onClick', () => {
+      it("should call 'resetFlashMessage()', when 'FlashMessage' is clicked", () => {
+        // arrange
+        context = {
+          message: 'Success',
+          resetFlashMessage: jest.fn()
+        };
+        useFlashMessageContext.mockReturnValue(context);
+
+        wrapper = createWrapperWithContext(<FlashMessage />);
+
+        // act
+        wrapper.props().onClick();
+
+        // assert
+        expect(context.resetFlashMessage).toHaveBeenNthCalledWith(1);
+        expect(wrapper).tobeNull;
+      });
     });
   });
 });
