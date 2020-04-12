05437df6b6238ae4be1558de6db0b93a79324923
Larry Adams <ladams1776@yahoo.com>
2020-04-12 11:00:56 -0400

    Change up the Styles on the Timer component
    The timer was shifting like crazy and it was driving me nuts, from
    number to number. This change will keep the number pretty stable.

--- src/pages/createOrEditTask/timer/Timer.js
+++ src/pages/createOrEditTask/timer/Timer.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ms from 'pretty-ms';
 import { displayMsInFractionalHourFormat } from 'utils';
-import styles from './Timer.module.css';
 
 const Timer = ({ time }) => {
   return (
-    <span
-      className={styles.timer}
+    <div
       data-test-id="timer__display__content">
-      {`Hours: ${displayMsInFractionalHourFormat(time)} - ${ms(time, { verbose: true })}`}
-    </span>
+      <div >{`Hours: ${displayMsInFractionalHourFormat(time)}`}</div>
+      <div>{ms(time, { secondsDecimalDigits: 2 })}</div>
+    </div>
   );
 };
 
