aa790decd5d05d544a81d15582ff6d82006b649d
Larry Adams <ladams1776@yahoo.com>
2020-05-01 11:45:14 -0400

    Use the new Tag Context in the CreateOrEditTaskPage

--- src/pages/createOrEditTask/CreateOrEditTaskPage.js
+++ src/pages/createOrEditTask/CreateOrEditTaskPage.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import EditTaskForm from './form/EditTaskForm';
 import AddTaskForm from './form/AddTaskForm';
+import TagContextProvider from './contexts/TagContext';
 
 const CreateOrEditTaskPage = ({ match }) => {
   const taskId = match?.params?.id;
 
-  if (taskId && taskId !== '-1') {
-    return <EditTaskForm taskId={taskId} />;
-  } else {
-    return <AddTaskForm />;
-  }
+  return (<TagContextProvider>
+    {(taskId && taskId !== '-1')
+      ? <EditTaskForm taskId={taskId} />
+      : <AddTaskForm />}
+  </TagContextProvider>)
 };
 
 export default CreateOrEditTaskPage;
