24f8780f703b05244007aa4230e5aa901155991f
Larry Adams <ladams1776@yahoo.com>
2020-05-01 11:44:40 -0400

    Plug TagDropdown into the new TagsContext and test it

--- src/pages/createOrEditTask/tagDropdown/TagDropdown.js
+++ src/pages/createOrEditTask/tagDropdown/TagDropdown.js
@@ -1,40 +1,17 @@
 import React from 'react';
-import { Field } from 'react-final-form';
-// import { useFetchProjectOptions } from 'hooks';
+import { MultiSelectDropdown } from 'components';
+import useTagContext from '../hooks/useTagContext';
+import useFetchTagOptions from '../hooks/useFetchTagOptions';
 import styles from './TagDropdown.module.css';
-
-const tags = [
-    {
-        _id: "1",
-        name: 'tag 1 name'
-    },
-    {
-        _id: "2",
-        name: 'tag 2 name'
-    },
-    {
-        _id: "3",
-        name: 'tag 3 name'
-    },
-    {
-        _id: "4",
-        name: 'tag 3 name'
-    },
-]
 
 //@TODO: Make test for this
 const TagDropdown = () => {
-    // const projectOptions = useFetchProjectOptions();
+    useFetchTagOptions();
+    const { tags } = useTagContext();
 
-    return <div className={styles.dropDown}>
+    return tags && <div className={styles.dropDown}>
         <div className={styles.tagsDropDown}>
-            <Field
-                name="tags"
-                component="Select"
-                displayEmpty
-                multiple>
-                {tags.map(tag => <option value={tag._id} key={tag._id}>{tag.name}</option>)}
-            </Field>
+            <MultiSelectDropdown data={tags} data-test-id="tagDropdown" />
         </div>
     </div>
 };
