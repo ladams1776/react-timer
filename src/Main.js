efcfcfd13691f967c931949fec824a49fd5beabf
Larry Adams <ladams1776@yahoo.com>
2020-05-01 11:42:31 -0400

    Move the TaskEditCormContext Provider out into the Context
    This cleans up Main.js quite a bit. No need to leak that stuff into there.

--- src/Main.js
+++ src/Main.js
@@ -5,53 +5,17 @@ import { LoadinSpinner, FlashMessage } from 'components';
 import IndexPage from 'pages/home/IndexPage';
 import TagsPage from 'pages/tags/TagsPage';
 import CreateOrEditTaskPage from 'pages/createOrEditTask/CreateOrEditTaskPage';
-import { TaskEditFormProvider } from './contexts/TaskEditFormContext';
+import TaskEditFormContextProvider from './contexts/TaskEditFormContext';
 import LoadinSpinnerContextProvider from './contexts/LoadinSpinnerContext';
 import FlashMessageContextProvider from './contexts/FlashMessageContext';
 import './Main.css';
 
 const Main = () => {
-  const [taskId, setTaskId] = useState(-1);
-  const [task, setTask] = useState({});
-  const [tasks, setTasks] = useState([]);
-  const [description, setDescription] = useState('');
-  const [selectedProject, setSelectedProject] = useState(0);
-  const [message, setMessage] = useState(null);
-  const projects = useFetchProjectOptions();
 
   return (
     <FlashMessageContextProvider>
       <LoadinSpinnerContextProvider>
-        <TaskEditFormProvider
-          value={{
-            taskId,
-            description,
-            selectedProject,
-            message,
-            projects,
-            tasks,
-            task,
-            updateTask: useCallback(task => {
-              setTask(task);
-            }, []),
-            updateTasks: useCallback(tasks => {
-              setTasks(tasks);
-            }, []),
-            updateTaskId: useCallback(taskId => {
-              setTaskId(taskId);
-            }, []),
-            updateDescription: useCallback(description => {
-              setDescription(description);
-            }, []),
-            updateDropDown: useCallback(selectedProject => {
-              setSelectedProject(selectedProject);
-            }, []),
-            //@TODO: Replace all consumers of this.
-            setMessage: useCallback(message => {
-              setMessage(message);
-            }, []),
-          }}
-        >
+        <TaskEditFormContextProvider>
           <HashRouter>
             <div className="content">
               <FlashMessage />
@@ -66,7 +30,7 @@ const Main = () => {
               <LoadinSpinner />
             </div>
           </HashRouter>
-        </TaskEditFormProvider>
+        </TaskEditFormContextProvider>
       </LoadinSpinnerContextProvider>
     </FlashMessageContextProvider>
   );
