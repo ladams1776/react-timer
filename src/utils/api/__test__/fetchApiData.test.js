db0eb2a945d8d53fc1a2048438133239683df7c5
Larry Adams <ladams1776@yahoo.com>
2020-04-30 20:26:03 -0400

    Adjust fetchApiData to take a callback
    because returning data in an async environment fails hard.

--- src/utils/api/__test__/fetchApiData.test.js
+++ src/utils/api/__test__/fetchApiData.test.js
@@ -2,39 +2,39 @@ import fetchApiData from '../fetchApiData';
 
 describe('src/utils/api/__test__/fetchApiData.test.js', () => {
   describe('fetchApiData', () => {
+    const url = 'some-route';
+    const data = {
+      id: 1
+    };
+    const dispatchSpy = jest.fn().mockImplementation();
+
+    beforeEach(() => {
+      dispatchSpy.mockReset();
+    });
+
     it('should successfully call fetch with a GET method type', async () => {
       // Arrange
-      const data = {
-        id: 1
-      };
-
       const responsePromise = Promise.resolve({
         ok: true,
         json: jest.fn().mockImplementation(() => Promise.resolve(data)),
       });
       global.fetch = jest.fn().mockImplementation(() => responsePromise);
 
-      const url = 'some-route';
       // Act
-      const actual = await fetchApiData(url, {});
+      await fetchApiData(url, {}, dispatchSpy);
 
       // Assert
-      expect(global.fetch).toBeCalledWith(`/api/${url}`, { "headers": { "Content-Type": "application/json" }, "method": "GET" });
-      expect(actual).toBe(data);
+      expect(global.fetch).toHaveBeenNthCalledWith(1, `/api/${url}`, { "headers": { "Content-Type": "application/json" }, "method": "GET" });
+      expect(dispatchSpy).toHaveBeenNthCalledWith(1, data);
     });
 
     it('should successfully call fetch with a POST method type, when passing in a body', async () => {
       // Arrange
-      const data = {
-        id: 1
-      };
-
       const responsePromise = Promise.resolve({
         ok: true,
         json: jest.fn().mockImplementation(() => Promise.resolve(data)),
       });
       global.fetch = jest.fn().mockImplementation(() => responsePromise);
-      const url = 'some-route';
       const settings = {
         body: {
           id: 'objectId'
@@ -42,27 +42,21 @@ describe('src/utils/api/__test__/fetchApiData.test.js', () => {
       };
 
       // Act
-      const actual = await fetchApiData(url, settings);
+      await fetchApiData(url, settings, dispatchSpy);
 
       // Assert
-      expect(global.fetch).toBeCalledWith(`/api/${url}`, { "body": "{\"id\":\"objectId\"}", "headers": { "Content-Type": "application/json" }, "method": "POST" });
-      expect(actual).toBe(data);
-      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(global.fetch).toHaveBeenNthCalledWith(1, `/api/${url}`, { "body": "{\"id\":\"objectId\"}", "headers": { "Content-Type": "application/json" }, "method": "POST" });
+      expect(dispatchSpy).toHaveBeenNthCalledWith(1, data);
     });
 
     it("should successfully call fetch with a GET method type, but will reject, when not a 'ok' status", async () => {
       // Arrange
-      const data = {
-        id: 1
-      };
-
       const responsePromise = Promise.resolve({
         ok: false,
         json: jest.fn().mockImplementation(() => Promise.resolve(data)),
       });
       global.fetch = jest.fn().mockImplementation(() => responsePromise);
 
-      const url = 'some-route';
       const settings = {
         body: {
           id: 'objectId'
@@ -71,12 +65,11 @@ describe('src/utils/api/__test__/fetchApiData.test.js', () => {
 
       // Act
       try {
-        await fetchApiData(url, settings);
+        await fetchApiData(url, settings, dispatchSpy);
       } catch (e) {
         // Assert
-        expect(e).toBe(data);
-        expect(global.fetch).toBeCalledWith(`/api/${url}`, { "body": "{\"id\":\"objectId\"}", "headers": { "Content-Type": "application/json" }, "method": "POST" });
-        expect(global.fetch).toHaveBeenCalledTimes(1);
+        expect(global.fetch).toHaveBeenNthCalledWith(1, `/api/${url}`, { "body": "{\"id\":\"objectId\"}", "headers": { "Content-Type": "application/json" }, "method": "POST" });
+        expect(dispatchSpy).toHaveBeenNthCalledWith(1, data);
       }
     });
   });
