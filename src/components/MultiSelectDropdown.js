ba2c5cddb0a390749a226ad235836e9cdb9d8174
Larry Adams <ladams1776@yahoo.com>
2020-05-02 01:04:08 -0400

    MultiSelect now sets the default correctly
    There was console errors being thrown by final form.
    They were concerned that we were:
    1. Not setting the type
    2. component set to 'Select' and not 'select'
    3. displayempty was missed spelt
    4. displayempty needs a default value

--- src/components/MultiSelectDropdown.js
+++ src/components/MultiSelectDropdown.js
@@ -2,15 +2,21 @@ import React from 'react';
 import PropType from 'prop-types';
 import { Field } from 'react-final-form';
 
-const MultiSelectDropdown = ({ data = [] }) => (
-    <Field
+const MultiSelectDropdown = ({ data = [] }) => {
+    const defaultValueWhenNoData = {
+        _id: 1,
+        name: 'No data'
+    }
+
+    return <Field
         name="tags"
-        component="Select"
-        displayEmpty
+        component="select"
+        type="select"
+        displayempty={defaultValueWhenNoData}
         multiple>
         {data.map(d => <option value={d._id} key={d._id} data-test-id="multi-select-dropdown">{d.name}</option>)}
     </Field>
-);
+};
 
 MultiSelectDropdown.PropType = {
     data: PropType.array,
