4302e6ad02f2d3e6051fdbac0dd7576de8d2e957
Larry Adams <ladams1776@yahoo.com>
2020-05-09 08:44:56 -0400

    Must remove the export in the hooks
    since we moved the useFretchTaskById hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,6 +2,5 @@ export { default as useFetchProjectOptions } from './useFetchProjectOptions';
 export { default as useGetProjectOptionLabel } from './useGetProjectOptionLabel';
 export { default as useTaskEditContext } from './useTaskEditContext';
 export { default as useBackButtonListener } from './useBackButtonListener';
-export { default as useFetchTaskById } from './useFetchTaskById';
 export { default as useLoadinSpinnerContext } from './useLoadinSpinnerContext';
 export { default as useFlashMessageContext } from './useFlashMessageContext';
--- src/pages/createOrEditTask/hooks/index.js
+++ src/pages/createOrEditTask/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useFetchTags } from './useFetchTags';
 export { default as useTagContext } from './useTagContext';
 export { default as useTagTransformer } from './useTagTransformer';
+export { default as useFetchTaskById } from './useFetchTaskById/useFetchTaskById';
