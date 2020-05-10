36fff9b346e9b9fc26a67d73bdac5d578137d7b8
Larry Adams <ladams1776@yahoo.com>
2020-05-10 13:48:19 -0400

    Move thr Delete button/logic to it's own component
    out of the ControlButtons, to isolate logic and make it
    easier to test the changes.

---
+++ src/pages/home/TaskListView/ControlButtons/DeleteButton.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { fetchApiData } from 'utils';
+import { useTaskEditContext, useFlashMessageContext, useLoadinSpinnerContext } from 'hooks';
+import styles from './ControlButtons.module.css';
+
+const DeleteButton = () => {
+    const { updateTasks, tasks } = useTaskEditContext();
+    const { setIsLoadin } = useLoadinSpinnerContext();
+    const { setSuccessFlashMessage } = useFlashMessageContext();
+
+    const handleDelete = e => {
+        e.preventDefault();
+        setIsLoadin(true);
+
+        const dispatch = () => {
+            setSuccessFlashMessage('Successfully deleted all tasks');
+            updateTasks([]);
+            setIsLoadin(false);
+        };
+
+        (async () => await fetchApiData('tasks', { method: 'DELETE' }, dispatch))();
+    };
+
+    return (
+        <>
+            {!tasks?.length ||
+                (<button
+                    type="a"
+                    className={styles.buttonDelete}
+                    onClick={handleDelete}
+                    data-test-id="btn-delete" >
+                    <span className="glyphicon glyphicon-remove mr-5px"></span>
+                Delete
+                </button>)
+            }
+        </>
+    )
+}
+
+export default DeleteButton;
---
+++ src/pages/home/TaskListView/__test__/DeleteButton.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { fetchApiData } from 'utils';
+import { useTaskEditContext, useFlashMessageContext, useLoadinSpinnerContext } from 'hooks';
+
+jerst.mock('utils/api/fetchApiData');
+jerst.mock('hooks/useTaskEditContext');
+jerst.mock('hooks/useFlashMessageContext');
+jerst.mock('hooks/useLoadinSpinnerContext');
+
+describe('src/pages/home/TaskListView/__test__/DeleteButton.test.js', () => {
+    describe('DeleteButton', () => {
+        
+        it('should render the button when tasks are available', () => {
+            
+        });
+    });
+});
