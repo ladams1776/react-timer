95511d31c8d8e6d6488afb40909f5a55b0764500
Larry Adams <ladams1776@yahoo.com>
2020-05-13 20:55:30 -0400

    Essentially get rid of this file - will need to fix all my tests that use it ☹️

--- src/testUtils/createWrapperWithContext.js
+++ src/testUtils/createWrapperWithContext.js
@@ -3,24 +3,24 @@ import Enzyme, { shallow } from 'enzyme';
 import chai from 'chai';
 import SinonChai from 'sinon-chai';
 import Adapter from 'enzyme-adapter-react-16';
-import * as useTaskEditContext from 'hooks/useTaskEditContext';
+// import * as useTaskEditContext from 'hooks/useTaskEditContext';
 
 Enzyme.configure({ adapter: new Adapter() });
 chai.use(SinonChai);
 
-let stuber;
-stuber = sinon.stub(useTaskEditContext, 'default');
-/**
- *
- * @param {function} component React Component
- * @param {Object} mockContext Context for the React Component for the test
- * @param {Object} sinonStub a Sinon stub wrapping the context
- */
-const createWrapperWithContext = (component, mockContext = {}) => {
-  if (mockContext !== {}) {
-    stuber.returns(mockContext);
-  }
-  return shallow(component);
-};
+// let stuber;
+// stuber = sinon.stub(useTaskEditContext, 'default');
+// /**
+//  *
+//  * @param {function} component React Component
+//  * @param {Object} mockContext Context for the React Component for the test
+//  * @param {Object} sinonStub a Sinon stub wrapping the context
+//  */
+// const createWrapperWithContext = (component, mockContext = {}) => {
+//   if (mockContext !== {}) {
+//     stuber.returns(mockContext);
+//   }
+//   return shallow(component);
+// };
 
-export default createWrapperWithContext;
+// export default createWrapperWithContext;
