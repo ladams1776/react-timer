186c9261c52f3449c86d1d53e2547571fe23e94d
larry adams <ladams1776@yahoo.com>
2020-05-15 22:59:48 -0400

    Update ControlButtons and the test

--- src/pages/home/TaskListView/ControlButtons/ControlButtons.js
+++ src/pages/home/TaskListView/ControlButtons/ControlButtons.js
@@ -1,27 +1,18 @@
-// @TODO: Left off here moving the 3 buttons from TaskListView over into this component
-// @TODO: Then we want to control when to display the other 2 buttons
-// @TODO: Based on if there are any tasks. So we might be pushing Tasks into the context as well and
-// @TODO: Creating a new context or adding to the existing one.
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import DeleteButton from './DeleteButton/DeleteButton';
 import DownloadButton from './DownloadButton/DownloadButton';
 import styles from './ControlButtons.module.css';
 
-const ControlButtons = () => {
-
-
-
-  return (
+const ControlButtons = () => (
     <>
       <DeleteButton />
       <DownloadButton />
       <NavLink to={'/task/-1'} className={styles.buttonAdd} data-test-id="btn-new">
-        <span className="glyphicon glyphicon- plus mr-5px" />
+        <span className="glyphicon glyphicon-plus mr-5px" />
         New Task
-    </NavLink >
+      </NavLink >
     </>
   );
-};
 
 export default ControlButtons;
--- src/pages/home/TaskListView/ControlButtons/__test__/ControlButtons.test.js
+++ src/pages/home/TaskListView/ControlButtons/__test__/ControlButtons.test.js
@@ -1,130 +1,21 @@
 import React from "react";
-import sinon from "sinon";
-import { getFormattedDate } from "utils";
-import { createWrapperWithContext, findByTestId } from "testUtils";
+import { shallow } from 'enzyme';
+import { findByTestId } from "testUtils";
 import ControlButtons from "../ControlButtons";
-import formatTimeContractAndCustomer from "../formatTimeContractAndCustomer";
-import * as writeJsonFile from "../writeJsonFile";
 
 describe("src/components/ControlButtons/__test__/ControlButtons.test.js", () => {
+  // Arrange 
   let wrapper;
 
-  const writeJsonFileSpy = jest.spyOn(writeJsonFile, "writeJsonFile");
-
-  beforeEach(() => {
-    writeJsonFileSpy.mockReset();
-  });
-
   describe("ControlButtons", () => {
-    it("should display ControlButtons with all 3 buttons when we have Tasks", () => {
-      const context = {
-        setMessage: sinon.spy(),
-        projects: [],
-        tasks: [{ id: "id" }, { id: "id" }]
-      };
-
-      wrapper = createWrapperWithContext(<ControlButtons />, context);
-
-      expect(wrapper.find(findByTestId("btn-download"))).toHaveLength(1);
-      expect(wrapper.find(findByTestId("btn-delete"))).toHaveLength(1);
-      expect(wrapper.find(findByTestId("btn-new"))).toHaveLength(1);
+    it("should display ControlButtons", () => {
+      // Arrange
+      
+      // Act
+      wrapper = shallow(<ControlButtons />);
+      
+      // Assert
+      expect(findByTestId(wrapper, "btn-new")).toHaveLength(1);
     });
-
-    it("should display ControlButtons with only new button when we don't have a task", () => {
-      const context = {
-        setMessage: sinon.spy(),
-        projects: [],
-        tasks: []
-      };
-
-      wrapper = createWrapperWithContext(<ControlButtons />, context);
-
-      expect(wrapper.find("[data-test-id='btn-download']")).toHaveLength(0);
-      expect(wrapper.find("[data-test-id='btn-delete']")).toHaveLength(0);
-      expect(wrapper.find("[data-test-id='btn-new']")).toHaveLength(1);
-    });
-
-    describe("#_handleDelete", () => {
-      it('should should display FlashMessage and empty the "tasks" array', async () => {
-        global.fetch = jest.fn().mockImplementation(() =>
-          Promise.resolve({
-            json: jest.fn().mockImplementation(() => Promise.resolve())
-          })
-        );
-
-        jest.useFakeTimers(); // Need to declare we are using setTimeout
-
-        const context = {
-          setMessage: jest.fn().mockImplementation(),
-          projects: [],
-          tasks: [{ id: "id" }, { id: "id" }],
-          updateTasks: jest.fn().mockImplementation()
-        };
-
-        wrapper = createWrapperWithContext(<ControlButtons />, context);
-
-        const response = await wrapper
-          .find("[data-test-id='btn-delete']")
-          .props()
-          .onClick({ preventDefault: jest.fn().mockImplementation() });
-
-        jest.runTimersToTime(500); // speed up the time on the setTimeout
-
-        expect(context.updateTasks).toHaveBeenCalledTimes(1);
-        expect(context.updateTasks).toHaveBeenCalledWith([]);
-        expect(context.setMessage).toHaveBeenCalledWith(
-          "Successfully deleted all tasks"
-        );
-        expect(context.setMessage).toHaveBeenCalledTimes(1);
-      });
-    });
-
-    describe("#_handleDownload", () => {
-      const task = {
-        time: 10000,
-        contractId: "project Id",
-        description: "task description becomes file name"
-      };
-
-      const projects = [
-        {
-          key: "project Id",
-          contract: "contract for the project",
-          customer: "customer for the contract"
-        },
-        {
-          key: "project Id2",
-          contract: "contract for the project2",
-          customer: "customer for the contract2"
-        },
-        {
-          key: "project Id3",
-          contract: "contract for the project3",
-          customer: "customer for the contract3"
-        }
-      ];
-
-      it('should prepare "tasks" into a format', () => {
-        const context = {
-          projects,
-          tasks: [task]
-        };
-
-        wrapper = createWrapperWithContext(<ControlButtons />, context);
-
-        const response = wrapper
-          .find("[data-test-id='btn-download']")
-          .props()
-          .onClick();
-
-        const expectTaskBundle = {
-          date: getFormattedDate(new Date()),
-          WorkUnit: [formatTimeContractAndCustomer(context.tasks[0], projects)]
-        };
-
-        expect(writeJsonFileSpy).toHaveBeenCalledWith(expectTaskBundle);
-      });
-    });
-    //@TODO: Test New Task
   });
 });
