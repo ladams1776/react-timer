9309f367e5807d7303793a54a3febf8a92d1d265
larry adams <ladams1776@yahoo.com>
2020-05-18 07:49:16 -0400

    Update ControlButtons and Update it's test

--- src/pages/home/TaskListView/ControlButtons/ControlButtons.js
+++ src/pages/home/TaskListView/ControlButtons/ControlButtons.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import useBrowserHistoryPush from 'hooks/useBrowserHistoryPush';
 import DeleteButton from './DeleteButton/DeleteButton';
 import DownloadButton from './DownloadButton/DownloadButton';
 import styles from './ControlButtons.module.css';
 
-const ControlButtons = () => (
+const ControlButtons = () => {
+  const { push } = useBrowserHistoryPush();
+
+  return (
     <>
       <DeleteButton />
       <DownloadButton />
-      <NavLink to={'/task/-1'} className={styles.buttonAdd} data-test-id="btn-new">
+      <div className={styles.buttonAdd} data-test-id="btn-new"
+        onClick={() => { push("/task/-1") }}>
         <span className="glyphicon glyphicon-plus mr-5px" />
         New Task
-      </NavLink >
+      </div>
     </>
-  );
+  )
+};
 
 export default ControlButtons;
--- src/pages/home/TaskListView/ControlButtons/__test__/ControlButtons.test.js
+++ src/pages/home/TaskListView/ControlButtons/__test__/ControlButtons.test.js
@@ -1,21 +1,45 @@
 import React from "react";
 import { shallow } from 'enzyme';
 import { findByTestId } from "testUtils";
+import useBrowserHistoryPush from 'hooks/useBrowserHistoryPush';
 import ControlButtons from "../ControlButtons";
 
+jest.mock('hooks/useBrowserHistoryPush');
+
+//@TODO: ‼️ Fill in this test's onClick ‼️
 describe("src/components/ControlButtons/__test__/ControlButtons.test.js", () => {
   // Arrange 
   let wrapper;
+  const historyMock = {
+    push: jest.fn()
+  };
+
+  beforeEach(() => {
+    useBrowserHistoryPush.mockReturnValue(historyMock);
+  });
 
   describe("ControlButtons", () => {
     it("should display ControlButtons", () => {
       // Arrange
-      
+
       // Act
       wrapper = shallow(<ControlButtons />);
-      
+
       // Assert
       expect(findByTestId(wrapper, "btn-new")).toHaveLength(1);
+    });
+
+    describe('onClick', () => {
+      it("should call 'push' function", () => {
+        // Arrange
+
+        // Act
+        wrapper = shallow(<ControlButtons />);
+        findByTestId(wrapper, "btn-new").props().onClick()
+
+        // Assert
+        expect(historyMock.push).toHaveBeenNthCalledWith(1, '/task/-1');
+      });
     });
   });
 });
