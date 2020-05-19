ed8ad4bfff7e2f1b4771bc1484b330b81fd25894
larry adams <ladams1776@yahoo.com>
2020-05-18 23:58:32 -0400

    Fix styles after breaking apart the component

--- src/pages/home/TaskListView/ControlButtons/ControlButtons.js
+++ src/pages/home/TaskListView/ControlButtons/ControlButtons.js
@@ -2,10 +2,11 @@ import React from 'react';
 import DeleteButton from './DeleteButton/DeleteButton';
 import DownloadButton from './DownloadButton/DownloadButton';
 import NewButton from './NewButton/NewButton';
+import styles from './ControlButtons.module.css';
 
 const ControlButtons = () => {
   return (
-    <div data-test-id="control-buttons">
+    <div className={styles.controlButtons} data-test-id="control-buttons">
       <DeleteButton />
       <DownloadButton />
       <NewButton />
