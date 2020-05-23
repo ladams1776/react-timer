84ee4d5d55ef893f6481f6cc6ec426afd1b0b66c
larry adams <ladams1776@yahoo.com>
2020-05-23 12:29:42 -0400

    Apply eslint and prettier to TaskService.test.js

--- server/domain/services/tasks/__test__/TaskService.test.js
+++ server/domain/services/tasks/__test__/TaskService.test.js
@@ -1,43 +1,41 @@
-const TaskService = require("../TaskService");
+const TaskService = require('../TaskService');
 const TaskRepository = require('../../../../infrastructure/repositories/tasks/TaskRepository');
 
 jest.mock('../../../../infrastructure/repositories/tasks/TaskRepository');
 
-describe("server/domain/services/tasks/__test__/TaskService.test.js", () => {
-    // Arrange 
+describe('server/domain/services/tasks/__test__/TaskService.test.js', () => {
+  // Arrange
 
-    describe("TaskService", () => {
-        // Arrange
-        const tasks = [
-            { _id: 1 }
-        ];
-        
-        it("should call TaskRepository.fetchAllTask(), when TaskService.fetchAllTask() is called.", async () => {
-            // Arrange
-            TaskRepository.fetchAllTask = jest.fn().mockImplementation(() => tasks);
-            jest.spyOn(TaskRepository, 'fetchAllTask');
+  describe('TaskService', () => {
+    // Arrange
+    const tasks = [{ _id: 1 }];
 
-            // Act
-            const actual = await TaskService.fetchAllTasks();
+    it('should call TaskRepository.fetchAllTask(), when TaskService.fetchAllTask() is called.', async () => {
+      // Arrange
+      TaskRepository.fetchAllTask = jest.fn().mockImplementation(() => tasks);
+      jest.spyOn(TaskRepository, 'fetchAllTask');
 
-            // Assert
-            expect(TaskRepository.fetchAllTask).toHaveBeenCalledTimes(1);
-            expect(actual).toEqual(tasks);
-        });
+      // Act
+      const actual = await TaskService.fetchAllTasks();
 
-        it("should call TaskRepository.fetchTaskById(), when TaskService.fetchAllTask() is called.", async () => {
-            // Arrange
-            const taskId = 1;
-
-            TaskRepository.fetchTaskById = jest.fn().mockImplementation(() => tasks);
-            jest.spyOn(TaskRepository, 'fetchTaskById');
-
-            // Act
-            const actual = await TaskService.fetchTaskById(taskId);
-
-            // Assert
-            expect(TaskRepository.fetchTaskById).toHaveBeenCalledTimes(1);
-            expect(actual).toEqual(tasks);
-        });
+      // Assert
+      expect(TaskRepository.fetchAllTask).toHaveBeenCalledTimes(1);
+      expect(actual).toEqual(tasks);
     });
+
+    it('should call TaskRepository.fetchTaskById(), when TaskService.fetchAllTask() is called.', async () => {
+      // Arrange
+      const taskId = 1;
+
+      TaskRepository.fetchTaskById = jest.fn().mockImplementation(() => tasks);
+      jest.spyOn(TaskRepository, 'fetchTaskById');
+
+      // Act
+      const actual = await TaskService.fetchTaskById(taskId);
+
+      // Assert
+      expect(TaskRepository.fetchTaskById).toHaveBeenCalledTimes(1);
+      expect(actual).toEqual(tasks);
+    });
+  });
 });
