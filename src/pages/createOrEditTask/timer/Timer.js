8f7102d626877f1e67564b50a2e287b0c822edf3
larry adams <ladams1776@yahoo.com>
2020-05-29 07:48:15 -0400

    Make Timer consume the new useTimeContext & update test

--- src/pages/createOrEditTask/timer/Timer.js
+++ src/pages/createOrEditTask/timer/Timer.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ms from 'pretty-ms';
 import { displayMsInFractionalHourFormat } from 'utils';
+import { useTimeContext } from '../hooks';
 
-const Timer = ({ time }) => {
+const Timer = () => {
+  const { time } = useTimeContext();
+
   return (
-    <div
-      data-test-id="timer__display__content">
-      <div data-test-id="fractionHour">{`Hours: ${displayMsInFractionalHourFormat(time)}`}</div>
-      <div data-test-id="secondDecimalDigitHour">{ms(time, { secondsDecimalDigits: 2 })}</div>
+    <div data-test-id="timer__display__content">
+      <div data-test-id="fractionHour">{`Hours: ${displayMsInFractionalHourFormat(
+        time,
+      )}`}</div>
+      <div data-test-id="secondDecimalDigitHour">
+        {ms(time, { secondsDecimalDigits: 2 })}
+      </div>
     </div>
   );
 };
