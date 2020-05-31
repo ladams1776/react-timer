034589be8653e1b2393a7d801fce9fe1bede2bb8
larry adams <ladams1776@yahoo.com>
2020-05-30 20:29:20 -0400

    Export useSubmit

--- src/pages/createOrEditTask/hooks/index.js
+++ src/pages/createOrEditTask/hooks/index.js
@@ -4,4 +4,5 @@ export { default as useTagTransformer } from './useTagTransformer';
 export { default as useFetchTaskById } from './useFetchTaskById/useFetchTaskById';
 export { default as useFormDispatch } from './useFormDispatch';
 export { default as useTimeContext } from './useTimeContext';
-export { default as useFetchDispatch } from './useFetchDispatch';
+export { default as useFetchDispatch } from './useFetchDispatch/useFetchDispatch';
+export { default as useSubmit } from './useSubmit/useSubmit';
