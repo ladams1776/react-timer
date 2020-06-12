c177810de7aaa3e74c7d5ec483859661f26b1760
larry adams <ladams1776@yahoo.com>
2020-06-12 07:43:21 -0400

    Use TinyMCE for TextAreaAdapter

--- src/components/TextAreaAdapter.js
+++ src/components/TextAreaAdapter.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import PropType from 'prop-types';
+// import CKEditor from 'ckeditor4-react';
+import { Editor } from '@tinymce/tinymce-react';
+import { useTaskEditContext } from 'hooks';
+import EditTaskForm from '../pages/createOrEditTask/form/EditTaskForm';
 
-const TextAreaAdapter = ({ input }) => (
-    <textarea
-        {...input}
-        type="textarea"
-        cols="80"
-        rows="10"
-        data-test-id="text-area-adapter"
+const TextAreaAdapter = ({ description, setDescription }) => {
+  // const { task } = useTaskEditContext();
+  // const description = task?.description;
+
+  return (
+    <Editor
+      initialValue={description}
+      id="description" //@TODO: Left off trying this stuff
+      name="description"
+      data-test-id="text-area-adapter"
+      // ref={compRef}
+      init={{
+        height: 500,
+        menubar: true,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount',
+        ],
+        toolbar:
+          'undo redo | formatselect | bold italic backcolor | \
+        alignleft aligncenter alignright alignjustify | \
+        bullist numlist outdent indent | removeformat | help',
+      }}
+      onEditorChange={(content, editor) => {
+        setDescription(content);
+      }}
     />
-);
-
-TextAreaAdapter.PropType = {
-    input: PropType.object,
+  );
 };
 
 export default TextAreaAdapter;
