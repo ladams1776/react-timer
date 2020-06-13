33a8126075af48f611914f36e26e42e877a7718f
larry adams <ladams1776@yahoo.com>
2020-06-13 13:19:05 -0400

    Refactor useFetchTaskById and update test
    (warning) Not testing hydration process

--- src/pages/createOrEditTask/hooks/useFetchTaskById/useFetchTaskById.js
+++ src/pages/createOrEditTask/hooks/useFetchTaskById/useFetchTaskById.js
@@ -1,20 +1,34 @@
 import { useEffect } from 'react';
 import { fetchApiData } from 'utils';
+import useTimeContext from '../useTimeContext';
 import { useLoadinSpinnerContext } from 'hooks';
 
 const useFetchTaskById = (taskId, dispatch) => {
+  const { setTime } = useTimeContext();
   const { setIsLoadin } = useLoadinSpinnerContext();
+
+  //@TODO: Not even testing this hydration. I suppose I could, but I want to move on.
+  const taskDispatch = data => {
+    const formData = {
+      id: data._id,
+      project: data.contractId,
+      tags: data.tags,
+      description: data.description,
+      time: data.time,
+    };
+    setIsLoadin(false);
+    setTime(data.time);
+    return dispatch(formData);
+  };
 
   return useEffect(() => {
     setIsLoadin(true);
 
-    if (taskId !== "-1") {
-      fetchApiData(`task/${taskId}`, {}, dispatch);
+    if (taskId !== '-1') {
+      fetchApiData(`task/${taskId}`, {}, taskDispatch);
     } else {
-      dispatch({ time: 0 })
+      taskDispatch({ time: 0 });
     }
-
-    setIsLoadin(false);
   }, [taskId, dispatch, setIsLoadin]);
 };
 
