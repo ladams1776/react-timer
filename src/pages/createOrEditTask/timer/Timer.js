23ca9233a636dca6ba04d344fab230bff66480e1
larry adams <ladams1776@yahoo.com>
2020-06-14 07:23:27 -0400

    Update Timer component and test

--- src/pages/createOrEditTask/timer/Timer.js
+++ src/pages/createOrEditTask/timer/Timer.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ms from 'pretty-ms';
 import { displayMsInFractionalHourFormat } from 'utils';
+import ControlPanel from './controlPanel/ControlPanel';
+import useUpdateCurrentTime from './hooks/useUpdateCurrentTime'
 import { useTimeContext } from '../hooks';
 
-const Timer = () => {
+const Timer = ({ setTime, isActive, setIsActive }) => {
   const { time } = useTimeContext();
+  useUpdateCurrentTime(time, isActive, setTime);
 
   return (
-    <div data-test-id="timer__display__content">
-      <div data-test-id="fractionHour">{`Hours: ${displayMsInFractionalHourFormat(
-        time,
-      )}`}</div>
-      <div data-test-id="secondDecimalDigitHour">
-        {ms(time, { secondsDecimalDigits: 2 })}
+    <ControlPanel
+      time={time}
+      setTime={setTime}
+      isActive={isActive} 
+      setIsActive={setIsActive}
+    >
+      <div data-test-id="timer__display__content">
+        <div data-test-id="fractionHour">{`Hours: ${displayMsInFractionalHourFormat(
+          time,
+        )}`}</div>
+        <input data-test-id="secondDecimalDigitHour"
+          value={ms(time, { secondsDecimalDigits: 2 })}
+          readOnly
+        />
       </div>
-    </div>
+    </ControlPanel>
   );
 };
 
--- src/pages/createOrEditTask/timer/__test__/Timer.test.js
+++ src/pages/createOrEditTask/timer/__test__/Timer.test.js
@@ -2,28 +2,51 @@ import React from 'react';
 import ms from 'pretty-ms';
 import { displayMsInFractionalHourFormat } from 'utils';
 import { createWrapperWithContext, findByTestId } from 'testUtils';
+import useUpdateCurrentTime from '../hooks/useUpdateCurrentTime';
 import { useTimeContext } from '../../hooks';
 import Timer from '../Timer';
 
 jest.mock('../../hooks/useTimeContext');
+jest.mock('../hooks/useUpdateCurrentTime');
 
 describe('src/pages/createOrEditTask/timer/__test__/Timer.test.js', () => {
   describe('Timer', () => {
+    // Arrange
     let wrapper;
+    const setTimeSpy = jest.fn();
+    const isActive = false;
     const timeContext = {
       time: 20000,
     };
 
-    useTimeContext.mockReturnValue(timeContext);
-    
+    beforeEach(() => {
+      useTimeContext.mockReturnValue(timeContext);
+      setTimeSpy.mockReset();
+    });
+
     it('should display the time in the right format', () => {
-      wrapper = createWrapperWithContext(<Timer />);
-      
+      // Arrange
+      const setIsActiveSpy = jest.fn();
+
+      // Act
+      wrapper = createWrapperWithContext(
+        <Timer
+          setTime={setTimeSpy}
+          isActive={isActive}
+          setIsActive={setIsActiveSpy}
+        />,
+      );
+
+      // Assert
+      expect(useUpdateCurrentTime).toBeCalledWith(
+        timeContext.time,
+        isActive,
+        setTimeSpy,
+      );
       expect(findByTestId(wrapper, 'fractionHour').text()).toEqual(
         `Hours: ${displayMsInFractionalHourFormat(timeContext.time)}`,
       );
-
-      expect(findByTestId(wrapper, 'secondDecimalDigitHour').text()).toEqual(
+      expect(findByTestId(wrapper, 'secondDecimalDigitHour').props().value).toEqual(
         `${ms(timeContext.time, { secondsDecimalDigits: 2 })}`,
       );
     });
