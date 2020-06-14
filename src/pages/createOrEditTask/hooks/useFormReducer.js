ec790dcba7a3089a6fb17602a3d52c4464dbf4bb
larry adams <ladams1776@yahoo.com>
2020-06-14 09:43:14 -0400

    abstract some form state management into useFormReducer

---
+++ src/pages/createOrEditTask/hooks/useFormReducer.js
@@ -0,0 +1,42 @@
+import { useReducer } from 'react';
+
+//@TODO: Need to unit test this.
+const initialState = {
+  id: -1,
+  description: '',
+  tags: [],
+  project: 0,
+  time: 0,
+};
+const formReducer = (state, newState) => {
+  return {
+    ...state,
+    ...newState,
+  };
+};
+
+const useFormReducer = () => {
+  const onProjectDropDownChange = e => {
+    dispatch({ project: e.target.value });
+  };
+
+  const onTextAreaChange = e => {
+    dispatch({ description: e });
+  };
+
+  const onTagChange = e => {
+    dispatch({ tags: e });
+  };
+
+  const [state, dispatch] = useReducer(formReducer, initialState);
+  // Could abstract some of these onClick listeneners 
+  return [
+    state,
+    dispatch,
+    onProjectDropDownChange,
+    onTextAreaChange,
+    onTagChange,
+  ];
+};
+
+export default useFormReducer;
