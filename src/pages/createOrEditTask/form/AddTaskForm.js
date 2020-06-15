cc02d694e38417f237580c944f077a8b8e1249c0
larry adams <ladams1776@yahoo.com>
2020-06-14 22:07:17 -0400

    AddTaskForm is refacted and tested

---
+++ src/pages/createOrEditTask/form/AddTaskForm.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useBackButtonListener, useSetCurrentLocation } from 'hooks';
+import { TextAreaAdapter } from 'components';
+import {
+  useFetchTaskById,
+  useTagContext,
+  useFetchTags,
+  useSubmit,
+  useFormReducer,
+} from '../hooks';
+import Timer from '../timer/Timer';
+import ProjectDropDown from '../projectDropdown/ProjectDropdown';
+import TagMultiSelect from '../tagMultiSelect/TagMultiSelect';
+import styles from './TaskForm.module.css';
+
+const AddTaskForm = ({ taskId }) => {
+  useSetCurrentLocation(`/task/${taskId}`);
+  useBackButtonListener();
+  const [
+    state,
+    dispatch,
+    onProjectChange,
+    onTextChange,
+    onTagChange,
+  ] = useFormReducer();
+
+  useFetchTaskById(taskId, dispatch);
+
+  // Tag Stuff
+  useFetchTags();
+  const { allTags } = useTagContext();
+  const onSubmit = useSubmit(state, allTags, dispatch);
+  const { description, tags, project } = state;
+
+  return (
+    <div className={styles.taskFormContainer}>
+      <div className={styles.form}>
+        <form
+          className={styles.taskForm}
+          method={taskId === -1 ? 'POST' : 'PUT'}
+          data-test-id="form"
+        >
+          <Timer />
+          <ProjectDropDown value={project} onChange={onProjectChange} />
+          <TagMultiSelect tags={tags} onChange={onTagChange} />
+
+          <TextAreaAdapter
+            description={description}
+            setDescription={onTextChange}
+          />
+
+          <button
+            type="submit"
+            className={styles.submit}
+            onClick={onSubmit}
+            data-test-id="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddTaskForm;
