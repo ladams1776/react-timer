a3de17ada2f5d517f29b52c575d2197dc9b715aa
larry adams <ladams1776@yahoo.com>
2020-06-15 20:25:43 -0400

    Reformat TextAreaAdapter's toolbar options

--- src/components/TextAreaAdapter.js
+++ src/components/TextAreaAdapter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
 const TextAreaAdapter = ({ description, setDescription }) => {
+  const toolbar = 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help';
   return (
     <Editor
       initialValue={description}
@@ -17,10 +18,7 @@ const TextAreaAdapter = ({ description, setDescription }) => {
           'searchreplace visualblocks code fullscreen',
           'insertdatetime media table paste code help wordcount',
         ],
-        toolbar:
-          'undo redo | formatselect | bold italic backcolor | \
-        alignleft aligncenter alignright alignjustify | \
-        bullist numlist outdent indent | removeformat | help',
+        toolbar
       }}
       onEditorChange={(content, editor) => {
         setDescription(content);
