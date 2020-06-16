0773f94732b2019bbd559c31e6d71040d994d563
larry adams <ladams1776@yahoo.com>
2020-06-15 20:29:27 -0400

    Clean up some comments on TextAreaAdapter

--- src/components/TextAreaAdapter.js
+++ src/components/TextAreaAdapter.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 
 const TextAreaAdapter = ({ description, setDescription }) => {
-  const toolbar = 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help';
+  const toolbar =
+    'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help';
   return (
     <Editor
       initialValue={description}
-      id="description" //@TODO: Left off trying this stuff
+      id="description"
       name="description"
       data-test-id="text-area-adapter"
-      // ref={compRef}
       init={{
         height: 500,
         menubar: true,
@@ -18,11 +18,9 @@ const TextAreaAdapter = ({ description, setDescription }) => {
           'searchreplace visualblocks code fullscreen',
           'insertdatetime media table paste code help wordcount',
         ],
-        toolbar
+        toolbar,
       }}
-      onEditorChange={(content, editor) => {
-        setDescription(content);
-      }}
+      onEditorChange={content => setDescription(content)}
     />
   );
 };
