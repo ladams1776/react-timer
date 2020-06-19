92649cac96b4beeacf3f53fd9480075b98e7b1dd
larry adams <ladams1776@yahoo.com>
2020-06-18 22:55:37 -0400

    Remove these guys, going to restructure and relocate

--- src/pages/tags/TagsPage/useFetchAllTags.js
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react';
-import { fetchApiData } from 'utils';
-
-const useFetchAllTags = setTags =>
-  useEffect(() => fetchApiData('tags', {}, setTags), [setTags]);
-
-export default useFetchAllTags;
