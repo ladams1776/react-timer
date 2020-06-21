0a6d6b73b516fa58f6636ec7376387fde4196832
larry adams <ladams1776@yahoo.com>
2020-06-21 10:47:49 -0400

    Clean up arrange section in useDispatch.test.js

--- src/pages/tags/TagListView/Tag/DeleteTagButton/__test__/useDispatch.test.js
+++ src/pages/tags/TagListView/Tag/DeleteTagButton/__test__/useDispatch.test.js
@@ -22,13 +22,14 @@ describe('src/pages/home/TaskListView/Task/DeleteTaskButton/__test__/useDispatch
         it('should return a function that when called will trigger a flash message and reload the window.', () => {
             // Arrange
             const taskId = 1;
-
+            const expected = `Successfully Deleted Task with id of ${taskId}`;
+            
             // Act
             const { result } = renderHook(() => useDispatch(taskId));
             act(() => result.current());
 
             // Assert
-            expect(flashMessageContextMock.setSuccessFlashMessage).toHaveBeenNthCalledWith(1, `Successfully Deleted Task with id of ${taskId}`);
+            expect(flashMessageContextMock.setSuccessFlashMessage).toHaveBeenNthCalledWith(1, expected);
             expect(window.location.reload).toHaveBeenCalledTimes(1);
         });
     });
