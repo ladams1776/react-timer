7011c03c62e30fffb20f494b95c86af86894ffc0
larry adams <ladams1776@yahoo.com>
2020-06-25 23:49:36 -0400

    Update deleteTagAction to use the jsonResponder

--- server/application/requestHandlers/tags/deleteTagAction.js
+++ server/application/requestHandlers/tags/deleteTagAction.js
@@ -1,7 +1,6 @@
 const TagService = require('../../../domain/services/tags/TagService');
+const jsonResponse = require('../jsonResponse');
 
 module.exports = async (req, res) => {
-    const { id } = req.params;
-    TagService.deleteTag(id);
-    res.jsonp({ tagId: id, isSuccess: true });
+    TagService.deleteTag(req.params.id, jsonResponse(res));
 };
