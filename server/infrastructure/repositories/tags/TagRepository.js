4644db5bdb86900a6216cb9ed86509fff7197d9a
larry adams <ladams1776@yahoo.com>
2020-06-28 21:22:22 -0400

    Update TagRepository & Test

--- server/infrastructure/repositories/tags/TagRepository.js
+++ server/infrastructure/repositories/tags/TagRepository.js
@@ -1,9 +1,33 @@
 const Tag = require('../../models/Tag');
-const hydrate = require('../../hydrators/hydrate');
+const hydrateAndResponse = require('../../hydrators/hydrateAndResponse');
+const assembleTag = require('./assembleTag');
 
 const TagRepository = {
-  fetchAllTags: () => Tag.find({}, hydrate),
-  deleteTag: id => Tag.deleteOne({ _id: id }),
+  fetchAllTags: (res) => Tag.find({}, hydrateAndResponse(res)),
+  deleteTag: (id, res) => Tag.deleteOne(
+    { _id: id },
+    hydrateAndResponse(res)
+  ),
+  addTag: (tagDto, res) => {
+    const tag = assembleTag(tagDto);
+    tag.save(hydrateAndResponse(res));
+  },
+  fetchTagById: (id, res) =>
+    Tag.findById(id, hydrateAndResponse(res)),
+  updateTag: (dto, res) => {
+    Tag.findOneAndUpdate(
+      { _id: dto.id },
+      {
+        $set: {
+          _id: dto.id,
+          name: dto.name,
+          description: dto.description
+        },
+      },
+      { new: true },
+      hydrateAndResponse(res),
+    );
+  }
 };
 
 module.exports = TagRepository;
