b0c8a123cc74b2cbb8ff6b8e3ffcac2f22bc81a2
larry adams <ladams1776@yahoo.com>
2020-08-11 22:45:04 -0400

    Pass in tasks to TaskListView - instead of it having to fetch them
    Need to do this because we need them for other components.

--- src/pages/home/TaskListView/TaskListView.js
+++ src/pages/home/TaskListView/TaskListView.js
@@ -1,20 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSetCurrentLocation } from 'hooks';
 import Task from './Task/Task';
-import ControlButtons from './ControlButtons/ControlButtons';
-import useFetchAllTasks from './useFetchAllTasks';
 import './TaskListView.css';
 
-const TaskListView = ({ className, taskId }) => {
+const TaskListView = ({ className, taskId, tasks }) => {
   useSetCurrentLocation('/');
-  const [tasks, setTasks] = useState([]);
-  useFetchAllTasks(setTasks);
 
   return (
     <div className={className} data-test-id="list-view">
-      <div className="task-list__header">
-        <ControlButtons tasks={tasks} />
-      </div>
       <ul className="task-list">
         {tasks.map(task => (
           <li key={task._id} className="task">
