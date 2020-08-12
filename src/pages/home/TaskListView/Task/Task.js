151ec99664f12b014d0cd00c15a6f35496a523a3
larry adams <ladams1776@yahoo.com>
2020-08-11 22:59:12 -0400

    Slimming down the Task in TaskListView

--- src/pages/home/TaskListView/Task/Task.js
+++ src/pages/home/TaskListView/Task/Task.js
@@ -3,37 +3,34 @@ import cn from 'classnames';
 import PropType from 'prop-types';
 import { useBrowserHistory, useGetProjectOptionLabel } from 'hooks';
 import DeleteTaskButton from './DeleteTaskButton/DeleteTaskButton';
-import { getFormattedDate, displayMsInFractionalHourFormat } from 'utils';
 import styles from './Task.module.css';
 
-const Task = ({ _id, description = '', contractId, time, date, tags, selectedId }) => {
+const Task = ({ _id, description = '', contractId, selectedId }) => {
   const { push } = useBrowserHistory();
-  const title = description.split("\n")[0];
+  const title = description?.split("\n")[0];
   const projectOptionLabel = useGetProjectOptionLabel(contractId);
   const isSelected = selectedId === _id;
-  console.log('---------------v')
-  console.log('_id: ', _id);
-  console.log('selected: ', selectedId);
-  console.log('---------------^')
-
 
   return (
-    <div className={cn(styles.taskItem, { [styles.selected]: isSelected })}>
-      <div
-        className={styles.taskItemLeft}
-        onClick={() => {
-          sessionStorage.setItem('LOCATION', `/${_id}`);
-          push(`/${_id}`);
-          window.location.reload();
-        }}
-      >
+    <div className={styles.taskContainer}>
+      <div className={cn(styles.taskItem, { [styles.selected]: isSelected })}>
         <div
-          className={styles.taskItemDescription, { [styles.selected]: selectedId == _id }}
-          dangerouslySetInnerHTML={{ __html: title }}
-        />
-
+          className={styles.taskItemLeft}
+          onClick={() => {
+            sessionStorage.setItem('LOCATION', `/${_id}`);
+            push(`/${_id}`);
+            window.location.reload();
+          }}
+        >
+          <div
+            className={styles.taskItemDescription}
+            dangerouslySetInnerHTML={{ __html: title }}
+          />
+          <span className={styles.taskItemCustomer}>{projectOptionLabel}</span>
+        </div>
+        <DeleteTaskButton taskId={_id} isSelected={isSelected} />
       </div>
-      <DeleteTaskButton taskId={_id} />
+      <div className={cn(styles.underBorder, { [styles.underBorderSelected]: isSelected })} />
     </div >
   );
 };
