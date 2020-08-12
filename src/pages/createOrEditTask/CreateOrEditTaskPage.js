efc0cdf18699bf85d68d7fecbdae260cd0eb23ae
larry adams <ladams1776@yahoo.com>
2020-08-11 22:43:15 -0400

    Restructure CreateOrEditTaskPage to incorporate new styles

--- src/pages/createOrEditTask/CreateOrEditTaskPage.js
+++ src/pages/createOrEditTask/CreateOrEditTaskPage.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TagContextProvider from './contexts/TagContext';
 import TimeContextProvider from './contexts/TimeContext';
 import AddTaskForm from './form/AddTaskForm';
 import TaskListView from '../home/TaskListView/TaskListView';
+import ControlButtons from '../home/TaskListView/ControlButtons/ControlButtons';
+import useFetchAllTasks from '../home/TaskListView/useFetchAllTasks';
 import styles from './CreateOrEditTaskPage.module.css';
 
 const CreateOrEditTaskPage = ({ match }) => {
   const taskId = match?.params?.id;
+  const [tasks, setTasks] = useState([]);
+  useFetchAllTasks(setTasks);
 
   return (
     <TagContextProvider>
       <TimeContextProvider>
         <div className={styles.container}>
-          <TaskListView className={styles.listView} taskId={taskId} />
-          <AddTaskForm taskId={taskId} className={styles.form} />
+          {/* Need to make a leftInnerContainer */}
+          <div className={styles.navBar}>
+            <ControlButtons tasks={tasks} />
+          </div>
+          <div className={styles.rightInnerContainer}>
+            <TaskListView className={styles.listView} taskId={taskId} tasks={tasks} />
+            <AddTaskForm taskId={taskId} className={styles.form} />
+          </div>
         </div>
       </TimeContextProvider>
     </TagContextProvider>
