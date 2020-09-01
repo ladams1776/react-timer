04a2b4b558d6401dcb48a0e2b0c9e68742febecf
larry adams <ladams1776@yahoo.com>
2020-08-31 22:08:19 -0400

    Fix New Button's styles, now that we moved over to generic Button component.

--- src/pages/home/TaskListView/ControlButtons/NewButton/NewButton.js
+++ src/pages/home/TaskListView/ControlButtons/NewButton/NewButton.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { useBrowserHistory } from 'hooks';
+import { Button } from 'components';
 import styles from './NewButton.module.css';
 
 const NewButton = () => {
   const { push } = useBrowserHistory();
 
-  return <button className={styles.buttonAdd} data-test-id="btn-new"
+  return <Button className={styles.buttonAdd} data-test-id="btn-new"
     onClick={() => {
       push("/task/-1");
       window.location.reload();
     }}>
     <span className="glyphicon glyphicon-edit" />
-  </button>
+  </Button>
 };
 
 export default NewButton;
