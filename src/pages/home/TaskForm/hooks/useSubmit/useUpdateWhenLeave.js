7a0ff9eb324f816549086b031ba018164d9f7a5a
larry adams <ladams1776@yahoo.com>
2020-09-05 08:56:22 -0400

    Update only non-new tasks, when we navigate away from editing one.

--- src/pages/home/TaskForm/hooks/useSubmit/useUpdateWhenLeave.js
+++ src/pages/home/TaskForm/hooks/useSubmit/useUpdateWhenLeave.js
@@ -5,12 +5,17 @@ import hydrateTaskForm from './hydrateTaskForm';
 import { useTagContext } from '../'
 import useTaskEditContext from '../../../hooks/useTaskEditContext';
 
+/**
+ * We want to save the task, when we navigate away from editing it, automatically.
+ * But we do not want to save the task if it is a new one (denoted with '-1') 
+ */
 const useUpdateWhenLeave = () => {
     const { time } = useTimeContext();
     const { state } = useTaskEditContext();
     const { allTags } = useTagContext();
 
     const onSubmit = useCallback(() => {
+        const location = sessionStorage.getItem('LOCATION');
         const dateFormatted = getFormattedDate(new Date());
         const dispatch = () => { };
         const payload = {
@@ -24,9 +29,16 @@ const useUpdateWhenLeave = () => {
 
         const method = state.id ? 'PUT' : 'POST';
 
-        if (state.id !== -1) {
+        /* We do not want to save the task if the task is a "new task."
+         *  Something that has not even been saved yet, no need to make a call
+         *  to save a task, we are leaving, if the task is not even going to be 
+         *  in the database.
+        */
+        const isNotFromNewNote = location.indexOf("-1") === -1;
+        if (state.id !== -1 && isNotFromNewNote) {
             fetchApiData('task', { body: timeTask, method }, dispatch);
         }
+
     }, [allTags, state, time]);
 
 
