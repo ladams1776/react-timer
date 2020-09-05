db50a03235ffb31609793c2fd4cf1c7860a93e80
larry adams <ladams1776@yahoo.com>
2020-09-04 23:56:22 -0400

    Fis an issue when we delete a task that caused a update on a deleted resource

--- src/pages/home/TaskListView/Task/DeleteTaskButton/DeleteTaskButton.js
+++ src/pages/home/TaskListView/Task/DeleteTaskButton/DeleteTaskButton.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { fetchApiData } from 'utils';
-import useDispatch from './useDispatch';
 import cn from 'classnames';
+import { fetchApiData } from 'utils';
+import { useBrowserHistory } from 'hooks';
+import useDispatch from './useDispatch';
 import { Button } from "components";
 import styles from './DeleteTaskButton.module.css';
 
 const DeleteTaskButton = ({ taskId, isSelected }) => {
+  const { push } = useBrowserHistory();
   const dispatch = useDispatch(taskId);
 
   const _deleteClick = async e => {
     const isDeleting = window.confirm("Are you sure you want to delete task?");
+    const reload = data => {
+      dispatch(data);
+      push(`task/-1`);
+    };
 
     if (isDeleting) {
       e.preventDefault();
-      fetchApiData(`task/${taskId}`, { method: 'DELETE' }, dispatch);
+      fetchApiData(`task/${taskId}`, { method: 'DELETE' }, reload);
     }
   };
 
