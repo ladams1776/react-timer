e5261336f050ac8c12b89f7f6deb7365da34bb6c
larry adams <ladams1776@yahoo.com>
2020-09-05 22:06:05 -0400

    Suppress a warning, this fetch is fine
    when ever the tagId changes, we will fetch a new tag, this is acceptable.
    All the other decoration is a waste

--- src/pages/tags/AddTagForm/useFetchTagById.js
+++ src/pages/tags/AddTagForm/useFetchTagById.js
@@ -4,10 +4,10 @@ import { useFlashMessageFetchApiData } from 'utils';
 const useFetchTagById = (tagId, setTag) => {
   const fetchApiData = useFlashMessageFetchApiData(`tag/${tagId}`, {}, setTag, '', 'Failed to get Tags');
 
-  /* eslint-disable-next-line react-hooks/exhaustive-deps */
   useEffect(() => {
     (tagId === -1 || tagId === "-1") && setTag({ _id: tagId });
     (tagId !== "-1" && tagId !== -1) && fetchApiData();
-  }, [tagId, setTag]);
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+  }, [tagId]);
 };
 export default useFetchTagById;
