f8be837c2dbaee4dc93403d140bea79ed8dbebb7
larry adams <ladams1776@yahoo.com>
2020-09-06 21:17:50 -0400

    Update TaskListView to receive setTasks, so it can pass it down to individual tasks.
    this will allow individual tasks to fetch tasks, when it is clicked.

--- src/pages/home/TaskListView/TaskListView.js
+++ src/pages/home/TaskListView/TaskListView.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import cn from 'classnames';
-import { useSetCurrentLocation } from 'hooks';
 import Task from './Task/Task';
 import styles from './TaskListView.module.css';
+import useTaskEditContext from '../hooks/useTaskEditContext';
 
-const TaskListView = ({ className, taskId, tasks }) => {
-  useSetCurrentLocation('/');
-
+const TaskListView = ({ className, taskId, tasks, setTasks }) => {
+  const { state } = useTaskEditContext();
   return <div className={cn(
     className,
     styles.task,
     { [styles.listViewAndTask]: taskId })}
     data-test-id="list-view">
     <ul className={styles.taskList}>
-      {tasks.map(task => (
-        <li key={task._id} className={cn(styles.task)}>
-          <Task {...task} key={task._id} selectedId={taskId} />
-        </li>
-      ))}
+      {tasks.map(task => {
+        const desc = (task._id === state.id) ? state.description : task.description;
+        return (
+          <li key={task._id} className={cn(styles.task)}>
+            <Task key={task._id}
+              {...task}
+              description={desc}
+              selectedId={taskId}
+              setTasks={setTasks} />
+          </li>
+        )
+      })}
     </ul>
   </div >
 };
