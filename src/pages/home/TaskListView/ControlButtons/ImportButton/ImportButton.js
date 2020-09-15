b5f37e6b1d3de35c07aa3da136839e4047a2f5c4
larry adams <ladams1776@yahoo.com>
2020-09-14 23:34:04 -0400

    Fill out the import button more

--- src/pages/home/TaskListView/ControlButtons/ImportButton/ImportButton.js
+++ src/pages/home/TaskListView/ControlButtons/ImportButton/ImportButton.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import cn from 'classnames';
 import { Button } from 'components';
+import { fetchApiData } from 'utils';
 import styles from './ImportButton.module.css';
 
 const ImportButton = tasks => {
-    const importDownload = () => {
 
+    const importDownload = e => {
+        e.preventDefault();
+        fetchApiData('import', { method: 'POST', body: e.target.files[0] }, () => {});
     };
 
-    return (
-        <>
-            {tasks.length || (
-                <Button
-                    className={cn(styles.importButton, "glyphicon glyphicon-arrow-up mr-5px")}
-                    onClick={importDownload}
-                    data-test-id="btn-import">
-                    <input type="file" name="file" className={styles.file}/>
-                </Button>)
-            }
-        </>);
+    return (<>
+        {tasks.length || (
+            <Button
+                className={cn(styles.importButton, "glyphicon glyphicon-arrow-up mr-5px")}
+                data-test-id="btn-import"
+                onClick={() => { }}>
+                <input type="file" name="file" className={styles.file} onChange={importDownload} />
+            </Button>)
+        }
+    </>);
 };
 
 export default ImportButton;
