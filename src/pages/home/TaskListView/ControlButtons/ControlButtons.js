8adc00995e945502920bc0dc07c8e78a8aa7208f
larry adams <ladams1776@yahoo.com>
2020-09-14 21:49:37 -0400

    Add import button to Control buttons

--- src/pages/home/TaskListView/ControlButtons/ControlButtons.js
+++ src/pages/home/TaskListView/ControlButtons/ControlButtons.js
@@ -3,11 +3,13 @@ import DownloadButton from './DownloadButton/DownloadButton';
 import NewButton from './NewButton/NewButton';
 import styles from './ControlButtons.module.css';
 import HomeButton from './HomeButton/HomeButton';
+import ImportButton from './ImportButton/ImportButton';
 
 const ControlButtons = tasks => {
   return <div className={styles.controlButtons} data-test-id="control-buttons">
     <HomeButton />
     <DownloadButton tasks={tasks} />
+    <ImportButton />
     <NewButton />
   </div>
 };
