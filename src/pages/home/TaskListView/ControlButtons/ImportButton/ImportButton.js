e1cf1b4b38dae8302cd92d382dcf6cb3729ac95e
larry adams <ladams1776@yahoo.com>
2020-09-23 19:09:44 -0400

    Update ImportButton
    Clean up some of the unnecessary comments.

--- src/pages/home/TaskListView/ControlButtons/ImportButton/ImportButton.js
+++ src/pages/home/TaskListView/ControlButtons/ImportButton/ImportButton.js
@@ -1,36 +1,17 @@
 import React from 'react';
 import cn from 'classnames';
 import { Button } from 'components';
-import { fetchApiData } from 'utils';
 import styles from './ImportButton.module.css';
 
 const ImportButton = () => {
-
-    // const importDownload = async e => {
-    //     e.preventDefault();
-    //     console.log('yeah ', e.target.files)
-    //     const form = new FormData();
-    //     form.append("file", e.target.files[0], e.target.files[0].name);
-
-    //     const response = await fetch(`/api/import`, { method: 'POST', FormData: form });
-    //     const data = response.json();
-    //     console.log('data: ', data);
-    // };
-
     return (
         <form method="post" encType="multipart/form-data" action="/api/import">
-            {/* <Button
-                className={cn(styles.importButton, "glyphicon glyphicon-arrow-up mr-5px")}
-                data-test-id="btn-import"
-                onClick={() => { }}> */}
-                <input type="file" name="file" />
+            <input type="file" name="file" />
             <Button
                 className={cn(styles.importButton, "glyphicon glyphicon-arrow-up mr-5px")}
                 data-test-id="btn-import"
                 onClick={() => { }}
-                type="submit"/>
-                {/* <input type="submit" value="upload"/> */}
-            {/* </Button> */}
+                type="submit" />
         </form>
     );
 };
