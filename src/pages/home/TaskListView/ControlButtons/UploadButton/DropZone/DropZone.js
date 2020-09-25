0c09ae2c9161426d3f8b60e0e6412117a6295ea9
larry adams <ladams1776@yahoo.com>
2020-09-24 20:14:26 -0400

    Create the DropZone Component

---
+++ src/pages/home/TaskListView/ControlButtons/UploadButton/DropZone/DropZone.js
@@ -0,0 +1,42 @@
+import React, { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { useBrowserHistory, useFlashMessageContext } from 'hooks';
+import { fetchApiData } from "utils";
+import styles from './DropZone.module.css'
+
+
+const DropZone = ({ className, onClick }) => {
+    const { setSuccessFlashMessage } = useFlashMessageContext();
+    const { push} = useBrowserHistory();
+    const onDrop = useCallback(acceptedFiles => {
+        const reader = new FileReader()
+
+        //@TODO: Could error message in these cases
+        reader.onabort = () => console.log('file reading was aborted')
+        reader.onerror = () => console.log('file reading has failed')
+        reader.onload = () => {
+            const binaryStr = JSON.parse(reader.result);
+            fetchApiData('import', { body: binaryStr, method: 'POST' }, () => { 
+                onClick(); 
+                setSuccessFlashMessage('Successfully Saved');
+                push("/task/-1");
+            });
+        }
+        reader.readAsBinaryString(acceptedFiles[0]);
+    });
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+    return (<div className={className}>
+        <button onClick={() => onClick()} className={styles.exitButton}>X</button>
+        <div {...getRootProps()} className={styles.container}>
+            <input {...getInputProps()} className={styles.innerContainer} />
+            {
+                isDragActive
+                    ? <p className={styles.dropText}>Drop the files here...</p>
+                    : <p className={styles.dropText}>Drag 'n' drop files here, or click to select file</p>
+            }
+        </div>
+    </div>);
+};
+
+export default DropZone;
