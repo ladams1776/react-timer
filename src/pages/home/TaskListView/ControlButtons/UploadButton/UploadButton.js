6b86c9436b90ab44090d358cdf38f9492bc6b73d
larry adams <ladams1776@yahoo.com>
2020-09-24 20:12:51 -0400

    Create an UploadButton.
    It will invoke the UploadModal.

---
+++ src/pages/home/TaskListView/ControlButtons/UploadButton/UploadButton.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import cn from 'classnames';
+import UploadModal from './UploadModal';
+import styles from './UploadButton.module.css';
+
+const UploadButton = () => {
+    const [isModalShowing, setIsModalShowing] = useState(false);
+
+    return (<>
+        <button className={cn(styles.uploadButton, "glyphicon glyphicon-arrow-up mr-5px")} onClick={setIsModalShowing} />
+        <UploadModal isShowing={isModalShowing} setIsShowing={setIsModalShowing} />
+    </>);
+};
+
+export default UploadButton;
