3d7a754ff6be1e0a970a720fd3c8b65414ebc57c
larry adams <ladams1776@yahoo.com>
2020-10-23 22:18:08 -0400

    Update TaskRepository Include each date time to get ready to display in UI.

--- server/infrastructure/repositories/tasks/TaskRepository.js
+++ server/infrastructure/repositories/tasks/TaskRepository.js
@@ -19,9 +19,28 @@ const TaskRepository = {
         .map(a => parseInt(a.time))
         .reduce((a, b) => a + b);
 
+      task.dateTime = doc.time
+        .map(dateTime => {
+          const date = dateTime.date?.toISOString()
+            .replace(/T/, ' ')
+            .replace(/\..+/, '');
+
+          const time = millisToMinutesAndSeconds(dateTime.time);
+          return { date, time };
+        });
+
       callback(task);
     }
   },
 };
+
+const millisToMinutesAndSeconds = (millis) => {
+  const hours = Math.floor((millis / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor(millis / 60000);
+  const seconds = ((millis % 60000) / 1000).toFixed(0);
+  //ES6 interpolated literals/template literals 
+  //If seconds is less than 10 put a zero in front.
+  return `${hours}:${minutes}:${(seconds < 10 ? "0" : "")}${seconds}`;
+}
 
 module.exports = TaskRepository;
