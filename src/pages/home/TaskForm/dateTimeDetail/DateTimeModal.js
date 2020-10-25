54cd879f7fc3c16c833a468777981f2ab142ac99
larry adams <ladams1776@yahoo.com>
2020-10-24 20:53:32 -0400

    Fix up the styles for DateTimeModal

--- src/pages/home/TaskForm/dateTimeDetail/DateTimeModal.js
+++ src/pages/home/TaskForm/dateTimeDetail/DateTimeModal.js
@@ -3,12 +3,14 @@ import styles from './DateTimeModal.module.css'
 
 const DateTimeModal = ({ dateTime }) => {
     return <div className={styles.modal}>
-        {dateTime.map(dT => {
-            return (<div className={styles.content} key={dT.date + dT.time}>
-                <div className={styles.date}>Date:{dT.date}</div>
-                <div className={styles.time}>Minutes: {dT.time}</div>
-            </div>)
-        })}
+        <div className={styles.modalContent}>
+            {dateTime.map(dT => {
+                return (<div className={styles.content} key={dT.date + dT.time}>
+                    <div className={styles.date}>Date:{dT.date}</div>
+                    <div className={styles.time}>Minutes: {dT.time}</div>
+                </div>)
+            })}
+        </div>
     </div>
 };
 
