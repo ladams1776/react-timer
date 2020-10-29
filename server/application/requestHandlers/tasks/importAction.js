3be9de1aeb34333373a5ae2b58c4c2357d750b97
larry adams <ladams1776@yahoo.com>
2020-10-28 23:23:02 -0400

    Update Import Action
    Carry over the tags for the tasks, when importing.
    Save the tags, if any are not in the system.

--- server/application/requestHandlers/tasks/importAction.js
+++ server/application/requestHandlers/tasks/importAction.js
@@ -1,14 +1,27 @@
 const Task = require("../../../infrastructure/models/Task");
+const TagService = require("../../../domain/services/tags/TagService");
+
 //@TODO: Clean this up
 module.exports = (req, res) => {
+    console.log('yum yum');
     [...req.body.WorkUnit[0].tasks.tasks].map(task => {
-        let t = new Task();
-        t.toObject();
-        t.time = task.time;
-        t.contractId = task.contractId;
-        t.description = task.description;
-        t.date = task.date;
-        return t.save((err, task) => {
+        console.log('task is: ', task?.tags);
+
+        
+        task?.tags.map(tag => {
+            const getTag = (items) => items.items;
+            const storedTag = TagService.fetchTagById(tag._id, getTag);
+            if (storedTag.error) TagService.addTag(tag, getTag);
+        });
+
+        let saveableTask = new Task();
+        saveableTask.toObject();
+        saveableTask.time = task.time;
+        saveableTask.contractId = task.contractId;
+        saveableTask.description = task.description;
+        saveableTask.date = task.date;
+        saveableTask.tags = task.tags;
+        return saveableTask.save((err, task) => {
             if (err) throw err;
         });
     });
