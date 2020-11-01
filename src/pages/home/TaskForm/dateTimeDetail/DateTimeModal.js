3bccd528548d4e8a45c4d63980341cfde5567090
larry adams <ladams1776@yahoo.com>
2020-11-01 17:21:23 -0500

    Update DateTimeModal
    to format the date/time, but I think I want to do this better. Going to revisit this

--- src/pages/home/TaskForm/dateTimeDetail/DateTimeModal.js
+++ src/pages/home/TaskForm/dateTimeDetail/DateTimeModal.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment-timezone';
 import styles from './DateTimeModal.module.css'
 
 const DateTimeModal = ({ dateTime, setIsShowing }) => {
+
+    const myTimezone = "America/Toronto";
+    const myDatetimeFormat = "YYYY-MM-DD hh:mm:ss a z";
+
     return <div className={styles.modal}>
         <div className={styles.modalContent}>
             {dateTime.map(dT => {
+                const myDatetimeString = moment(dT.date).tz(myTimezone).format(myDatetimeFormat);
+
                 return (<div className={styles.content} key={dT.date + dT.time}>
-                    <div className={styles.date}>Date:{dT.date}</div>
+                    <div className={styles.date}>Date:{myDatetimeString}</div>
                     <div className={styles.time}>Minutes: {dT.time}</div>
                 </div>)
             })}
