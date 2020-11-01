db8ed3c3f4130ce711bc003e50bd479ab06b43f3
larry adams <ladams1776@yahoo.com>
2020-11-01 17:20:51 -0500

    Update the export file for getCurrentDateTimeEstFormat

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-export { default as getFormattedDate } from './formatters/getFormattedDate';
+export { default as getCurrentDateTimeEstFormat } from './formatters/getCurrentDateTimeEstFormat';
 export { default as displayMsInFractionalHourFormat } from './formatters/displayMsInFractionalHourFormat';
 export { default as fetchApiData } from './api/fetchApiData/fetchApiData';
 export { default as selectNormalizer } from './normalizers/selectNormalizer';
