7a3d94d642df19a5d3102ea99fd244cc369ecd9a
larry adams <ladams1776@yahoo.com>
2020-11-01 09:54:43 -0500

    Update getFormattedDate
    I just removed the argument, all clients did not need to pass in Date.

--- src/utils/formatters/__test__/getFormattedDate.test.js
+++ src/utils/formatters/__test__/getFormattedDate.test.js
@@ -3,17 +3,20 @@ import getFormattedDate from '../getFormattedDate';
 describe('src/utils/__test__/DateFormat.test.js', () => {
   describe('getFormattedDate', () => {
     it('should format the time correctly - 1 hr', () => {
+      // Arrange
       const todaysDate = new Date();
 
-      const todaysFormattedDate = getFormattedDate(todaysDate);
-      expect(todaysFormattedDate).toEqual(
-        todaysDate.getMonth() +
-          1 +
-          '/' +
-          todaysDate.getDate().toString() +
-          '/' +
-          todaysDate.getFullYear().toString()
-      );
+      const expected = (parseInt(todaysDate.getMonth()) + 1)
+        + '/'
+        + todaysDate.getDate().toString()
+        + '/'
+        + todaysDate.getFullYear().toString();
+
+      // Act
+      const todaysFormattedDate = getFormattedDate();
+
+      // Assert
+      expect(todaysFormattedDate).toEqual(expected);
     });
   });
 });
--- src/utils/formatters/getFormattedDate.js
+++ src/utils/formatters/getFormattedDate.js
@@ -1,11 +1,8 @@
-/**
- * Take in a regular date and format it into a more readable format:
- *
- * @param {Date} taskDate: 2019-11-17T17:38:50.575Z
- * @returns 11/17/2019
- */
-export default function getFormattedDate(taskDate) {
-  const existingDate = new Date(taskDate);
+
+export default function getFormattedDate() {
+
+  const existingDate = new Date();
+
   const dateFormatted =
     existingDate.getMonth() +
     1 +
