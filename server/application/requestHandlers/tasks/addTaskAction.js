8577c66593fd463e6c0d3cbb671992c8a32365e7
larry adams <ladams1776@yahoo.com>
2020-11-01 10:29:17 -0500

    Update addTaskAction, just removing unused code.

--- server/application/requestHandlers/tasks/addTaskAction.js
+++ server/application/requestHandlers/tasks/addTaskAction.js
@@ -20,22 +20,3 @@ module.exports = async (req, res) => {
     });
     res.jsonp(m);
 };
-
-// /**
-//  * Take in a regular date and format it into a more readable format:
-//  *
-//  * @param {Date} taskDate: 2019-11-17T17:38:50.575Z
-//  * @returns 11/17/2019
-//  */
-// export default function getFormattedDate() {
-//     const existingDate = new Date();
-//     const dateFormatted =
-//         existingDate.getMonth() +
-//         1 +
-//         '/' +
-//         existingDate.getDate().toString() +
-//         '/' +
-//         existingDate.getFullYear().toString();
-//     return dateFormatted;
-// }
-
