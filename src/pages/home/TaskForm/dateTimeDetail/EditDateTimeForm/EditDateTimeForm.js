165d95012fd16d125131e051f0c8971a331ae02c
larry adams <ladams1776@yahoo.com>
2020-11-07 10:00:08 -0500

    Create EditDateTimeForm with test suite

---
+++ src/pages/home/TaskForm/dateTimeDetail/EditDateTimeForm/EditDateTimeForm.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types'
+import styles from './EditDateTimeForm.module.css';
+import useForm from './useForm';
+
+const EditDateTimeForm = ({ editDateTime }) => {
+    const { onClick, dateTime, setDateTime } = useForm(editDateTime);
+
+    return <div className={styles.form}>
+        <form data-testid="editDateTimeForm">
+            <input type="hidden" name="id" value={dateTime.id} />
+            <div className={styles.date}>Date: <input value={dateTime.date} name="date" onChange={e => setDateTime({ ...dateTime, date: e.target.value })} /></div>
+            <div className={styles.minutes}>Minutes: <input value={dateTime.minutes} name="minutes" onChange={e => setDateTime({ ...dateTime, minutes: e.target.value })} /></div>
+            <button type="submit" className={styles.submit} onClick={onClick}>Submit</button>
+        </form>
+    </div>
+};
+
+EditDateTimeForm.prototype = {
+    id: PropTypes.string,
+    date: PropTypes.string,
+    minutes: PropTypes.number
+};
+
+export default EditDateTimeForm;
