4dc6a0f2c1891c7d50515dc45feb5d9a3723e996
larry adams <ladams1776@yahoo.com>
2020-11-07 08:31:50 -0500

    Update DateTimeModal To afford editing of datetimes

--- src/pages/home/TaskForm/dateTimeDetail/DateTimeModal.js
+++ src/pages/home/TaskForm/dateTimeDetail/DateTimeModal.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment-timezone';
-import styles from './DateTimeModal.module.css'
+import styles from './DateTimeModal.module.css';
+import EditDateTime from './EditDateTime/EditDateTime';
 
+//@TODO: Probs do the fetching for the DateTimes, when the Modal is pulled up.
 const DateTimeModal = ({ dateTimes, setIsShowing }) => {
+    const [editDateTime, setEditDateTime] = useState({});
 
     const myTimezone = "America/New_York";
     const myDatetimeFormat = "YYYY-MM-DD hh:mm:ss a";
 
     return <div className={styles.modal}>
         <div className={styles.modalContent}>
-            {dateTimes.map(dT => {
-                const myDatetimeString = moment(dT.date)
-                    .tz(myTimezone)
-                    .format(myDatetimeFormat);
+            {!editDateTime?.id
+                && (dateTimes.map(dT => {
 
-                return (<form
-                    className={styles.taskForm}
-                    method='PUT'>
-                    <div className={styles.content} key={dT.id}>
+                    //@TODO: abstract this stuff out
+                    const myDatetimeString = moment(dT.date)
+                        .tz(myTimezone)
+                        .format(myDatetimeFormat);
+
+                    const onClick = () => {
+                        setEditDateTime({ id: dT.id, date: dT.date, minutes: dT.time })
+                    };
+
+
+                    return <div className={styles.content} key={dT.id} onClick={onClick}>
+                        <input type="hidden" value={dT.id} name="id" />
                         <div className={styles.date}>Date: {myDatetimeString}</div>
-                        <div className={styles.time}>Minutes: <input onChange={e => {}} value={dT.time} className={styles.time}/></div>
+                        <div className={styles.time}>Minutes: {dT.time}</div>
                     </div>
-                </form>)
-            })}
+                }))
+                || (<EditDateTime setEditDateTime={setEditDateTime} id={editDateTime.id} date={editDateTime.date} minutes={editDateTime.minutes} />)
+            }
+
             <button className={styles.closeButton} onClick={() => setIsShowing(false)}>Close</button>
         </div>
     </div>
