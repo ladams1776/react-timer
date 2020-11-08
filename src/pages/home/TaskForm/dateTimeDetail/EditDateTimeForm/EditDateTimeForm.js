7563e698fdc2d1685b82bb048f474392ca57fa79
larry adams <ladams1776@yahoo.com>
2020-11-08 09:29:46 -0500

    Create test suite for useForm

--- src/pages/home/TaskForm/dateTimeDetail/EditDateTimeForm/EditDateTimeForm.js
+++ src/pages/home/TaskForm/dateTimeDetail/EditDateTimeForm/EditDateTimeForm.js
@@ -4,14 +4,14 @@ import styles from './EditDateTimeForm.module.css';
 import useForm from './useForm';
 
 const EditDateTimeForm = ({ editDateTime }) => {
-    const { onClick, dateTime, setDateTime } = useForm(editDateTime);
+    const { onSubmit, dateTime, setDateTime } = useForm(editDateTime);
 
     return <div className={styles.form}>
         <form data-testid="editDateTimeForm">
             <input type="hidden" name="id" value={dateTime.id} />
             <div className={styles.date}>Date: <input value={dateTime.date} name="date" onChange={e => setDateTime({ ...dateTime, date: e.target.value })} /></div>
             <div className={styles.minutes}>Minutes: <input value={dateTime.minutes} name="minutes" onChange={e => setDateTime({ ...dateTime, minutes: e.target.value })} /></div>
-            <button type="submit" className={styles.submit} onClick={onClick}>Submit</button>
+            <button type="submit" className={styles.submit} onClick={onSubmit}>Submit</button>
         </form>
     </div>
 };
--- src/pages/home/TaskForm/dateTimeDetail/EditDateTimeForm/useForm.js
+++ src/pages/home/TaskForm/dateTimeDetail/EditDateTimeForm/useForm.js
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 import { fetchApiData } from 'utils';
 
+export const onFormSubmit = (dateTime, setDateTime) => e => {
+    e.preventDefault();
+    const config = { body: { id: dateTime.id, date: dateTime.date, minutes: dateTime.minutes }, method: 'PUT' };
+    fetchApiData(`dateTime/${dateTime.id}`, config, setDateTime);
+};
+
 const useForm = (editDateTime) => {
     const [dateTime, setDateTime] = useState(editDateTime);
-    const onClick = e => {
-        e.preventDefault();
-        const config = { body: { id: dateTime.id, date: dateTime.date, minutes: dateTime.minutes }, method: 'PUT' };
-        fetchApiData(`dateTime/${dateTime.id}`, config, setDateTime);
-    };
-
-    return { onClick, dateTime, setDateTime };
+    const onSubmit = onFormSubmit(dateTime, setDateTime);
+    return { onSubmit, dateTime, setDateTime };
 }
 
 export default useForm;
