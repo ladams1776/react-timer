8c767874fcf597f27b6803909430988c67dd947a
larry adams <ladams1776@yahoo.com>
2020-11-11 21:05:48 -0500

    Move TaskRepository over to single purpose Repo file and update the TaskService

--- server/domain/services/tasks/TaskService.js
+++ server/domain/services/tasks/TaskService.js
@@ -1,12 +1,13 @@
 const FetchAllTasksRepository = require('../../../infrastructure/repositories/tasks/Repositories/FetchAllTasksRepository');
-const TaskRepository = require('../../../infrastructure/repositories/tasks/TaskRepository');
+const FetchTaskByIdRepository = require('../../../infrastructure/repositories/tasks/Repositories/FetchTaskByIdRepository');
 
 const TaskService = {
   fetchAllTasks: () => FetchAllTasksRepository(),
   /**
    * @param {String} taskId
+   * @param {Function} callback
    */
-  fetchTaskById: (taskId, callback) => TaskRepository.fetchTaskById(taskId, callback),
+  fetchTaskById: (taskId, callback) => FetchTaskByIdRepository(taskId, callback),
 };
 
 module.exports = TaskService;
