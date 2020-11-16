b08c684e018a29b04679f3934cdab57468422924
larry adams <ladams1776@yahoo.com>
2020-11-15 23:11:10 -0500

    Update UpdateDateTimeAction with the TaskService

--- server/application/requestHandlers/tasks/__test__/updateDateTimeAction.test.js
+++ server/application/requestHandlers/tasks/__test__/updateDateTimeAction.test.js
@@ -1,7 +1,34 @@
-describe('server/application/requestHandler/tasks/__test__/updateDateTimeAction.test.js', () => {
-    describe('updateDateTimeAction', () => {
-        it('');
-    });
+const TaskService = require('../../../../domain/services/tasks/TaskService');
+const jsonResponse = require('../../jsonResponse');
+const UpdateDateTimeAction = require('../updateDateTimeAction');
 
-    describe('')
+jest.mock('../../../../domain/services/tasks/TaskService');
+jest.mock('../../jsonResponse');
+
+describe('server/application/requestHandler/tasks/__test__/UpdateDateTimeAction.test.js', () => {
+    describe('UpdateDateTimeAction', () => {
+        it('should return updated task with the updated date and time', () => {
+            // Arrange
+            const dateTime = { _id: 'dateTimeID' };
+            const req = {
+                params: { taskId: 'taskId' },
+                body: dateTime
+            };
+            const res = jest.fn();
+            const expected = {
+                _id: 'taskId',
+                dateTime
+            };
+            const responder = jest.fn();
+            jsonResponse.mockImplementation(d => responder);
+            TaskService.updateDateTimeOfTask = jest.fn().mockImplementation(() => expected);
+
+            // Act
+            UpdateDateTimeAction(req, res);
+
+            // Assert
+            expect(TaskService.updateDateTimeOfTask).toBeCalledWith(expected._id, dateTime);
+            expect(responder).toBeCalledWith(expected);
+        });
+    });
 });
--- server/application/requestHandlers/tasks/updateDateTimeAction.js
+++ server/application/requestHandlers/tasks/updateDateTimeAction.js
@@ -1,37 +1,12 @@
-const Task = require('../../../infrastructure/models/Task');
-const hydrateAndResponse = require('../../../infrastructure/hydrators/hydrateAndResponse');
 const jsonResponse = require('../jsonResponse');
-const minutesToMilliseconds = require('../../../utils/minutesToMilliseconds');
+const TaskService = require('../../../domain/services/tasks/TaskService');
 
-
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
     const responder = jsonResponse(res);
-    const hydrate = hydrateAndResponse(responder);
-    console.log('Updating a DateTime');
-    const taskId = req.params.taskId;
+
+    const id = req.params.taskId;
     const dateTime = req.body;
-
-    const task = await Task.findOne({ _id: taskId });
-
-    const dateTimes = task.time.map(dT => {
-        if (dT._id == dateTime.id) {
-            console.log('dateTime is: ', dateTime)
-            const newDateTime =  {
-                _id: dT._id,
-                date: dateTime.date,
-                time: minutesToMilliseconds(dateTime.minutes),
-            };
-
-            console.log('newDateTime: ', newDateTime);
-            return newDateTime;
-        } else {
-            return dT;
-        }
-    });
-
-    task.time = dateTimes;
-    await task.save(hydrate)
-
-    // User.update({ email: 'joe@foo.com' }, { 'profile.name.first': 'Joseph' }, callback)
-    // Task.update({_id: req.body.id})
+    
+    const updatedTaskWithDateTime = TaskService.updateDateTimeOfTask(id, dateTime);
+    responder(updatedTaskWithDateTime);
 };
