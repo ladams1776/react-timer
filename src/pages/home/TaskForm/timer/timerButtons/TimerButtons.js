8a9f8419290aefa5aaf3960b5c0be40456d501c7
larry adams <ladams1776@yahoo.com>
2020-11-18 22:14:49 -0500

    Update TimerButtons and add test suite

--- src/pages/home/TaskForm/timer/timerButtons/TimerButtons.js
+++ src/pages/home/TaskForm/timer/timerButtons/TimerButtons.js
@@ -1,59 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import cn from 'classnames';
 import { Button } from 'components';
 import styles from './TimerButtons.module.css';
 
-const TimerButtons = ({ time, setTime, isActive = false, setIsActive, children }) => {
+const TimerButtons = ({ time, isActive = false, setIsActive, children }) => {
     const toggle = () => setIsActive(!isActive);
-
-    const reset = e => {
-        e.preventDefault();
-        setIsActive(false);
-        setTime(0);
-    };
 
     return <div className={cn("navbar navbar-default")}>
         <div className="container-fluid">
             <div className={styles.navbarHeader}>
-                <Button
-                    title="Reset"
-                    className={cn(styles.timerReset)}
-                    data-test-id="timerReset"
-                    onClick={reset}
-                    disabled={time === 0}>
-                    <span className={cn(styles.resetIcon, "glyphicon glyphicon-refresh")} />
-                </Button>
-
-                {isActive && (
-                    <Button className={styles.timerStop}
-                        title="Stop"
-                        data-test-id="timerStop"
-                        onClick={toggle}>
-                        <span className={cn(styles.stopIcon, "glyphicon glyphicon-pause")} />
-                    </Button>
+                
+                {isActive && (<Button
+                    className={cn(styles.timerStop, styles.stopIcon, "glyphicon glyphicon-pause")}
+                    title="Stop"
+                    onClick={toggle} />
                 )}
 
-                {time !== 0 && !isActive && (
-                    <Button className={styles.timerResume}
-                        title="Resume"
-                        onClick={toggle}>
-                        <span className={cn(styles.stopIcon, "glyphicon glyphicon-play")} />
-                    </Button>
+                {time !== 0 && !isActive && (<Button
+                    className={cn(styles.timerResume, styles.stopIcon, "glyphicon glyphicon-play")}
+                    title="Resume"
+                    testid="resume"
+                    onClick={() => setIsActive(!isActive)} />
                 )}
 
-                {time === 0 && !isActive && (
-                    <Button className={styles.timerResume}
-                        title="Start"
-                        onClick={toggle}>
-                        <span className={cn(styles.stopIcon, "glyphicon glyphicon-play")} />
-                    </Button>
+                {time === 0 && !isActive && (<Button
+                    className={cn(styles.timerStart, styles.stopIcon, "glyphicon glyphicon-play")}
+                    title="Start"
+                    onClick={toggle} />
                 )}
+
                 <div className={styles.children}>
                     {children}
                 </div>
             </div>
         </div>
     </div>
+};
+
+TimerButtons.propTypes = {
+    time: PropTypes.number.isRequired,
+    isActive: PropTypes.bool.isRequired,
+    setIsActive: PropTypes.func.isRequired,
+    children: PropTypes.element,
 };
 
 export default TimerButtons;
