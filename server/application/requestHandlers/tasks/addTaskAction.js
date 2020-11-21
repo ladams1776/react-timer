e01ce496df16e7f23c94653005959094af4c6d18
larry adams <ladams1776@yahoo.com>
2020-11-21 11:00:54 -0500

    Fix AddTaskAction
    it was not saving for us. We actually don't need any details at the initial creation

--- server/application/requestHandlers/tasks/addTaskAction.js
+++ server/application/requestHandlers/tasks/addTaskAction.js
@@ -5,15 +5,9 @@ const Task = require('../../../infrastructure/models/Task');
 module.exports = async (req, res) => {
     const m = new Task();
     m.toObject();
-    m.date = req.body.date;
-    m.description = req.body.WorkUnit[0].description;
-    m.contractId = req.body.WorkUnit[0].contractId;
-
-    m.time = [{
-        time: req.body.WorkUnit[0].time,
-        date: req.body.date
-    }];
-    m.tags = req.body.WorkUnit[0].tags;
+    m.date = '';
+    m.description = '';
+    m.contractId = '';
 
     const t = await m.save((err, task) => {
         if (err) throw err;
