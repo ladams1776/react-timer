96a223b78865443263b75f7b489d943830ef73d0
larry adams <ladams1776@yahoo.com>
2020-12-02 18:01:19 -0500

    Update useFetchTaskById to use the new function

--- src/pages/home/TaskForm/hooks/useFetchTaskById/__test__/useFetchTaskById.test.js
+++ src/pages/home/TaskForm/hooks/useFetchTaskById/__test__/useFetchTaskById.test.js
@@ -1,89 +1,52 @@
 import { renderHook, act } from '@testing-library/react-hooks';
-import useLoadinSpinnerContext from 'hooks/useLoadinSpinnerContext';
-import fetchApiData from 'utils/api/fetchApiData/fetchApiData';
 import useFetchTaskById from '../useFetchTaskById';
+
 import useTimeContext from '../../useTimeContext';
+import useDispatch from '../useDispatch';
+import useFlashMessageFetchApiData from 'utils/api/useFlashMessageFetchApiData';
 
-jest.mock('hooks/useLoadinSpinnerContext');
-jest.mock('utils/api/fetchApiData/fetchApiData');
 jest.mock('../../useTimeContext');
+jest.mock('../useDispatch');
+jest.mock('utils/api/useFlashMessageFetchApiData');
 
-describe('src/hooks/__test__/useFetchTaskById.test.js', () => {
-  describe('#useFetchTaskById', () => {
+describe('useFetchTaskById', () => {
+  it("should call fetchApiData, when taskId is anything but '-1'", () => {
     // Arrange
-    const useTimeContextStub = {
-      setTime: jest.fn(),
-    };
-    const useLoadinSpinnerContextStub = {
-      setIsLoadin: jest.fn(),
-    };
+    const taskId = 1;
+    const dispatchTaskSpy = jest.fn();
+    const setTimeSpy = jest.fn();
+    const fetchApiDataSpy = jest.fn();
 
-    const dispatch = jest.fn();
+    useTimeContext.mockImplementationOnce(() => setTimeSpy);
+    useDispatch.mockImplementationOnce(() => dispatchTaskSpy);
+    useFlashMessageFetchApiData.mockImplementationOnce(() => fetchApiDataSpy);
+    
+    // Act
+    const { result } = renderHook(() => useFetchTaskById(taskId, dispatchTaskSpy));
+    act(() => result.current);
 
-    beforeEach(() => {
-      fetchApiData.mockReset();
-      dispatch.mockReset();
-      useTimeContextStub.setTime.mockReset();
-      useTimeContext.mockReturnValue(useTimeContextStub);
-      useLoadinSpinnerContextStub.setIsLoadin.mockReset();
-      useLoadinSpinnerContext.mockReturnValue(useLoadinSpinnerContextStub);
-    });
+    // Assert
+    expect(fetchApiDataSpy).toHaveBeenCalled();
+    expect(dispatchTaskSpy).not.toHaveBeenCalled();
+  });
 
-    it("should call fetchApiData, when taskId is anything but '-1'", async () => {
-      // Arrange
-      const taskId = 1;
-      const data = {
-        _id: 1,
-        contractId: 1,
-        tags: [1],
-        description: 1,
-        time: 1,
-      };
-      fetchApiData.mockReturnValue((a, b, c) => {
-        return c(data);
-      });
-      
-      // Act
-      const { result } = renderHook(() => useFetchTaskById(taskId, dispatch));
-      await act(async () => result.current);
+  it("should not call fetchApiData(), when taskId is '-1'", () => {
+    // Arrange
+    const taskId = -1;
+    const dispatchTaskSpy = jest.fn();
+    const setTimeSpy = jest.fn();
+    const fetchApiDataSpy = jest.fn();
 
-      // Assert
-      expect(useLoadinSpinnerContextStub.setIsLoadin).toHaveBeenCalledWith(
-        true,
-      );
-      expect(fetchApiData).toHaveBeenNthCalledWith(
-        1,
-        `task/${taskId}`,
-        {},
-        expect.anything(),
-      );
-      expect(useLoadinSpinnerContextStub.setIsLoadin).toHaveBeenNthCalledWith(
-        1,
-        true,
-      );
+    useTimeContext.mockImplementationOnce(() => setTimeSpy);
+    useDispatch.mockImplementationOnce(() => dispatchTaskSpy);
+    useFlashMessageFetchApiData.mockImplementationOnce(() => fetchApiDataSpy);
 
-      expect(dispatch).not.toBeCalled();
-    });
+    // Act
+    const { result } = renderHook(() => useFetchTaskById(taskId, dispatchTaskSpy));
+    act(() => result.current);
 
-    it("should not call fetchApiData(), when taskId is '-1'", async () => {
-      // Arrange
-      const taskId = '-1';
-
-      // Act
-      const { result } = renderHook(() => useFetchTaskById(taskId, dispatch));
-      await act(async () => result.current);
-
-      // Assert
-      expect(useLoadinSpinnerContextStub.setIsLoadin).toHaveBeenCalledWith(
-        true,
-      );
-      expect(fetchApiData).not.toBeCalled();
-      expect(dispatch).toHaveBeenNthCalledWith(1, { time: 0 });
-      expect(useLoadinSpinnerContextStub.setIsLoadin).toHaveBeenCalledWith(
-        false,
-      );
-      expect(useLoadinSpinnerContextStub.setIsLoadin).toHaveBeenCalledTimes(2);
-      expect(dispatch).toHaveBeenNthCalledWith(1, { time: 0 });
-    });
+    // Assert
+    expect(dispatchTaskSpy).toHaveBeenCalled();
+    expect(fetchApiDataSpy).not.toHaveBeenCalled();
   });
 });
--- src/pages/home/TaskForm/hooks/useFetchTaskById/useFetchTaskById.js
+++ src/pages/home/TaskForm/hooks/useFetchTaskById/useFetchTaskById.js
@@ -1,40 +1,22 @@
 import { useEffect, useCallback } from 'react';
-import { fetchApiData } from 'utils';
 import useTimeContext from '../useTimeContext';
-import { useLoadinSpinnerContext } from 'hooks';
+import { useFlashMessageFetchApiData } from 'utils';
+import useDispatch from './useDispatch';
 
 //@TODO: Model this after useFetchTagById
-const useFetchTaskById = (taskId, dispatch) => {
+const useFetchTaskById = (taskId, dispatchTask) => {
+
   const { setTime } = useTimeContext();
-  const { setIsLoadin } = useLoadinSpinnerContext();
 
   //@TODO: Not even testing this hydration. I suppose I could, but I want to move on.
-  const taskDispatch = useCallback(
-    data => {
-      const formData = {
-        id: data._id,
-        project: data.contractId,
-        tags: data.tags,
-        description: data.description,
-        time: data.time,
-        dateTimes: data.dateTimes,
-      };
-      setIsLoadin(false);
-      setTime(data.time);
-      return dispatch(formData);
-    },
-    [setIsLoadin, setTime, dispatch],
-  );
+  const taskDispatch = useDispatch(setTime, dispatchTask);
+
+  const fetchApiData = useFlashMessageFetchApiData(`task/${taskId}`, {}, taskDispatch, '', 'Failed to get Task');
 
   return useEffect(() => {
-    setIsLoadin(true);
-
-    if (taskId !== '-1') {
-      fetchApiData(`task/${taskId}`, {}, taskDispatch);
-    } else {
-      taskDispatch({ time: 0 });
-    }
-  }, [taskId, dispatch, setIsLoadin, taskDispatch]);
+    (taskId === -1 || taskId === "-1") && taskDispatch({ time: 0 });
+    (taskId !== "-1" && taskId !== -1) && fetchApiData()
+  }, [taskId]);
 };
 
 export default useFetchTaskById;
