be001212b7acbb87892f4202c7538345e779a79d
larry adams <ladams1776@yahoo.com>
2020-12-12 23:59:40 -0500

    Create Scaffolding for DateTimePage.test.js

---
+++ src/pages/home/TaskForm/form/dateTimePage/page/__test__/DateTimePage.test.js
@@ -0,0 +1,8 @@
+describe('DateTimePage.test.js', () => {
+    it('should show EditDateTimeForm, when editDateTime with id is available', () => {
+
+    });
+    it('should show DateTimeListView, when no editDateTime with id', () => {
+
+    });
+});
