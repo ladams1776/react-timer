ac7915cb8dafc46a7f567904b860e39b87d2dcfe
larry adams <ladams1776@yahoo.com>
2020-12-16 22:57:28 -0500

    Update TextAreaAdapter If we are not online we will use textarea and not TinyMCE. Because TinyMCE requires internet access.

--- src/components/TextAreaAdapter.js
+++ src/components/TextAreaAdapter.js
@@ -3,24 +3,41 @@ import { Editor } from '@tinymce/tinymce-react';
 
 const TextAreaAdapter = ({ description, setDescription, id }) => {
   const toolbar = 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help';
-  return <Editor
-      key={id}
-      value={description}
-      id={id}
-      name="description"
-      data-test-id="text-area-adapter"
-      init={{
-        height: '60vh',
-        menubar: true,
-        plugins: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount',
-        ],
-        toolbar,
-      }}
-      onEditorChange={setDescription}
-    />
+
+  const onHandler = e => {
+    setDescription(e.target.value);
+  }
+
+  const adapter = <Editor
+    key={id}
+    value={description}
+    id={id}
+    name="description"
+    data-test-id="text-area-adapter"
+    init={{
+      height: '60vh',
+      menubar: true,
+      plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table paste code help wordcount',
+      ],
+      toolbar,
+    }}
+    onEditorChange={setDescription}
+  />;
+
+  const textarea = <textarea
+    rows="15"
+    cols="125"
+    name="description"
+    value={description}
+    onChange={onHandler}
+    id={id}
+  />;
+
+  const comp = navigator.onLine ? adapter : textarea;
+  return comp;
 };
 
 export default TextAreaAdapter;
