371ffda5a62a3919188ddc6962ed6940d5de341c
larry adams <ladams1776@yahoo.com>
2020-12-17 23:35:14 -0500

    Update DateTimeListView to include propTypes

--- src/pages/home/TaskForm/form/dateTimePage/dateTimeListView/DateTimeListView.js
+++ src/pages/home/TaskForm/form/dateTimePage/dateTimeListView/DateTimeListView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import DateTimeItem from './DateTimeItem';
 import styles from './DateTimeListView.module.css';
 
@@ -6,9 +7,21 @@ const DateTimeListView = ({ dateTimes, setEditDateTime }) => {
 
     return <div className={styles.grid}>
         {dateTimes?.map(dT => {
-            return <DateTimeItem dateTime={dT} key={dT.id + dT.time} setEditDateTime={setEditDateTime} />
+            return <DateTimeItem
+                dateTime={dT}
+                key={dT.id + dT.time}
+                setEditDateTime={setEditDateTime} />
         })}
     </div>
+}
+
+DateTimeListView.propTypes = {
+    dateTimes: PropTypes.arrayOf(PropTypes.exact({
+        id: PropTypes.string,
+        date: PropTypes.string,
+        time: PropTypes.string
+    })),
+    setEditDateTime: PropTypes.func.isRequired
 }
 
 export default DateTimeListView;
