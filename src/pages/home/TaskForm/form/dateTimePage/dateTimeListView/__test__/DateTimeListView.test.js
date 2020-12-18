a358cb43695255f825796658edab46924356d5ee
larry adams <ladams1776@yahoo.com>
2020-12-17 23:35:28 -0500

    Create test suite for DateTimeListView

--- src/pages/home/TaskForm/form/dateTimePage/dateTimeListView/__test__/DateTimeListView.test.js
+++ src/pages/home/TaskForm/form/dateTimePage/dateTimeListView/__test__/DateTimeListView.test.js
@@ -1,5 +1,36 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+// import target
+import DateTimeListView from '../DateTimeListView';
+
+// mock components
+jest.mock('../DateTimeItem', () => {
+    return jest.fn(() => <div data-testid="DateTimeItem"></div>);
+});
+
 describe('DateTimeListView.test.js', () => {
-    it('should render', () => {
-        
+    it('should render with DateTimeItem', () => {
+        // Arrange
+        const dateTimes = [{ id: 'dateTimeID' }];
+        const setEditDateTimeSpy = jest.fn();
+
+        // Act
+        const { queryByTestId } = render(<DateTimeListView dateTimes={dateTimes} setEditDateTime={setEditDateTimeSpy} />);
+
+        // Assert
+        expect(queryByTestId('DateTimeItem')).toBeInTheDocument();
+    });
+    it('should render without DateTimeItem', () => {
+        // Arrange
+        const dateTimes = [];
+        const setEditDateTimeSpy = jest.fn();
+
+        // Act
+        const { queryByTestId } = render(<DateTimeListView dateTimes={dateTimes} setEditDateTime={setEditDateTimeSpy} />);
+
+        // Assert
+        expect(queryByTestId('DateTimeItem')).toBeNull();
     });
 });
