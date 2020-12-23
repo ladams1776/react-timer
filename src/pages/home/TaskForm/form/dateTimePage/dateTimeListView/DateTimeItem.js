450584089606ea09ed815ac5f2ee9ff1c8674e61
larry adams <ladams1776@yahoo.com>
2020-12-22 23:38:21 -0500

    Update DateTimeItme to use the new ripple hook

--- src/pages/home/TaskForm/form/dateTimePage/dateTimeListView/DateTimeItem.js
+++ src/pages/home/TaskForm/form/dateTimePage/dateTimeListView/DateTimeItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import moment from 'moment-timezone';
+import { useRippleEffectById } from 'hooks';
 import styles from './DateTimeListView.module.css';
 
 const myTimezone = "America/New_York";
 const myDatetimeFormat = "YYYY-MM-DD hh:mm:ss a";
 
-const DateTimeItem = ({ dateTime, setEditDateTime}) => {
+const DateTimeItem = ({ dateTime, setEditDateTime }) => {
     const myDatetimeString = moment(dateTime.date)
         .tz(myTimezone)
         .format(myDatetimeFormat);
@@ -13,8 +14,9 @@ const DateTimeItem = ({ dateTime, setEditDateTime}) => {
     const onClick = () => {
         setEditDateTime({ id: dateTime.id, date: dateTime.date, minutes: dateTime.time })
     };
+    const rippleClick = useRippleEffectById(dateTime.id, onClick);
 
-    return <div className={styles.content} key={dateTime.id + dateTime.time} onClick={onClick} data-testid="DateTimeItem">
+    return <div id={dateTime.id} className={styles.content} key={dateTime.id + dateTime.time} onClick={rippleClick} data-testid="DateTimeItem">
         <input type="hidden" value={dateTime.id} name="id" />
         <div className={styles.date}>Date: {myDatetimeString}</div>
         <div className={styles.time}>Minutes: {dateTime.time}</div>
