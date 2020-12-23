23bc3ff5791f3c24f975afa1332e5a1244b3ac4d
larry adams <ladams1776@yahoo.com>
2020-12-22 22:53:49 -0500

    Update Button clean up and remove some unnecessary imports

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from "prop-types";
 import cn from 'classnames';
+import { useRippleEffect} from 'hooks';
 import styles from './Button.module.css';
-import useRippleEffect from '../hooks/useRippleEffect/useRippleEffect';
 
 
 const Button = ({ className, onClick, disabled, value, children, title, testid }) => {
   const clickCallback = useRippleEffect('Button', onClick);
   return (<button className={cn('btn', 'btn-default', 'navbar-btn', styles.baseBtn, className)}
-    data-testid={testid}
     onClick={clickCallback}
+    data-testid={testid}
     disabled={disabled}
     title={title}>
     {children}
