fefb0c8e65d560285e4b0c5526a06d85b2793a72
larry adams <ladams1776@yahoo.com>
2020-12-27 23:35:40 -0500

    Update AddTaskFOrm starting to decouple from context to redux.

--- src/pages/home/TaskForm/form/AddTaskForm.js
+++ src/pages/home/TaskForm/form/AddTaskForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import { useSelector, useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { TextAreaAdapter } from 'components';
 import useTaskEditContext from 'pages/home/hooks/useTaskEditContext';
 import ProjectDropDown from 'pages/home/TaskForm/projectDropdown/ProjectDropdown';
@@ -8,28 +8,28 @@ import TagMultiSelect from 'pages/home/TaskForm/tagMultiSelect/TagMultiSelect';
 import Timer from 'pages/home/TaskForm/timer/Timer';
 import DateTimeButton from './dateTimePage/DateTimeButton';
 import SubmitButton from './SubmitButton';
-import styles from './TaskForm.module.css';
 import { useFetchTags, useFetchTaskById } from '../hooks';
-import { simpleAction } from '../../../../redux/actionCreators/simpleAction';
 import { fetchTaskById } from '../../../../redux/actionCreators/actions';
+import useFetchTaskByIdTransition from '../hooks/useFetchTaskById/useFetchTaskByIdTransition';
+import useTaskSelector from 'redux/selectors/useTaskSelector';
 
+import styles from './TaskForm.module.css';
 
 const AddTaskForm = ({ taskId, className }) => {
   const {
-    state,
     dispatchTask,
     onProjectDropDownChange,
     onTextAreaChange,
     onTagChange,
   } = useTaskEditContext();
+  
   const dispatch = useDispatch()
   useFetchTaskById(taskId, dispatchTask);
   useFetchTags();
-  const { description, tags, project } = state;
+  useFetchTaskByIdTransition(taskId);
+  const task = useTaskSelector();
+  const { description, tags, project } = task;
 
-  const result = useSelector(state => state.simpleReducer.result);
-
-  console.log('yeah', result);
   return (
     <div className={className} data-testid="addTaskForm">
       <DateTimeButton taskId={taskId} />
