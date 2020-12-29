866e8881dc517344dc0ea8bee1cf980f514578a2
larry adams <ladams1776@yahoo.com>
2020-12-28 23:43:23 -0500

    Updage AddTaskForm Using taskSelector so can remove these pieces

--- src/pages/home/TaskForm/form/AddTaskForm.js
+++ src/pages/home/TaskForm/form/AddTaskForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import { useDispatch, useSelector } from 'react-redux'
 import { TextAreaAdapter } from 'components';
 import useTaskEditContext from 'pages/home/hooks/useTaskEditContext';
 import ProjectDropDown from 'pages/home/TaskForm/projectDropdown/ProjectDropdown';
@@ -9,7 +8,6 @@ import Timer from 'pages/home/TaskForm/timer/Timer';
 import DateTimeButton from './dateTimePage/DateTimeButton';
 import SubmitButton from './SubmitButton';
 import { useFetchTags, useFetchTaskById } from '../hooks';
-import { fetchTaskById } from '../../../../redux/actionCreators/actions';
 import useFetchTaskByIdTransition from '../hooks/useFetchTaskById/useFetchTaskByIdTransition';
 import useTaskSelector from 'redux/selectors/useTaskSelector';
 
@@ -23,7 +21,6 @@ const AddTaskForm = ({ taskId, className }) => {
     onTagChange,
   } = useTaskEditContext();
   
-  const dispatch = useDispatch()
   useFetchTaskById(taskId, dispatchTask);
   useFetchTags();
   useFetchTaskByIdTransition(taskId);
@@ -47,7 +44,6 @@ const AddTaskForm = ({ taskId, className }) => {
         description={description}
         setDescription={onTextAreaChange}
         id={taskId} />
-      <button onClick={() => dispatch(fetchTaskById(taskId))}>click me</button>
 
       <form
         className={styles.taskForm}
