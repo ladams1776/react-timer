df40e9aa4debbf37cdfa4272b204cb49f7bb742b
larry adams <ladams1776@yahoo.com>
2020-12-29 22:22:42 -0500

    Swap in the refactored AddTaskForm but not completed

--- src/pages/home/TaskForm/form/AddTaskForm.js
+++ src/pages/home/TaskForm/form/AddTaskForm.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import { TextAreaAdapter } from 'components';
+import { useSubmit } from '../hooks';
+import { Form, Field } from 'react-final-form'
 import useTaskEditContext from 'pages/home/hooks/useTaskEditContext';
 import ProjectDropDown from 'pages/home/TaskForm/projectDropdown/ProjectDropdown';
 import TagMultiSelect from 'pages/home/TaskForm/tagMultiSelect/TagMultiSelect';
 import Timer from 'pages/home/TaskForm/timer/Timer';
 import DateTimeButton from './dateTimePage/DateTimeButton';
-import SubmitButton from './SubmitButton';
 import { useFetchTags, useFetchTaskById } from '../hooks';
 import useFetchTaskByIdTransition from '../hooks/useFetchTaskById/useFetchTaskByIdTransition';
 import useTaskSelector from 'redux/selectors/useTaskSelector';
+import TextAreaAdapterNew from '../../../../components/TextAreaAdapterNew';
 
 import styles from './TaskForm.module.css';
 
@@ -17,42 +18,44 @@ const AddTaskForm = ({ taskId, className }) => {
   const {
     dispatchTask,
     onProjectDropDownChange,
-    onTextAreaChange,
     onTagChange,
   } = useTaskEditContext();
-  
+
   useFetchTaskById(taskId, dispatchTask);
   useFetchTags();
   useFetchTaskByIdTransition(taskId);
+  const onSubmit = useSubmit();
+
   const task = useTaskSelector();
-  const { description, tags, project } = task;
+  const { tags, project } = task;
 
-  return (
-    <div className={className} data-testid="addTaskForm">
-      <DateTimeButton taskId={taskId} />
-      <Timer />
+  return (<div className={className} data-testid="addTaskForm">
+    <DateTimeButton taskId={taskId} />
+    <Timer />
+    <Form
+      onSubmit={onSubmit}
+      initialValues={task}
+      render={({ handleSubmit }) => {
+        return (
+          <form
+            onSubmit={handleSubmit}
+            className={styles.taskForm}
+            method={taskId === -1 ? 'POST' : 'PUT'}
+            data-test-id="form">
 
-      <div className={styles.timeInfoContainer}>
-        <div className={styles.innerLeft}>
-          <ProjectDropDown value={project} onChange={onProjectDropDownChange} />
-        </div>
-        <div className={styles.innerRight}>
-          <TagMultiSelect tags={tags} onChange={onTagChange} />
-        </div>
-      </div>
-      <TextAreaAdapter
-        description={description}
-        setDescription={onTextAreaChange}
-        id={taskId} />
-
-      <form
-        className={styles.taskForm}
-        method={taskId === -1 ? 'POST' : 'PUT'}
-        data-test-id="form">
-        <SubmitButton />
-      </form>
-    </div>
-  );
+            <div className={styles.timeInfoContainer}>
+              <div className={styles.innerLeft}>
+                <ProjectDropDown value={project} onChange={onProjectDropDownChange} />
+              </div>
+              <div className={styles.innerRight}>
+                <TagMultiSelect tags={tags} onChange={onTagChange} />
+              </div>
+            </div>
+            <Field name="description" component={TextAreaAdapterNew} />
+            <button type="submit">Submit</button>
+          </form>)
+      }} />
+  </div>);
 };
 
 AddTaskForm.propTypes = {
