71d2385b6d3e25dde4e452e44d401f1aec5d357e
larry adams <ladams1776@yahoo.com>
2020-12-29 22:23:33 -0500

    Update useSubmit to accommodate saving the description with new style

--- src/pages/home/TaskForm/hooks/useSubmit/useSubmit.js
+++ src/pages/home/TaskForm/hooks/useSubmit/useSubmit.js
@@ -5,15 +5,14 @@ import hydrateTaskForm from './hydrateTaskForm';
 
 const useSubmit = () => {
   const { allTags } = useTagContext();
-  const { state, dispatchTask } = useTaskEditContext();
+  const { dispatchTask } = useTaskEditContext();
   const formDispatch = useFormDispatch(dispatchTask);
   const { time } = useTimeContext();
 
-  return (values) => {
+  return ({ _id, dateTimes, description, project, tags }) => {
     const dateFormatted = getCurrentDateTimeEstFormat();
-    const { id, project, tags, description } = state;
-    const timeTask = hydrateTaskForm(id, allTags, project, description, dateFormatted, time, tags);
-    const method = id ? 'PUT' : 'POST';
+    const timeTask = hydrateTaskForm(_id, allTags, project, description, dateFormatted, time, tags);
+    const method = _id ? 'PUT' : 'POST';
     fetchApiData('task', { body: timeTask, method }, formDispatch);
   };
 };
