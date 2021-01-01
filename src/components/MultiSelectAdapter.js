ef1d8fc9c1f47551970575e28ceb717d1690ed25
larry adams <ladams1776@yahoo.com>
2021-01-01 17:37:32 -0500

    Update MultiSelectAdapter Update so it can use Final Form correctly. Mostly, get the onCHange and values differently. Then make sure we transform them correctly.

--- src/components/MultiSelectAdapter.js
+++ src/components/MultiSelectAdapter.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import MultiSelect from 'react-multi-select-component';
+import { useTagTransformer } from '../pages/home/TaskForm/hooks';
 
-const MultiSelectAdapter = ({ ...rest }) => <MultiSelect labelledBy={'Select'} {...rest} data-test-id="multi-select" />;
+const MultiSelectAdapter = ({ ...rest }) => {
+    const { onChange, value } = rest.input;
+    const values = useTagTransformer(value);
+
+    return <MultiSelect
+        {...rest}
+        labelledBy={'Select'}
+        value={values}
+        onChange={onChange}
+        data-test-id="multi-select"
+    />;
+};
 
 export default MultiSelectAdapter;
