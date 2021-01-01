6125cb82a8463e38c01381c34b9f3a4bfbcd197f
larry adams <ladams1776@yahoo.com>
2021-01-01 17:41:25 -0500

    Update selectNormalizer to check if input is an array, makes it more versatile and resilient

--- src/utils/normalizers/selectNormalizer.js
+++ src/utils/normalizers/selectNormalizer.js
@@ -1,10 +1,12 @@
 const selectNormalizer = data => {
-    return data?.map(d => (
-        {
-            value: d?._id || d.value,
-            label: d?.name || d.label
-        }
-    ));
+    return (data instanceof Array)
+        ? data?.map(d => (
+            {
+                value: d?._id || d.value,
+                label: d?.name || d.label
+            }
+        ))
+        : [];
 }
 
 export default selectNormalizer;
