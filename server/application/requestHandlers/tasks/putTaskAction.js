0b888b0044d5d6b5df21d0c95bdcb7e75b384bda
larry adams <ladams1776@yahoo.com>
2021-01-01 22:00:47 -0500

    Update putTaskAction Update the name and update the api response handler (remove deprecated).

--- server/application/requestHandlers/tasks/putTaskAction.js
+++ server/application/requestHandlers/tasks/putTaskAction.js
@@ -1,9 +1,10 @@
+const FETCH_TASK_BY_ID_RESPONSE = require('../reduxTypes');
 const TaskService = require('../../../domain/services/tasks/TaskService');
-const jsonResponse = require('../jsonResponse');
+const apiResponse = require('../apiResponse');
 const RequestToTaskDto = require('./assemblers/RequestToTaskDto');
 
 module.exports = async (req, res) => {
-    const response = jsonResponse(res);
+    const response = apiResponse(res, FETCH_TASK_BY_ID_RESPONSE);
     const incomingDto = RequestToTaskDto(req);
     TaskService.updateTask(incomingDto, response);
-}
+};
