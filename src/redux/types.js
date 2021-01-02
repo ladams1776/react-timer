2e74aed6248c9b9491241e64f42bfdcf30fb7499
larry adams <ladams1776@yahoo.com>
2021-01-01 22:09:06 -0500

    Update types.js add more types for adding task and getting tags

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,2 +1,11 @@
+// fetchTaskById
 export const FETCH_TASK_BY_ID = 'FETCH_TASK_BY_ID';
 export const FETCH_TASK_BY_ID_RESPONSE = 'FETCH_TASK_BY_ID_RESPONSE';
+
+// putTaskById
+export const PUT_TASK_BY_ID = 'PUT_TASK_BY_ID';
+export const PUT_TASK_BY_ID_RESPONSE = 'PUT_TASK_BY_ID_RESPONSE';
+
+// fetchAllTags
+export const FETCH_ALL_TAGS = 'FETCH_ALL_TAGS';
+export const FETCB_ALL_TAGS_RESPONSE = 'FETCB_ALL_TAGS_RESPONSE'; 
