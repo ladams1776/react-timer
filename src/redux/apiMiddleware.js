e6b174bd884e7ecab2bd0093e84551a49193b7f7
larry adams <ladams1776@yahoo.com>
2021-01-01 22:08:43 -0500

    Update apiMiddleware to get it ready for FlashMessage functionality

--- src/redux/apiMiddleware.js
+++ src/redux/apiMiddleware.js
@@ -1,14 +1,17 @@
-import { fetchApiData } from 'utils';
+import fetchApiDataFlashMessage from '../utils/api/fetchApiData/fetchApiDataFlashMessage';
 import fetchApiDataTest from '../utils/api/fetchApiData/fetchApiDataTest';
 
 export const apiMiddleware = reduxStore => {
     const { dispatch } = reduxStore;
 
     return next => action => {
-        const { type, url, method, requestApi } = action;
+        const { type, url, method, requestApi, isFlash, body } = action;
 
-        if (type && requestApi) {
-            return fetchApiDataTest(url, {method}, dispatch);
+        if (type && requestApi && isFlash) {
+            return fetchApiDataFlashMessage(url, { method, body }, dispatch);
+        }
+        else if (type && requestApi) {
+            return fetchApiDataTest(url, { method, body }, dispatch);
         } else {
             return next(action);
         }
