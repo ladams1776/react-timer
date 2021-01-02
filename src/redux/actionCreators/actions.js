3863e0029f78399aff80c51478c9d26bfb00cdd7
larry adams <ladams1776@yahoo.com>
2021-01-01 22:09:26 -0500

    Add the put task by id and fetch all tags actions

--- src/redux/actionCreators/actions.js
+++ src/redux/actionCreators/actions.js
@@ -1,9 +1,27 @@
-import { FETCH_TASK_BY_ID } from "../types";
+import { FETCH_ALL_TAGS, FETCH_TASK_BY_ID, PUT_TASK_BY_ID } from "../types";
 
 export const fetchTaskById = (taskId) => {
     return {
         type: FETCH_TASK_BY_ID,
         url: `task/${taskId}`,
+        requestApi: true
+    }
+};
+
+export const fetchAllTags = () => {
+    return {
+        TYPE: FETCH_ALL_TAGS,
+        url: `tags`,
+        requestApi: true
+    }
+};
+
+export const putTaskById = (body) => {
+    return {
+        type: PUT_TASK_BY_ID,
+        url: `task`,
+        method: 'PUT',
+        body,
         requestApi: true
     }
 };
