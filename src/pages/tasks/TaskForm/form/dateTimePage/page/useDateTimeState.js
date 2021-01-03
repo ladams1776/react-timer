a3df7783303b26f84a52a106c9c1ea8d4b7f1af5
larry adams <ladams1776@yahoo.com>
2021-01-03 10:24:40 -0500

    Remove useDateTimeState bc no need for it now that we rely on final form

--- src/pages/tasks/TaskForm/form/dateTimePage/page/useDateTimeState.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const useDateTimeState = () => {
-    const [editDateTime, setEditDateTime] = React.useState({});
-    return { editDateTime, setEditDateTime };
-}
-
-export default useDateTimeState;
