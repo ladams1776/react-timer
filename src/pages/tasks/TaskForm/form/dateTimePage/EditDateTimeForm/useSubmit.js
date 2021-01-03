f4cf7d17b007f3ff7cb685308ff5f2ec9daa4b4a
larry adams <ladams1776@yahoo.com>
2021-01-03 10:24:22 -0500

    Update useSubmit Parse based on input fields not context.

--- src/pages/tasks/TaskForm/form/dateTimePage/EditDateTimeForm/useSubmit.js
+++ src/pages/tasks/TaskForm/form/dateTimePage/EditDateTimeForm/useSubmit.js
@@ -1,12 +1,19 @@
 import { fetchApiData } from 'utils';
-
+//@TODO: Redux this!
 const useSubmit = (dateTime, setDateTime, taskId, setIsShowing) => {
     const dispatch = e => {
         setDateTime(e);
         setIsShowing(false);
     };
-    return () => {
-        const config = { body: { id: dateTime.id, date: dateTime.date, minutes: dateTime.minutes }, method: 'PUT' };
+    return ({ id, date, minutes }) => {
+        const config = {
+            body: {
+                id,
+                date,
+                minutes,
+            },
+            method: 'PUT'
+        };
         fetchApiData(`task/${taskId}/dateTime/${dateTime.id}`, config, dispatch);
     };
 };
