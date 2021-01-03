1c6d1747bd747d8f53ed28dcb4d4f3a628333a68
larry adams <ladams1776@yahoo.com>
2021-01-02 19:38:36 -0500

    update DateTimePage the rename of useFetchTaskById

--- src/pages/tasks/TaskForm/form/dateTimePage/page/DateTimePage.js
+++ src/pages/tasks/TaskForm/form/dateTimePage/page/DateTimePage.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import useDateTimeState from './useDateTimeState';
 import DateTimeListView from '../dateTimeListView/DateTimeListView';
 import EditDateTimeForm from '../EditDateTimeForm/EditDateTimeForm';
-import useFetchTaskByIdTransition from '../../../hooks/useFetchTaskById/useFetchTaskByIdTransition';
+import useFetchTaskByIdDispatch from '../../../hooks/useFetchTaskById/useFetchTaskByIdDispatch';
 import useTaskByIdSelector from '../../../../../../redux/selectors/useTaskByIdSelector';
 
 const DateTimePage = ({ taskId, setIsShowing }) => {
     const { editDateTime, setEditDateTime } = useDateTimeState();
-    useFetchTaskByIdTransition(taskId);
+    useFetchTaskByIdDispatch(taskId);
     const task = useTaskByIdSelector();
     const { dateTimes } = task;
     return <div className="childrenContent">
