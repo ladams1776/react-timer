96c972e7dd14f7e25d4ce2059279c7dbf4e4bdc9
larry adams <ladams1776@yahoo.com>
2021-01-10 08:55:05 -0500

    Update FetchAllTasksRepository Use the EntityToDto

--- server/infrastructure/repositories/tasks/Repositories/FetchAllTasksRepository.js
+++ server/infrastructure/repositories/tasks/Repositories/FetchAllTasksRepository.js
@@ -1,4 +1,12 @@
 const Task = require('../../../models/Task');
 const hydrate = require('../../../hydrators/hydrate');
+const EntityToDto = require('./FetchTaskByIdRepository/EntityToDto');
 
-module.exports = () => Task.find({}, hydrate);
+module.exports = () => Task.find({}, hydrateTask);
+
+const hydrateTask = (err, doc) => {
+    const tasks = hydrate(err,doc);
+
+    const dtos = tasks.map(task => EntityToDto(task));
+    return dtos;
+}
