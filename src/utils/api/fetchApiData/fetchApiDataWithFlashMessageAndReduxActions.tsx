924b1e6c5fca0df5e6678741b67aa83f4c6d131b
larry adams <ladams1776@yahoo.com>
2021-01-09 23:37:17 -0500

    Convert and Rename fetchApiDataFlashMessage to typescript

---
+++ src/utils/api/fetchApiData/fetchApiDataWithFlashMessageAndReduxActions.tsx
@@ -0,0 +1,26 @@
+interface FetchParams {
+  body?: any;
+  headers?: {};
+  method: string;
+}
+
+type dispatchParam = (data: {}) => void;
+
+const fetchApiDataWithFlashMessageAndReduxActions = async (url: string, { method, body, ...settings }: FetchParams, dispatch: dispatchParam) => {
+  const headers = { 'Content-Type': 'application/json' };
+  const config = {
+    method: body ? 'POST' : 'GET',
+    ...settings,
+    headers: {
+      ...headers,
+      ...settings.headers,
+    },
+    body: body ? JSON.stringify(body) : null,
+  };
+
+  const response = await fetch(`/api/${url}`, config);
+  const data = await response.json();
+  if (data && response.ok) dispatch({ data: data.items, type: data.type });
+};
+
+export default fetchApiDataWithFlashMessageAndReduxActions;
