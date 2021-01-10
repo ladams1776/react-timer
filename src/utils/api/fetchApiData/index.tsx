ef902c2a0a01da584b0c426eb648ea1e027ea269
larry adams <ladams1776@yahoo.com>
2021-01-09 23:41:29 -0500

    export with index

---
+++ src/utils/api/fetchApiData/index.tsx
@@ -0,0 +1,3 @@
+export { default as fetchApiDataWithFlashMessageAndReduxActions } from './fetchApiDataWithFlashMessageAndReduxActions';
+export { default as fetchApiDataWithReduxActions } from './fetchApiDataWithReduxActions';
+export { default as fetchApiData } from './fetchApiData';
