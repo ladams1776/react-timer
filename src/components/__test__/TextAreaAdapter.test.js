34c5141fbb990438288389d0a82856052ce9dce9
larry adams <ladams1776@yahoo.com>
2021-01-10 10:00:58 -0500

    Fix test for TextAreaAdatepr changes

--- src/components/__test__/TextAreaAdapter.test.js
+++ src/components/__test__/TextAreaAdapter.test.js
@@ -1,49 +1,34 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { createWrapperWithContext, findByTestId } from 'testUtils';
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import TextAreaAdapter from '../TextAreaAdapter';
 
-describe('src/components/__test__/TextAreaAdapter.test.js', () => {
-  describe('TextAreaAdapter', () => {
-    let wrapper;
+// mock the third party components
+jest.mock('@tinymce/tinymce-react', () => {
+  return {
+    Editor: () => <div className="editor">{'Editor'}</div>
+  }
+})
 
-    it('should render TextAreaAdapter', () => {
-      // Arrange
-      const description = 'yeah';
-      const setDescriptionSpy = jest.fn();
+jest.mock('react-final-form', () => {
+  return {
+    Form: jest.fn().mockImplementation(() => { }),
+    Field: () => <div className="field">{'Field'}</div>
+  }
+});
 
-      const expected = {
-        cloudChannel: '5',
-        'data-test-id': 'text-area-adapter',
-        id: 'description',
-        init: {
-          height: 500,
-          menubar: true,
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount',
-          ],
-          toolbar:
-            'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-        },
-        initialValue: description,
-        name: 'description',
-        onEditorChange: setDescriptionSpy,
-      };
+describe('TextAreaAdapter', () => {
+  it('should render TextAreaAdapter', () => {
+    // Arrange
+    const input = {
+      onChange: jest.fn(),
+      value: 'value',
+    }
 
-      // Act
-      wrapper = shallow(
-        <TextAreaAdapter
-          description={description}
-          setDescription={setDescriptionSpy}
-        />,
-      );
+    // Act
+    const target = render(<TextAreaAdapter input={input} />);
 
-      // Assert
-      expect(findByTestId(wrapper, 'text-area-adapter').props()).toEqual(
-        expected,
-      );
-    });
+    // Assert
+    expect(target.container.querySelector(".field")).toBeTruthy();
   });
 });
