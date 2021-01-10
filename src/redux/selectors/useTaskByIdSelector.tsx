c5a519cc282fa899f4e784691fa1bdfcf5f599d1
larry adams <ladams1776@yahoo.com>
2021-01-09 23:36:49 -0500

    update useTaskByIdSelector and what it returns

--- src/redux/selectors/useTaskByIdSelector.tsx
+++ src/redux/selectors/useTaskByIdSelector.tsx
@@ -1,25 +1,33 @@
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 
 const useTaskByIdSelector = (): ReturnedState => {
-    return useSelector((state: State): ReturnedState => {
-        return {
-            ...state.tasks.taskById,
-            project: state?.tasks?.taskById?.contractId,
-        };
-    });
+  return useSelector(
+    (state: State): ReturnedState => {
+      return {
+        id: state.tasks.taskById._id,
+        ...state.tasks.taskById,
+        project: state?.tasks?.taskById?.contractId,
+      };
+    },
+  );
 };
 
 export default useTaskByIdSelector;
 
 interface State {
-    tasks: {
-        taskById: {
-            contractId: string;
-        };
-    }
-};
-    
+  tasks: {
+    taskById: {
+      _id: string;
+      contractId: string;
+      description: string;
+    };
+  };
+}
+
 interface ReturnedState {
-    contractId: string;
-    project: string;
-};
+  id: string;
+  _id: string;
+  description: string;
+  contractId: string;
+  project: string;
+}
