fa22b5a823ee62756c085ffd433cf8878b441ce0
larry adams <ladams1776@yahoo.com>
2021-01-09 23:41:08 -0500

    Add the fetchApiDataWithReduxActions (rename) and convert to typescript

---
+++ src/utils/api/fetchApiData/fetchApiDataWithReduxActions.tsx
@@ -0,0 +1,26 @@
+interface FetchParams {
+  body?: any;
+  headers?: {};
+  method: string;
+}
+
+type dispatchParam = (data: {}) => void;
+
+const fetchApiDataWithReduxActions = async (url: string, { method, body, ...settings }: FetchParams, dispatch: dispatchParam) => {
+  const headers = { 'Content-Type': 'application/json' };
+  const config = {
+    method: method || body ? 'POST' : 'GET',
+    ...settings,
+    headers: {
+      ...headers,
+      ...settings.headers,
+    },
+    body: body ? JSON.stringify(body) : null,
+  };
+
+  const response = await fetch(`/api/${url}`, config);
+  const data = await response.json();
+  if (data && response.ok) dispatch({ data: data.items, type: data.type });
+};
+
+export default fetchApiDataWithReduxActions;
