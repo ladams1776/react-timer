5cd78e3df583b50b123858cd50212d535370050f
larry adams <ladams1776@yahoo.com>
2021-01-09 23:36:15 -0500

    Convert TaskListView to Typescript

---
+++ src/pages/tasks/TaskListView/TaskListView.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import cn from 'classnames';
+import { TaskInterface } from 'interfaces/pages/tasks/Task';
+import useTaskByIdSelector from 'redux/selectors/useTaskByIdSelector';
+import useSmoothScrolling from './hooks/useSmoothScrolling';
+import styles from './TaskListView.module.css';
+import Task from './Task/Task';
+
+interface TaskListViewProp {
+  className: string;
+  tasks: TaskInterface[];
+  setTasks: (tasks: TaskInterface[]) => void;
+  refs: any;
+}
+
+const TaskListView: React.FC<TaskListViewProp> = ({ className, tasks, setTasks, refs }) => {
+  const task = useTaskByIdSelector();
+  console.log(task);
+  const { _id, description } = task;
+
+  useSmoothScrolling(refs, _id, description);
+
+  return (
+    <div className={cn(className, styles.task, { [styles.listViewAndTask]: id })} data-testid="list-view">
+      <ul className={styles.taskList}>
+        {tasks.map((task: TaskInterface) => {
+          const desc = task._id === _id ? description : task.description;
+          const ref = refs[task._id];
+          return (
+            <li key={task._id} className={cn(styles.task)} ref={ref}>
+              <Task key={task._id} contractId={task.projectId} {...task} description={desc} selectedId={id} setTasks={setTasks} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default TaskListView;
