ddca4db05cbfd3c2767de6f116a9e7e6e3be68de
larry adams <ladams1776@yahoo.com>
2021-01-09 23:41:21 -0500

    Rename consumption in apiMiddleware

--- src/redux/apiMiddleware.tsx
+++ src/redux/apiMiddleware.tsx
@@ -1,30 +1,28 @@
-import fetchApiDataFlashMessage from '../utils/api/fetchApiData/fetchApiDataFlashMessage';
-import fetchApiDataTest from '../utils/api/fetchApiData/fetchApiDataTest';
+import { fetchApiDataWithFlashMessageAndReduxActions, fetchApiDataWithReduxActions } from '../utils/api/fetchApiData';
 
-const apiMiddleware = (reduxStore:any):Function => {
-    const { dispatch } = reduxStore;
+const apiMiddleware = (reduxStore: any): Function => {
+  const { dispatch } = reduxStore;
 
-    return (next:Function) => (action:action) => {
-        const { type, url, method, requestApi, isFlash, body } = action;
+  return (next: (action: action) => void) => (action: action) => {
+    const { type, url, method, requestApi, isFlash, body } = action;
 
-        if (type && requestApi && isFlash) {
-            return fetchApiDataFlashMessage(url, { method, body }, dispatch);
-        }
-        else if (type && requestApi) {
-            return fetchApiDataTest(url, { method, body }, dispatch);
-        } else {
-            return next(action);
-        }
+    if (type && requestApi && isFlash) {
+      return fetchApiDataWithFlashMessageAndReduxActions(url, { method, body }, dispatch);
+    } else if (type && requestApi) {
+      return fetchApiDataWithReduxActions(url, { method, body }, dispatch);
+    } else {
+      return next(action);
     }
-}
+  };
+};
 
 interface action {
-    type: string;
-    url: string;
-    method: string;
-    requestApi: boolean;
-    isFlash: boolean;
-    body: Object;
+  type: string;
+  url: string;
+  method: string;
+  requestApi: boolean;
+  isFlash: boolean;
+  body: Object;
 }
 
 export default apiMiddleware;
