d088de20788d938941504ee993fc975ca6c1951b
larry adams <ladams1776@yahoo.com>
2021-01-10 22:57:14 -0500

    COnvert EditDateTimeForm to TypeScript

---
+++ src/pages/tasks/TaskForm/form/dateTimePage/EditDateTimeForm/EditDateTimeForm.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Field, Form } from 'react-final-form';
+import { Button } from 'components';
+import useSubmit from './useSubmit';
+import styles from './EditDateTimeForm.module.css';
+
+interface EditDateTime {
+    id: string;
+    date: string;
+    minutes: number;
+  };
+
+interface EditDateTimeFormProp {
+  setEditDateTime: (editDateTime: EditDateTime) => void;
+  editDateTime: EditDateTime;
+  taskId: string;
+  setIsShowing: (setIsShowing: boolean) => void;
+}
+
+const EditDateTimeForm: React.FC<EditDateTimeFormProp> = ({ setEditDateTime, editDateTime, taskId, setIsShowing }) => {
+  const onSubmit = useSubmit(editDateTime, setEditDateTime, taskId, setIsShowing);
+
+  return (
+    <Form
+      onSubmit={onSubmit}
+      initialValues={editDateTime}
+      render={({ handleSubmit }) => {
+        return (
+          <form className={styles.form} data-testid="form" onSubmit={handleSubmit} method="PUT">
+            <Field type="hidden" name="id" value={editDateTime.id} component="input" />
+            <div className={styles.field}>
+              <label htmlFor="date">Date:</label> <Field type="text" name="date" id="date" value={editDateTime.date} component="input" />
+            </div>
+            <div className={styles.field}>
+              <label htmlFor="minutes">Minutes:</label>
+              <Field type="text" name="minutes" id="minutes" value={editDateTime.minutes} component="input" />
+            </div>
+            <div className={styles.submitContainer}>
+              <Button type="submit" className={styles.submit} value="Submit" />
+            </div>
+          </form>
+        );
+      }}
+    />
+  );
+};
+
+export default EditDateTimeForm;
