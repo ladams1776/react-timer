da21852c1881d25966b2b1a9ccdc1c86996a688a
larry adams <ladams1776@yahoo.com>
2021-01-13 22:57:32 -0500

    Update/fix DateTimePage unit test

--- src/pages/tasks/TaskForm/form/dateTimePage/page/__test__/DateTimePage.test.js
+++ src/pages/tasks/TaskForm/form/dateTimePage/page/__test__/DateTimePage.test.js
@@ -1,64 +1,63 @@
 import React from "react";
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 // import target
 import DateTimePage from '../DateTimePage';
 
 // import target dependencies
-import useFetchTaskById from '../useFetchTaskById';
-import useDateTimeState from '../useDateTimeState';
+import useFetchTaskByIdDispatch from "pages/tasks/TaskForm/hooks/useFetchTaskById/useFetchTaskByIdDispatch";
+import useTaskByIdSelector from 'redux/selectors/useTaskByIdSelector';
 
 // mock hooks
-jest.mock('../useFetchTaskById');
-jest.mock('../useDateTimeState');
+jest.mock("pages/tasks/TaskForm/hooks/useFetchTaskById/useFetchTaskByIdDispatch");
+jest.mock('redux/selectors/useTaskByIdSelector');
 
 // mock components
 jest.mock('../../dateTimeListView/DateTimeListView', () => {
-    return jest.fn(() => <div data-testid="DateTimeListView"></div>)
+    return () => <div className="DateTimeListView">DateTimeListView</div>
 });
 jest.mock('../../EditDateTimeForm/EditDateTimeForm', () => {
-    return jest.fn(() => <div data-testid="EditDateTimeForm"></div>)
+    return () => <div className="EditDateTimeForm">EditDateTimeForm</div>
 });
 
 describe('DateTimePage.test.js', () => {
-    it('should show EditDateTimeForm, when editDateTime with id is available', () => {
+    it('should show EditDateTimeForm, when editDateTime with id is available', async () => {
         // Arrange
         const taskId = 'taskId';
-        const setIsShowingSpy = jest.fn();
-        useDateTimeState.mockReturnValue({
-            dateTimes: null,
-            setDateTimes: 'setDateTimes',
-            editDateTime: null,
-            setEditDateTime: null
+        const setIsShowingSpy = jest.fn().mockImplementation();
+        useTaskByIdSelector.mockReturnValue({
+            dateTimes: [{ id: 'dateTimeID' }],
+            time: 10000,
         });
 
         // Act
-        const { queryByTestId } = render(<DateTimePage taskId={taskId} setIsShowing={setIsShowingSpy} />);
+        const { queryAllByText, findByText}  = render(<DateTimePage taskId={taskId} setIsShowing={setIsShowingSpy} />);
 
         // Assert
-        expect(useFetchTaskById).toBeCalledWith(taskId, 'setDateTimes');
-        expect(queryByTestId('DateTimeListView')).toBeInTheDocument();
-        expect(queryByTestId('EditDateTimeForm')).toBeNull();
+        expect(useFetchTaskByIdDispatch).toBeCalledWith(taskId);
+        expect(await findByText('DateTimeListView')).toBeInTheDocument();
+        expect(queryAllByText('EditDateTimeForm')).toEqual([]);
     });
 
-    it('should show DateTimeListView, when no editDateTime with id', () => {
+    it('should show DateTimeListView, when no editDateTime with id', async () => {
         // Arrange
         const taskId = 'taskId';
-        const setIsShowingSpy = jest.fn();
-        useDateTimeState.mockReturnValue({
-            dateTimes: null,
-            setDateTimes: 'setDateTimes',
-            editDateTime: { id: 'editDateTimeID' },
-            setEditDateTime: null
+        const setIsShowingSpy = jest.fn().mockImplementation();
+        useTaskByIdSelector.mockReturnValue({
+            dateTimes: [{ id: 'dateTimeID' }],
+            time: 10000,
         });
+        const realUseState = React.useState;
+        jest.spyOn(React, 'useState')
+            .mockImplementationOnce(() => realUseState(() => { return { id: 'yes' } }));
 
         // Act
-        const { queryByTestId } = render(<DateTimePage taskId={taskId} setIsShowing={setIsShowingSpy} />);
+        const { queryAllByText, findByText } = render(<DateTimePage taskId={taskId} setIsShowing={setIsShowingSpy} />);
 
         // Assert
-        expect(useFetchTaskById).toBeCalledWith(taskId, 'setDateTimes');
-        expect(queryByTestId('EditDateTimeForm')).toBeInTheDocument();
-        expect(queryByTestId('DateTimeListView')).toBeNull();
+        expect(useFetchTaskByIdDispatch).toBeCalledWith(taskId);
+        expect(await findByText('EditDateTimeForm')).toBeInTheDocument();
+        expect(queryAllByText('DateTimeListView')).toEqual([]);
     });
 });
