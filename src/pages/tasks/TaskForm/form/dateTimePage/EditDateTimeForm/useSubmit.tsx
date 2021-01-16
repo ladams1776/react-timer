993010143531515dce48aa8ddd0c99da8aa9250d
larry adams <ladams1776@yahoo.com>
2021-01-16 14:58:03 -0500

    Refactor useSubmit and convert to Typescript Refactored: incorporated the redux stuff

---
+++ src/pages/tasks/TaskForm/form/dateTimePage/EditDateTimeForm/useSubmit.tsx
@@ -0,0 +1,34 @@
+import { EditDateTimeInterface } from 'interfaces/pages/tasks/Task';
+import { useDispatch } from 'react-redux';
+import { putDateTime } from 'redux/actionCreators/actions';
+
+interface SubmitProp {
+  id: string;
+  date: string;
+  minutes: number;
+}
+
+const useSubmit = (
+  dateTime: EditDateTimeInterface,
+  taskId: String,
+  setIsShowingEditDateTimeForm: (isShowing: boolean) => void,
+): ((editDateTime: EditDateTimeInterface) => void) => {
+    console.log('yes')
+  const dispatch = useDispatch();
+  return ({ id, date, minutes }: SubmitProp) => {
+    const config = {
+      body: {
+        id,
+        date,
+        minutes,
+      },
+      taskId,
+      dateTimeId: dateTime.id,
+    };
+
+    dispatch(putDateTime(config));
+    setIsShowingEditDateTimeForm(false);
+  };
+};
+
+export default useSubmit;
