62f52d1fa72bf86201e3e4bf5b8139aa87e2bb1e
larry adams <ladams1776@yahoo.com>
2021-01-16 14:58:18 -0500

    Add test for refactored useSubmit.tsx

---
+++ src/pages/tasks/TaskForm/form/dateTimePage/EditDateTimeForm/__test__/useSubmit.test.js
@@ -0,0 +1,42 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { act } from 'react-dom/test-utils';
+
+// target
+import useSubmit from '../useSubmit';
+
+// dependencies
+import { putDateTime } from 'redux/actionCreators/actions';
+
+// mock dependencies
+// jest.mock('react-redux/useDispatch');
+jest.mock('redux/actionCreators/actions');
+
+jest.mock('react-redux', () => ({
+    useDispatch: () => jest.fn()
+}));
+
+describe('useSubmit.test.js', () => {
+    it('returns submit', () => {
+        // Arrange
+        const dateTime = {
+            id: 'dateTimeId',
+            date: 'date',
+            minutes: 1234
+        };
+        const taskId = 'taskId';
+        const setIsShowingEditDateTimeFormSpy = jest.fn();
+        const expected = {
+            body: dateTime,
+            taskId,
+            dateTimeId: dateTime.id,
+        }
+
+        // Act
+        const { result } = renderHook(() => useSubmit(dateTime, taskId, setIsShowingEditDateTimeFormSpy));
+        act(() => result.current({ ...dateTime }));
+
+        // Assert
+        expect(putDateTime).toBeCalledWith(expected);
+        expect(setIsShowingEditDateTimeFormSpy).toBeCalledWith(false);
+    });
+});
