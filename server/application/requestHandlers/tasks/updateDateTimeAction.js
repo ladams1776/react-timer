9a644070977d63544b429e094b3bd8126771348b
larry adams <ladams1776@yahoo.com>
2021-01-16 14:26:19 -0500

    Remove feature flag from updateDateTimeAction

--- server/application/requestHandlers/tasks/updateDateTimeAction.js
+++ server/application/requestHandlers/tasks/updateDateTimeAction.js
@@ -1,30 +1,16 @@
-const jsonResponse = require('../jsonResponse');
 const apiResponse = require('../apiResponse');
-
 const TaskService = require('../../../domain/services/tasks/TaskService');
-
-const UPDATE_DATE_TIME_RESPONSE = require('../reduxTypes');
+const { UPDATE_DATE_TIME_RESPONSE } = require('../reduxTypes');
 
 
 
 
 module.exports = (req, res) => {
+    const responder = apiResponse(res, UPDATE_DATE_TIME_RESPONSE);
 
-    const feature = false;
-    const responder = getFeatureResonse(feature, res);
-    
     const id = req.params.taskId;
     const dateTime = req.body;
-    
+
     const updatedTaskWithDateTime = TaskService.updateDateTimeOfTask(id, dateTime);
     responder(updatedTaskWithDateTime);
 };
-
-
-const getFeatureResonse = (feature, res) => {
-    if (feature) {
-        return apiResponse(res, UPDATE_DATE_TIME_RESPONSE);
-    } else {
-        return jsonResponse(res);
-    }
-}
