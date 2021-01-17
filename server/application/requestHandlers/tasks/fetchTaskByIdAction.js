dd931f70b32597f557826e481b492d56fb265d9b
larry adams <ladams1776@yahoo.com>
2021-01-17 13:06:33 -0500

    Fix fetchTaskByIdAction

--- server/application/requestHandlers/tasks/fetchTaskByIdAction.js
+++ server/application/requestHandlers/tasks/fetchTaskByIdAction.js
@@ -1,6 +1,6 @@
 const TaskService = require('../../../domain/services/tasks/TaskService');
 const apiResponse = require('../apiResponse');
-const FETCH_TASK_BY_ID_RESPONSE = require('../reduxTypes');
+const { FETCH_TASK_BY_ID_RESPONSE } = require('../reduxTypes');
 
 module.exports = async (req, res) => {
   const response = apiResponse(res, FETCH_TASK_BY_ID_RESPONSE);
