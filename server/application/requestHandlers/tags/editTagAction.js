52a55e70a74823d75ddfeac2e782ee26caba6440
larry adams <ladams1776@yahoo.com>
2021-01-17 13:05:45 -0500

    Update editTagAction to get rdy for redux Adding the feature toggle - not on yet.

--- server/application/requestHandlers/tags/editTagAction.js
+++ server/application/requestHandlers/tags/editTagAction.js
@@ -1,8 +1,20 @@
 const TagService = require('../../../domain/services/tags/TagService');
+const apiResponse = require('../apiResponse');
 const jsonResponse = require('../jsonResponse');
+const { UPDATE_DATE_TIME_RESPONSE } = require('../reduxTypes');
 
 //@TODO: Need to do error handling in these request handlers
 module.exports = (req, res) => {
-    const responder = jsonResponse(res);
+    console.log('working here - edit Tag Action');
+    const feature = false;
+    const responder = ifFeatureChangeResponse(feature, res);
     TagService.updateTag(req.body, responder)
+};
+
+const ifFeatureChangeResponse = (feature, res) => {
+    if (feature) {
+        return apiResponse(res, UPDATE_DATE_TIME_RESPONSE);
+    }
+    
+    return jsonResponse(res);
 };
