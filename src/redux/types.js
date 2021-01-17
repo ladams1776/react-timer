fd2909feda9634844f613c6dfbdb136ed295a786
larry adams <ladams1776@yahoo.com>
2021-01-17 13:17:06 -0500

    update types.js to include tag update constant

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,3 +1,8 @@
+export const GET = 'GET';
+export const PUT = 'PUT';
+export const POST = 'POST';
+export const DELETE = 'DELETE';
+
 // fetchTaskById
 export const FETCH_TASK_BY_ID = 'FETCH_TASK_BY_ID';
 export const FETCH_TASK_BY_ID_RESPONSE = 'FETCH_TASK_BY_ID_RESPONSE';
@@ -13,3 +18,7 @@ export const FETCB_ALL_TAGS_RESPONSE = 'FETCB_ALL_TAGS_RESPONSE';
 // dateTimes
 export const UPDATE_DATE_TIME = 'UPDATE_DATE_TIME';
 export const UPDATE_DATE_TIME_RESPONSE = 'UPDATE_DATE_TIME_RESPONSE';
+
+// tags
+export const UPDATE_TAG_BY_ID = 'UPDATE_TAG_BY_ID';
+export const UPDATE_TAG_BY_ID_RESPONSE = 'UPDATE_TAG_BY_ID_RESPONSE';
