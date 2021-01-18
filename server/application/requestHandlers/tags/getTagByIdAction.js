ffdcb827f8654336867623304da367fc851ada94
larry adams <ladams1776@yahoo.com>
2021-01-17 23:05:57 -0500

    update getTagByIdAction - redux

--- server/application/requestHandlers/tags/getTagByIdAction.js
+++ server/application/requestHandlers/tags/getTagByIdAction.js
@@ -1,7 +1,9 @@
 const TagService = require('../../../domain/services/tags/TagService');
-const jsonResponse = require('../jsonResponse');
+const apiResponse = require('../apiResponse');
+const { FETCH_TAG_BY_ID_RESPONSE } = require('../reduxTypes');
 
-module.exports = (req, res) => {
-  const responseHandler = jsonResponse(res);
-  TagService.fetchTagById(req.params.id, responseHandler);
+module.exports = async (req, res) => {
+  const responseHandler = apiResponse(res, FETCH_TAG_BY_ID_RESPONSE);
+  const tag = await TagService.fetchTagById(req.params.id, responseHandler);
+  responseHandler(tag);
 };
