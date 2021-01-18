a0f1ecd88012190ef0d7648c13c9797eaba71307
larry adams <ladams1776@yahoo.com>
2021-01-18 10:57:35 -0500

    Add field validation to EditDateTimeForm

--- src/pages/tasks/TaskForm/form/dateTimePage/EditDateTimeForm/EditDateTimeForm.tsx
+++ src/pages/tasks/TaskForm/form/dateTimePage/EditDateTimeForm/EditDateTimeForm.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Field, Form } from 'react-final-form';
+import { minSecValidator, utcFormatValidator } from 'forms/validators';
 import { Button } from 'components';
 import useSubmit from './useSubmit';
-import styles from './EditDateTimeForm.module.css';
 
-interface EditDateTime {
-    id: string;
-    date: string;
-    minutes: number;
-  };
+import styles from './EditDateTimeForm.module.css';
+import { EditDateTimeInterface } from 'interfaces/pages/tasks/Task';
 
 interface EditDateTimeFormProp {
-  editDateTime: EditDateTime;
+  editDateTime: EditDateTimeInterface;
   taskId: string;
   setIsShowingEditDateTimeForm: (setIsShowing: boolean) => void;
 }
@@ -23,19 +20,37 @@ const EditDateTimeForm: React.FC<EditDateTimeFormProp> = ({ editDateTime, taskId
     <Form
       onSubmit={onSubmit}
       initialValues={editDateTime}
-      render={({ handleSubmit }) => {
+      render={({ handleSubmit, submitting, pristine }) => {
         return (
           <form className={styles.form} data-testid="form" onSubmit={handleSubmit} method="PUT">
             <Field type="hidden" name="id" value={editDateTime.id} component="input" />
             <div className={styles.field}>
-              <label htmlFor="date">Date:</label> <Field type="text" name="date" id="date" value={editDateTime.date} component="input" />
+              <Field name="date" validate={utcFormatValidator} value={editDateTime.date}>
+                {({ input, meta }) => {
+                  return (
+                    <div>
+                      <label htmlFor="date">Date</label>
+                      <input {...input} type="text" id="date"/>
+                      {meta.error && meta.touched && <span className={styles.error}>{meta.error}</span>}
+                    </div>
+                  );
+                }}
+              </Field>
             </div>
-            <div className={styles.field}>
-              <label htmlFor="minutes">Minutes:</label>
-              <Field type="text" name="minutes" id="minutes" value={editDateTime.minutes} component="input" />
-            </div>
+            <Field name="minutes" validate={minSecValidator} value={editDateTime.minutes}>
+              {({input, meta}) => {
+                return (
+                  <div className={styles.field}>
+                    <label htmlFor="minutes">Minutes</label>
+                    <input {...input} type="text" id="minutes" />
+                    {meta.error && meta.touched && <span className={styles.error}>{meta.error}</span>}
+                  </div>
+                );
+              }}
+              
+            </Field>
             <div className={styles.submitContainer}>
-              <Button type="submit" className={styles.submit} value="Submit" />
+              <Button type="submit" className={styles.submit} value="Submit" disabled={submitting || pristine} />
             </div>
           </form>
         );
