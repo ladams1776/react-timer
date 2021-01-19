9dddba5f14c3c667e03db6526fbe5090ca8fd0c6
larry adams <ladams1776@yahoo.com>
2021-01-18 22:14:08 -0500

    add new helper function to formatMinsAndSecsForDisplay

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,5 +1,6 @@
 export { default as getCurrentDateTimeEstFormat } from './formatters/getCurrentDateTimeEstFormat';
 export { default as displayMsInFractionalHourFormat } from './formatters/displayMsInFractionalHourFormat';
+export { default as formatMinsAndSecsForDisplay } from './formatters/formatMinsAndSecsForDisplay';
 export { default as fetchApiData } from './api/fetchApiData/fetchApiData';
 export { default as selectNormalizer } from './normalizers/selectNormalizer';
 export { default as useFlashMessageFetchApiData } from './api/useFlashMessageFetchApiData';
