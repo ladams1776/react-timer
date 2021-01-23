676e03b0bf51a8faff2fc30b8a7ef36e43d2dec1
larry adams <ladams1776@yahoo.com>
2021-01-23 06:38:19 -0500

    Convert useSubmit to typescript

--- src/pages/tasks/TaskForm/hooks/useSubmit/useSubmit.tsx
+++ src/pages/tasks/TaskForm/hooks/useSubmit/useSubmit.tsx
@@ -4,12 +4,19 @@ import { useTimeContext, useTagContext } from '..';
 import { putTaskById } from 'redux/actionCreators/actions';
 import hydrateTaskForm from './hydrateTaskForm';
 
-const useSubmit = () => {
+interface taskDtoInterface {
+  _id: string;
+  description: string;
+  project: string;
+  tags: any[];
+}
+const useSubmit = (): ((task: taskDtoInterface) => void) => {
   const { allTags } = useTagContext();
   const dispatch = useDispatch();
   const { time } = useTimeContext();
 
-  return ({ _id, description, project, tags }) => {
+  return (task: taskDtoInterface): void => {
+    const { _id, description, project, tags } = task;
     const dateFormatted = getCurrentDateTimeEstFormat();
     const timeTask = hydrateTaskForm(_id, allTags, project, description, dateFormatted, time, tags);
     dispatch(putTaskById(timeTask));
