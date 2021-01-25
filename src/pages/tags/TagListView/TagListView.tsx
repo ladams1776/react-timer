26d7c5d513a17546c7941e7668a2b3d18f18dfda
larry adams <ladams1776@yahoo.com>
2021-01-24 23:19:50 -0500

    Convert TagListView to TypeScript

---
+++ src/pages/tags/TagListView/TagListView.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Tag from './Tag/Tag';
+import styles from './TagListView.module.css';
+
+interface TagInterface {
+  name: string;
+  description: string;
+  _id: string;
+};
+
+
+interface TagListViewProp {
+  className:string;
+  tagId:string;
+  tags: TagInterface[]
+}
+
+const TagsListView: React.FC<TagListViewProp> = ({ className, tagId, tags }) => {
+  return (
+    <div className={className}>
+      <ul className={styles.tagList}>
+        {tags.map(tag => (
+          <li key={tag._id} className="tag">
+            <Tag {...tag} key={tag._id} selectedId={tagId} />
+          </li>
+        ))}
+      </ul>
+    </div >
+  );
+};
+
+export default TagsListView;
--- src/pages/tags/TagListView/__test__/TagListView.test.js
+++ src/pages/tags/TagListView/__test__/TagListView.test.js
@@ -1,38 +1,30 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import { findByTestId } from "testUtils";
-import useFetchAllTags from '../useFetchAllTags';
-import ControlButtons from '../ControlButtons/ControlButtons';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+// target
 import TagsListView from '../TagListView';
 
-jest.mock('../useFetchAllTags');
+// dependencies
+
+// mock
+jest.mock('../Tag/Tag', () => {
+    return () => <>TagComp</>
+});
 
 describe('serc/pages/tags/__test__/TagListView.test.js', () => {
     describe('TagListView', () => {
-        // Arrange
-        let wrapper;
-        
-        const realUseState = React.useState;
-        jest.spyOn(React, 'useState')
-            .mockImplementationOnce(() => realUseState([]));
-
         it('should render', () => {
             // Arrange
-            const expected = {
-                children: [
-                    <div className="tag-list__header">
-                        <ControlButtons />
-                    </div>,
-                    <ul className="tag-list" />,
-                ],
-                "data-test-id": "list-view",
-            }.toString();
 
             // Act
-            wrapper = shallow(<TagsListView />);
+            const { queryByText } = render(<TagsListView
+                className={"className"}
+                tagId={"1"}
+                tags={[{ _id: 'id' }]} />);
 
             // Assert
-            expect(findByTestId(wrapper, 'list-view').props().toString()).toEqual(expected);
-        });
+            expect(queryByText('TagComp')).toBeInTheDocument();
+        })
     });
 });
