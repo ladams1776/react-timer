91835b43f857a2d3d13ea15d4cf26d98f276e9d1
larry adams <ladams1776@yahoo.com>
2021-01-25 23:35:52 -0500

    Update AddTagForm Convert over to final form

--- src/pages/tags/AddTagForm/AddTagForm.js
+++ src/pages/tags/AddTagForm/AddTagForm.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import cn from 'classnames';
+import { useSelector } from 'react-redux';
+import { Form, Field } from 'react-final-form'
 import { Button } from 'components';
 import { useSetCurrentLocation } from 'hooks'
-import useFetchTagById from './useFetchTagById';
 import useFormSetup from './useFormSetup';
-import styles from './AddTagForm.module.css';
 import useFetchTagByIdDispatch from './useFetchTagByIdDispatch';
-import { useSelector } from 'react-redux';
+import styles from './AddTagForm.module.css';
 
 
 const useD = () => {
@@ -15,50 +15,46 @@ const useD = () => {
     });
 };
 
-const AddTagePage = ({ tagId, className }) => {
+const AddTagePage = ({ tagId }) => {
     useSetCurrentLocation(`/tag/${tagId}`);
     useFetchTagByIdDispatch(tagId);
     const tag = useD();
-    console.log('tag ', tag)
-    const { onSubmit, setName, setDescription, nameRef, descriptionRef, idRef } = useFormSetup(tagId);
-    // useFetchTagById(tagId, setTag);
+    const { onSubmit } = useFormSetup(tagId);
 
     return (
         <div className={styles.container}>
-            <form
-                data-test-id="form"
-                method={tag.id === -1 ? 'POST' : 'PUT'}
-                className={styles.form}>
-                <h3>Add a New Tag</h3>
-                <input name="id" type="hidden" value={tag.id} ref={idRef} />
-                <input
-                    name="name"
-                    onChange={e => setName(e.target.value)}
-                    component="input"
-                    defaultValue={tag?.name}
-                    className={styles.name}
-                    autoFocus
-                    type="text"
-                    ref={nameRef}
-                />
-                <textarea
-                    name="description"
-                    placeholder={"Tag's Description"}
-                    onChange={e => setDescription(e.target.value)}
-                    cols="80"
-                    rows="10"
-                    defaultValue={tag?.description}
-                    className={styles.description}
-                    ref={descriptionRef}
-                />
-                <Button type="submit"
-                    onClick={onSubmit}
-                    className={cn("btn", "btn-primary", styles.submit)}
-                    data-test-id="addTagPageSubmit"
-                >
-                    Submit
-                    </Button>
-            </form>
+            <Form
+                onSubmit={onSubmit}
+                initialValues={tag}
+                render={({ handleSubmit }) => {
+                    return (
+                        <form
+                            data-test-id="form"
+                            className={styles.form}
+                            onSubmit={handleSubmit}>
+                            <h3>Add a New Tag</h3>
+
+                            <Field
+                                name="name"
+                                component="input"
+                                className={styles.name}
+                            />
+                            <Field
+                                name="description"
+                                cols="80"
+                                rows="10"
+                                className={styles.description}
+                                component="textarea"
+                            />
+                            <Button type="submit"
+                                onClick={onSubmit}
+                                className={cn("btn", "btn-primary", styles.submit)}
+                                data-test-id="addTagPageSubmit"
+                                value="Submit Form" />
+                        </form>
+                    );
+                }}
+            />
         </div>
     );
 }
