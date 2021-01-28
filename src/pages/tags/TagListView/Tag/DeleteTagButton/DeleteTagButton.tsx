ef5d53b5a1a34698007e92426612bd87ba765a34
larry adams <ladams1776@yahoo.com>
2021-01-27 23:10:40 -0500

    Convert DeleteTagButton to TypeScript

---
+++ src/pages/tags/TagListView/Tag/DeleteTagButton/DeleteTagButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import cn from 'classnames';
+import { Button } from 'components';
+import { useFlashMessageFetchApiData, reloadAndRefresh } from 'utils';
+import styles from './DeleteTagButton.module.css';
+
+interface DeleteTagButtonProp {
+  tagId: string;
+  isSelected: boolean;
+}
+
+const DeleteTagButton: React.FC<DeleteTagButtonProp> = ({ tagId, isSelected }) => {
+  const reload = reloadAndRefresh('tags', {}, () => {});
+  const dispatch = useFlashMessageFetchApiData(`tag/${tagId}`, { method: 'DELETE' }, reload, 'Successfully deleted tag', 'Error deleting tag');
+
+  const _deleteClick = (e: any): void => {
+    e.preventDefault(); // stops us from bubbling up to the Tag.
+    dispatch();
+  };
+
+  return <Button onClick={_deleteClick} className={cn(styles.deleteBtn, 'glyphicon glyphicon-trash', { [styles.selected]: isSelected })} data-test-id="delete-tag-button" />;
+};
+
+export default DeleteTagButton;
