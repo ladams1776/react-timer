4d53c9707cee14ff2d0ac57d1923129ebaca5682
larry adams <ladams1776@yahoo.com>
2021-01-27 23:26:15 -0500

    Update test with React testing library

--- src/pages/tags/TagListView/Tag/DeleteTagButton/__test__/DeleteTagButton.test.js
+++ src/pages/tags/TagListView/Tag/DeleteTagButton/__test__/DeleteTagButton.test.js
@@ -1,63 +1,31 @@
 import React from "react";
-import { shallow } from 'enzyme';
-import { useFlashMessageFetchApiData, reloadAndRefresh } from 'utils';
-import { findByTestId } from "testUtils";
+import { fireEvent, render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+// target
 import DeleteTagButton from "../DeleteTagButton";
 
-jest.mock('utils/api/useFlashMessageFetchApiData');
-jest.mock('utils/api/reloadAndRefresh');
+// mocks
+jest.mock('utils/api/reloadAndRefresh', () => {
+  return () => jest.fn();
+});
+
+jest.mock('utils/api/useFlashMessageFetchApiData', () => {
+  return () => jest.fn();
+});
 
 describe("src/pages/tags/TaskListView/Tag/DeleteTagButton/__test__/DeleteTagButton.test.js", () => {
   describe('DeleteTagButton', () => {
-    // Arrange
-    let wrapper;
-
     it('should render the button, and helper functions should be called to be ready for the onClick', () => {
       // Arrange
-      const id = 1;
-      const setTags = jest.fn();
-      const reload = jest.fn();
-      reloadAndRefresh.mockReturnValue(reload);
-      const dispatch = jest.fn();
-      useFlashMessageFetchApiData.mockReturnValue(dispatch);
-
-      const expected = {
-        className: "deleteBtn glyphicon glyphicon-remove",
-        "data-test-id": "delete-tag-button",
-        onClick: expect.anything()
-      }
-
+      const tagId = 'tagId';
+      const isSelected = true;
+      
       // Act
-      wrapper = shallow(<DeleteTagButton tagId={id} setTags={setTags} />);
+      const target = render(<DeleteTagButton tagId={tagId} isSelected={isSelected} />);
 
       // Assert
-      expect(reloadAndRefresh).toHaveBeenNthCalledWith(1, 'tags', {}, setTags);
-      expect(useFlashMessageFetchApiData).toHaveBeenNthCalledWith(1, `tag/${id}`, { method: 'DELETE' }, reload, 'Successfully deleted tag', 'Error deleting tag');
-      expect(findByTestId(wrapper, 'delete-tag-button').props()).toEqual(expected);
-    });
-
-    describe('_deleteClick', () => {
-      it('should call dispatch, which was prepped during button instantiation', async () => {
-        // Arrange
-        const event = {
-          preventDefault: jest.fn(),
-        };
-        const id = 1;
-        const setTags = jest.fn();
-        const reload = jest.fn();
-        reloadAndRefresh.mockReturnValue(reload);
-        const dispatch = jest.fn();
-        useFlashMessageFetchApiData.mockReturnValue(dispatch);
-
-        wrapper = shallow(<DeleteTagButton tagId={id} setTags={setTags} />);
-
-        // Act
-        await findByTestId(wrapper, 'delete-tag-button').props().onClick(event);
-
-        // Assert
-        expect(event.preventDefault).toHaveBeenCalledTimes(1);
-        expect(dispatch).toHaveBeenCalledTimes(1);
-      });
+      expect(target.getByTestId('delete-tag-button')).toBeInTheDocument();
     });
   });
 });
