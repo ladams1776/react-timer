9c5d96473959bb715dfc61fbb7c4be9963934fb9
larry adams <ladams1776@yahoo.com>
2021-02-03 22:23:30 -0500

    Clean up useFormSetup but still need to redux-ify it.

--- src/pages/tags/AddTagForm/useFormSetup.js
+++ src/pages/tags/AddTagForm/useFormSetup.js
@@ -1,34 +1,20 @@
-import { useState, useRef } from 'react';
 import { fetchApiData } from 'utils';
 import { useBrowserHistory } from 'hooks'
 import { flashMessageHandleResponse, useFlashMessageAndIsLoadin } from 'utils';
 
-const useFormSetup = (tagId) => {
+const useFormSetup = () => {
     const { setSuccessFlashMessage, setErrorFlashMessage, setIsLoadin } = useFlashMessageAndIsLoadin();
     const { push } = useBrowserHistory();
 
-    const nameRef = useRef(null);
-    const descriptionRef = useRef(null);
-    const idRef = useRef(null);
-
-    const [tag, setTag] = useState({ _id: tagId });
-    /* eslint-disable-next-line no-unused-vars */
-    const [name, setName] = useState(tag?.name);
-    /* eslint-disable-next-line no-unused-vars */
-    const [description, setDescription] = useState(tag?.description);
-
-    const submit = (setTag, nameRef, descriptionRef, idRef, push) => async (event) => {
-        event.preventDefault();
+    const onSubmit = ({ _id, description, name }) => {
+        console.log('id', _id);
         const dispatch = items => {
-            setTag(items);
             push(`/tags`);
         }
-        const name = nameRef.current.value;
-        const description = descriptionRef.current.value;
-        const id = idRef.current.value;
+
         const config = {
-            body: { name, description, id },
-            method: id !== "-1" ? 'PUT' : 'POST'
+            body: { name, description, id: _id },
+            method: _id === undefined ? 'POST' : 'PUT'
         };
 
 
@@ -37,10 +23,7 @@ const useFormSetup = (tagId) => {
         fetchApiData('tag', config, r);
     };
 
-    const onSubmit = submit(setTag, nameRef, descriptionRef, idRef, push);
-
-
-    return { onSubmit, setName, setDescription, setTag, tag, nameRef, descriptionRef, idRef };
+    return { onSubmit };
 };
 
 export default useFormSetup;
