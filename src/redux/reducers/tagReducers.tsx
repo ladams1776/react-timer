be246f347df82a7a9f932a362e2a187836fb3c6b
larry adams <ladams1776@yahoo.com>
2021-02-03 22:44:41 -0500

    Add more reducers to the tagReducers

--- src/redux/reducers/tagReducers.tsx
+++ src/redux/reducers/tagReducers.tsx
@@ -1,31 +1,36 @@
-import { combineReducers } from 'redux'
-import {
-    FETCH_ALL_TAGS,
-    FETCB_ALL_TAGS_RESPONSE
-} from "../types";
+import { combineReducers } from 'redux';
+import { FETCH_ALL_TAGS, FETCH_ALL_TAGS_RESPONSE, FETCH_TAG_BY_ID_RESPONSE } from '../types';
 import { Tag, ResponseAction } from 'interfaces/redux/reducers';
 
 export const fetchTagsReducer = (state = null, action: ResponseAction<'FETCH_ALL_TAGS', Tag>) => {
-    switch (action.type) {
-        case FETCH_ALL_TAGS:
-            return state;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case FETCH_ALL_TAGS:
+      return action.data;
+    default:
+      return state;
+  }
 };
 
-export const responseAllTagsReducer = (state = null, action:ResponseAction<'FETCB_ALL_TAGS_RESPONSE', Tag>) => {
+export const responseAllTagsReducer = (state = null, action: ResponseAction<'FETCH_ALL_TAGS_RESPONSE', Tag>) => {
+  switch (action.type) {
+    case FETCH_ALL_TAGS_RESPONSE:
+      return action.data;
+    default:
+      return state;
+  }
+};
+
+export const responseFetchTagByIdReducer = (state = null, action: ResponseAction<'FETCH_TAG_BY_ID_RESPONSE', Tag>) => {
     switch (action.type) {
-        case FETCB_ALL_TAGS_RESPONSE:
-            return action.data;
-        default:
-            return state;
-    }
-}
-
-
+    case FETCH_TAG_BY_ID_RESPONSE:
+      return action.data;
+    default:
+      return state;
+  }
+};
 
 export default combineReducers({
-    fetchTagsReducer: fetchTagsReducer,
-    allTags: responseAllTagsReducer,
+  fetchTagsReducer: fetchTagsReducer,
+  tagById: responseFetchTagByIdReducer,
+  allTags: responseAllTagsReducer,
 });
