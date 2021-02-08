3d97a76d4f72a1ca478d93ff5320c269cb2b2a2a
larry adams <ladams1776@yahoo.com>
2021-02-07 22:59:37 -0500

    Remove id from tags schema

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,43 +1,36 @@
 db.auth('admin-user', 'admin-password')
 
 db.tags.insertOne({
-    id: 1,
     name: 'DDD',
     description: 'Domain Driven Design'
 });
 
 db.tags.insertOne({
-    id: 2,
     name: 'Clean Architecture',
     description: 'Uncle Bob related tag for clean architecture'
 });
 
 db.tags.insertOne({
-    id: 3,
     name: 'Unit Testing',
     description: 'Unit Testing in general'
 });
 
 db.tags.insertOne({
-    id: 4,
     name: 'Architecture',
     description: 'Architecture in general'
 });
 
 db.tags.insertOne({
-    id: 5,
     name: 'Microservices',
     description: 'Related to Microservices'
 });
 
 db.tags.insertOne({
-    id: 6,
     name: 'React Timer App',
     description: 'Related to the React Timer App'
 });
 
 db.tags.insertOne({
-    id: 7,
     name: 'Udemy',
     description: 'Related to a Udemy Course'
 });
