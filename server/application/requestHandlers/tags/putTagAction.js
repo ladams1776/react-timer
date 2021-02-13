a76647a9caccca7437ea1872b304009232d10a16
larry adams <ladams1776@yahoo.com>
2021-02-12 21:37:27 -0500

    update the order in imports

--- server/application/requestHandlers/tags/putTagAction.js
+++ server/application/requestHandlers/tags/putTagAction.js
@@ -1,6 +1,6 @@
+const { PUT_TAG_RESPONSE } = require('../reduxTypes');
 const TagService = require('../../../domain/services/tags/TagService');
 const apiResponse = require('../apiResponse');
-const { PUT_TAG_RESPONSE } = require('../reduxTypes');
 
 module.exports = (req, res) => {
     const responder = apiResponse(res, PUT_TAG_RESPONSE);
