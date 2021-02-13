0d33dae0b7874cd540f023e47a775ad91943357d
larry adams <ladams1776@yahoo.com>
2021-02-12 21:37:40 -0500

    update the order in fetchAllTags repository

--- server/infrastructure/repositories/tags/fetchAllTags.js
+++ server/infrastructure/repositories/tags/fetchAllTags.js
@@ -1,5 +1,5 @@
 const hydrateAndResponse = require("../../hydrators/hydrateAndResponse");
 const Tag = require("../../models/Tag");
 
-module.exports = (res) => Tag.find({}, hydrateAndResponse(res));
+module.exports = (res) => Tag.find({}, hydrateAndResponse(res)).sort('-_id');
 
